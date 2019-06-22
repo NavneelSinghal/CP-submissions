5d62ccb73ad77c7d20777fd7c49bfa405fe8d17b
Navneel Singhal <navneel.singhal@ymail.com>
2019-06-22 13:22:00 +0530

    Add solution for problem `B - Mahmoud and a Triangle` Link: https://codeforces.com/contest/766/problem/B Tags: constructive algorithms, geometry, greedy, math, number theory, sortings, *1000 Ref: https://codeforces.com/contest/766/submission/55922139

---
+++ codeforces/766/B.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i = 0; i<n; i++){
+        cin>>a[i];
+    }
+    sort(a.begin(), a.end());
+    for(int i = 2; i<n; i++){
+        if(a[i-1]+a[i-2]>a[i]){
+            cout<<"YES";
+            return 0;
+        }
+    }
+    cout<<"NO";
+    return 0;
+}
