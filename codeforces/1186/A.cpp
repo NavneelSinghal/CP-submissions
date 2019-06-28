a68914e5d70c9a0f8f961ad485eadf031114109a
Navneel Singhal <navneel.singhal@ymail.com>
2019-06-28 13:52:00 +0530

    Add solution for problem `A - Vus the Cossack and a Contest` Link: https://codeforces.com/contest/1186/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1186/submission/56194687

---
+++ codeforces/1186/A.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, m, k;
+    cin>>n>>m>>k;
+    if((m>=n) && (k>=n)){
+        cout<<"Yes";
+    }
+    else{
+        cout<<"No";
+    }
+}
