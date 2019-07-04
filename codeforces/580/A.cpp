082add6423e6aad8db8666af3b448c25e777fa99
Navneel Singhal <navneel.singhal@ymail.com>
2019-07-04 13:27:00 +0530

    Add solution for problem `A - Kefa and First Steps` Link: https://codeforces.com/contest/580/problem/A Tags: brute force, dp, implementation, *900 Ref: https://codeforces.com/contest/580/submission/56504720

--- codeforces/580/A.cpp
+++ codeforces/580/A.cpp
@@ -1,35 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-void fastscan(int &number) 
-{  
-    bool negative = false; 
-    register int c; 
-  
-    number = 0; 
-
-    c = getchar(); 
-    if (c=='-') 
-    { 
-        negative = true; 
-        c = getchar(); 
-    } 
-
-    for (; (c>47 && c<58); c=getchar()) 
-        number = number *10 + c - 48; 
-
-    if (negative) 
-        number *= -1; 
-} 
-
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int n;
-    fastscan(n);
+    cin>>n;
     int a[n];
     for(int i = 0; i<n; i++){
-        fastscan(a[i]);
+        cin>>a[i];
     }
     int maxsf = 1;
     int maxfinal = 1;
