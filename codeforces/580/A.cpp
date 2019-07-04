d63f6d7995ea1b7214fdbec622ab29242bc80ed9
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2019-07-04 13:26:00 +0530

    Add solution for problem `A - Kefa and First Steps` Link: https://codeforces.com/contest/580/problem/A Tags: brute force, dp, implementation, *900 Ref: https://codeforces.com/contest/580/submission/56504665

--- codeforces/580/A.cpp
+++ codeforces/580/A.cpp
@@ -1,13 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
+inline int getint(){
+	int f=0;
+	char c;
+	do{
+		c=getchar();
+	}while( c<'0' || c>'9' ) ;
+	do{
+		f=f*10+c-'0';
+		c=getchar();
+	}while( c>='0' && c<='9' ) ;
+	return f;
+}
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int n;
-    cin>>n;
+    n = getint();
     int a[n];
     for(int i = 0; i<n; i++){
-        cin>>a[i];
+        a[i] = getint();
     }
     int maxsf = 1;
     int maxfinal = 1;
