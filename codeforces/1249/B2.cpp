79ffaa27caefea20ed4c7b9d3aae2a8029b8aff0
Navneel Singhal <navneel.singhal@ymail.com>
2019-10-24 13:43:00 +0530

    Add solution for problem `B2 - Books Exchange (hard version)` Link: https://codeforces.com/contest/1249/problem/B2 Tags: dfs and similar, dsu, math, *1300 Ref: https://codeforces.com/contest/1249/submission/63279566

---
+++ codeforces/1249/B2.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define int ll
+signed main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> p(n), visited(n), ans(n);
+        for(int i = 0; i<n; i++){
+            cin>>p[i];
+            p[i]--;
+        }
+        for (int j = 0; j < n; ++j) {
+			if (!visited[j]) {
+				vector<int> cur;
+				while (!visited[j]) {
+					cur.push_back(j);
+					visited[j] = true;
+					j = p[j];
+				}
+				for (auto el : cur) ans[el] = cur.size();
+			}
+		}
+        for(int i = 0; i<n; i++){
+            cout<<ans[i]<<" ";
+        }
+        cout<<endl;
+    }
+}
