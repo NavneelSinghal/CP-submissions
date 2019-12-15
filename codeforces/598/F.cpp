e0a9f1d1cec906839070dbebe6286f39d5f66bc6
Navneel Singhal <navneel.singhal@ymail.com>
2019-12-15 20:07:00 +0530

    Add solution for problem `F - Cut Length` Link: https://codeforces.com/contest/598/problem/F Tags: geometry, *2900 Ref: https://codeforces.com/contest/598/submission/66975701

--- codeforces/598/F.cpp
+++ codeforces/598/F.cpp
@@ -1,24 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long
+
+typedef long long ll;
+typedef long double ld;
+#define int ll
 typedef pair<long double, long double> pd;
 #define F first
 #define S second
 #define REP(i, n) for(int i = 0; i < (int) n; i++)
+#define INF 1000000000
 #define eps 1e-10
+
 vector<pd> vv;
+
+inline int cmp(double x){
+	return (x > eps) - (x < -eps);
+}
+
+long double dot(pd a, pd b){
+	return a.F * b.F + a.S * b.S;
+}
+
+long double  length(pd a){
+	return sqrt(dot(a, a));
+}
+
+long double cross(pd a, pd b){
+	return a.F * b.S - a.S * b.F;
+}
+
+long double intersect(pd a, pd b, pd c, pd d){
+	pd u = {a.F - c.F, a.S - c.S};
+	return cross(d, u)/cross(b, d);
+}
+
 pd operator + (pd a, pd b) {return {a.F+b.F, a.S+b.S};}
 pd operator - (pd a, pd b) {return {a.F-b.F, a.S-b.S};}
-inline int cmp(long double x){return (x > eps) - (x < -eps);}
-long double dot(pd a, pd b){return a.F * b.F + a.S * b.S;}
-long double length(pd a){return sqrt(dot(a, a));}
-long double cross(pd a, pd b){return a.F * b.S - a.S * b.F;}
-long double intersect(pd a, pd b, pd c, pd d){return cross(d, a-c)/cross(b, d);}
+
 void segint(pd p, pd v, pd q, pd w){
-	if(!cmp(cross(v, w)) && !cmp(cross(v, q-p))){
-		long double l = dot(v, q-p) / dot(v, v), r = dot(v, q+w-p) / dot(v, v);
-		if(l > r) swap(l, r);
-		vv.push_back({l, -1}), vv.push_back({r, +1});
+	if(!cmp(cross(v, w))){
+		if(!cmp(cross(v, q-p))){
+			long double l = dot(v, q-p) / dot(v, v);
+			long double r = dot(v, q+w-p) / dot(v, v);
+			if(l > r) swap(l, r);
+			vv.push_back({l, -1});
+			vv.push_back({r, +1});
+		}
 	}
 	else{
 		long double tmp = intersect(q, w, p, v);
@@ -30,18 +57,23 @@ void segint(pd p, pd v, pd q, pd w){
 		}
 	}
 }
+
 signed main() {
 	cout << setprecision(20) << fixed;
 	int n, m;
 	cin >> n >> m;
 	vector<pd> pts(n+1);
-	for(int i = 0; i < n; i++) cin >> pts[i].F >> pts[i].S;
+	for(int i = 0; i < n; i++){
+		cin >> pts[i].F >> pts[i].S;
+	}
 	pts[n] = pts[0];
 	for(int j = 0; j < m; j++){
 		pd a, b;
 		cin >> a.F >> a.S >> b.F >> b.S;
 		vv.clear();
-		for(int i = 0; i < n; i++) segint(a, b-a, pts[i], pts[i+1]-pts[i]);
+		for(int i = 0; i < n; i++){
+			segint(a, b-a, pts[i], pts[i+1]-pts[i]);
+		}
 		sort(vv.begin(), vv.end());
 		long double ans = 0;
 		long double k = 0;
