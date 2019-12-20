1d4d0cec144eacbe8f7c1f7f238732d433abbb05
Navneel Singhal <navneel.singhal@ymail.com>
2019-12-20 05:32:00 +0530

    Add solution for problem `D - Cow and Snacks` Link: https://codeforces.com/contest/1209/problem/D Tags: dfs and similar, dsu, graphs, *1700 Ref: https://codeforces.com/contest/1209/submission/67257478

--- codeforces/1209/D.cpp
+++ codeforces/1209/D.cpp
@@ -1,17 +1,6 @@
 #include <bits/stdc++.h>
 #define int long long
 using namespace std;
-vector<int> visited(100000);
-vector<vector<int>> adj(100000);
-
-void dfs (int v){
-	if(!visited[v]){
-		visited[v] = 1;
-		for(auto u : adj[v]){
-			dfs(u);
-		}
-	}
-}
 
 signed main(){
 	ios::sync_with_stdio(0);
@@ -19,6 +8,7 @@ signed main(){
 	int n;
 	int k;
 	cin >> n >> k;
+	vector<vector<int>> adj(n, vector<int>());
 	for(int i = 0; i < k; i++){
 		int u, v;
 		cin >> u >> v;
@@ -26,6 +16,15 @@ signed main(){
 		adj[u].push_back(v);
 		adj[v].push_back(u);
 	}
+	vector<int> visited(n, 0);
+	function<void(int)> dfs = [&](int v){
+		if(!visited[v]){
+			visited[v] = 1;
+			for(auto u : adj[v]){
+				dfs(u);
+			}
+		}
+	};
 	int ans = 0;
 	for(int i = 0; i < n; i++){
 		ans += !visited[i];
