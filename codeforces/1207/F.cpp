dd58a4f2e7ff30f9983cdeea1b97d88dfe7e400c
Navneel Singhal <navneel.singhal@ymail.com>
2019-12-21 14:38:00 +0530

    Add solution for problem `F - Remainder Problem` Link: https://codeforces.com/contest/1207/problem/F Tags: brute force, data structures, implementation, *2100 Ref: https://codeforces.com/contest/1207/submission/67324093

--- codeforces/1207/F.cpp
+++ codeforces/1207/F.cpp
@@ -3,54 +3,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define maxn 500005
-#define sq 270
+#define sq 275
 
 int a[maxn];
 int ans[sq+1][sq+1];
 int t, q, x, y, i, sol;
 
-void read(int &a) {
-    static char c;
-    c = getchar();
-    if (c == '-') {
-        a = 0;
-        while (c = getchar(), c >= '0')
-            a = (a << 3) + (a << 1) + (c - '0');
-        a = -a;
-    } else {
-        a = c - '0';
-        while (c = getchar(), c >= '0')
-            a = (a << 3) + (a << 1) + (c - '0');
-    }
-}
- 
-void read(char &c) {
-    c = getchar();
-    getchar();
-}
- 
-void read(string &s) {
-    s = "";
-    static char c;
-    while (c = getchar(), c >= 'a')
-        s += c;
-}
- 
-void print(int x) {
-    if (x < 0) putchar('-'), x = -x;
-    if (x >= 10) print(x / 10);
-    putchar(x % 10 + '0');
-}
- 
-void print(char c) {
-    putchar(c);
-}
-
 int main(){
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	read(t);
+	cin >> t;
 	while(t--){
-		read(q); read(x); read(y);
+		cin >> q >> x >> y;
 		if(q == 1){
 			a[x] += y;
 			for(i = 1; i <= sq; i++) ans[i][x % i] += y;
@@ -59,12 +22,10 @@ int main(){
 			if(x > sq){
 				sol = 0;
 				for(i = y; i <= maxn; i+=x) sol += a[i];
-				print(sol);
-				putchar('\n');
+				cout << sol << "\n";
 			}
 			else{
-				print(ans[x][y]);
-				putchar('\n');
+				cout << ans[x][y] << "\n";
 			}
 		}
 	}
