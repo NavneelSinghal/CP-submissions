2d2f3ae99d75d074bfaea735c967f3f544172225
Navneel Singhal <navneel.singhal@ymail.com>
2019-12-21 14:30:00 +0530

    Add solution for problem `F - Remainder Problem` Link: https://codeforces.com/contest/1207/problem/F Tags: brute force, data structures, implementation, *2100 Ref: https://codeforces.com/contest/1207/submission/67323748

--- codeforces/1207/F.cpp
+++ codeforces/1207/F.cpp
@@ -3,25 +3,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define maxn 500005
-#define sq 400
+#define sq 350
 
 int a[maxn];
 int ans[sq+1][sq+1];
-int t, q, x, y, i, sol;
 
-int main(){
+signed main(){
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t;
 	cin >> t;
+	int q, x, y;
 	while(t--){
 		cin >> q >> x >> y;
 		if(q == 1){
 			a[x] += y;
-			for(i = 1; i <= sq; i++) ans[i][x % i] += y;
+			for(int i = 1; i <= sq; i++) ans[i][x % i] += y;
 		}
 		else{
 			if(x > sq){
-				sol = 0;
-				for(i = y; i <= maxn; i+=x) sol += a[i];
+				int sol = 0;
+				for(int i = y; i <= maxn; i+=x) sol += a[i];
 				cout << sol << "\n";
 			}
 			else{
