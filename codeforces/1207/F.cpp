11896a582a8a135112c061ab2212fc4e5d60a68c
Navneel Singhal <navneel.singhal@ymail.com>
2019-12-21 15:14:00 +0530

    Add solution for problem `F - Remainder Problem` Link: https://codeforces.com/contest/1207/problem/F Tags: brute force, data structures, implementation, *2100 Ref: https://codeforces.com/contest/1207/submission/67325886

--- codeforces/1207/F.cpp
+++ codeforces/1207/F.cpp
@@ -10,122 +10,62 @@ int a[maxn];
 int ans[sq+1][sq+1];
 int t, q, x, y, i, sol;
 
-namespace IO {
-    const int BUFFER_SIZE = 1 << 15;
- 
-    char input_buffer[BUFFER_SIZE];
-    int input_pos = 0, input_len = 0;
- 
-    char output_buffer[BUFFER_SIZE];
-    int output_pos = 0;
- 
-    char number_buffer[100];
-    uint8_t lookup[100];
- 
-    void _update_input_buffer() {
-        input_len = fread(input_buffer, sizeof(char), BUFFER_SIZE, stdin);
-        input_pos = 0;
- 
-        if (input_len == 0)
-            input_buffer[0] = EOF;
-    }
- 
-    inline char next_char(bool advance = true) {
-        if (input_pos >= input_len)
-            _update_input_buffer();
- 
-        return input_buffer[advance ? input_pos++ : input_pos];
-    }
- 
-    template<typename T>
-    inline void read_int(T &number) {
-        bool negative = false;
-        number = 0;
- 
-        while (!isdigit(next_char(false)))
-            if (next_char() == '-')
-                negative = true;
- 
-        do {
-            number = 10 * number + (next_char() - '0');
-        } while (isdigit(next_char(false)));
- 
-        if (negative)
-            number = -number;
-    }
- 
-    template<typename T, typename... Args>
-    inline void read_int(T &number, Args &... args) {
-        read_int(number);
-        read_int(args...);
-    }
- 
-    void _flush_output() {
-        fwrite(output_buffer, sizeof(char), output_pos, stdout);
-        output_pos = 0;
-    }
- 
-    inline void write_char(char c) {
-        if (output_pos == BUFFER_SIZE)
-            _flush_output();
- 
-        output_buffer[output_pos++] = c;
-    }
- 
-    template<typename T>
-    inline void write_int(T number, char after = '\0') {
-        if (number < 0) {
-            write_char('-');
-            number = -number;
-        }
- 
-        int length = 0;
- 
-        while (number >= 10) {
-            uint8_t lookup_value = lookup[number % 100];
-            number /= 100;
-            number_buffer[length++] = (lookup_value & 15) + '0';
-            number_buffer[length++] = (lookup_value >> 4) + '0';
-        }
- 
-        if (number != 0 || length == 0)
-            write_char(number + '0');
- 
-        for (int i = length - 1; i >= 0; i--)
-            write_char(number_buffer[i]);
- 
-        if (after)
-            write_char(after);
-    }
- 
-    void init() {
-        // Make sure _flush_output() is called at the end of the program.
-        bool exit_success = atexit(_flush_output) == 0;
-        assert(exit_success);
- 
-        for (int i = 0; i < 100; i++)
-            lookup[i] = (i / 10 << 4) + i % 10;
+void read(int &a) {
+    static char c;
+    c = getchar();
+    if (c == '-') {
+        a = 0;
+        while (c = getchar(), c >= '0')
+            a = (a << 3) + (a << 1) + (c - '0');
+        a = -a;
+    } else {
+        a = c - '0';
+        while (c = getchar(), c >= '0')
+            a = (a << 3) + (a << 1) + (c - '0');
     }
 }
-using namespace IO;
+ 
+void read(char &c) {
+    c = getchar();
+    getchar();
+}
+ 
+void read(string &s) {
+    s = "";
+    static char c;
+    while (c = getchar(), c >= 'a')
+        s += c;
+}
+ 
+void print(int x) {
+    if (x < 0) putchar('-'), x = -x;
+    if (x >= 10) print(x / 10);
+    putchar(x % 10 + '0');
+}
+ 
+void print(char c) {
+    putchar(c);
+}
 
 int main(){
-	init();
-	read_int(t);
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	read(t);
 	while(t--){
-		read_int(q, x, y);
+		read(q); read(x); read(y);
 		if(q == 1){
 			a[x] += y;
-			for(i = 1; i <= sq; i++) ans[i][x % i] += y;
+			for(i = 1; i < sq; i++) ans[i][x % i] += y;
 		}
 		else{
-			if(x > sq){
+			if(x >= sq){
 				sol = 0;
-				for(i = y; i <= maxn; i+=x) sol += a[i];
-				write_int(sol, '\n');
+				for(i = y; i < maxn; i+=x) sol += a[i];
+				print(sol);
+				putchar('\n');
 			}
 			else{
-				write_int(ans[x][y], '\n');
+				print(ans[x][y]);
+				putchar('\n');
 			}
 		}
 	}
