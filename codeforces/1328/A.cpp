c903a0bac180b6f401836af747787b1585449ab0
Navneel Singhal <navneel.singhal@ymail.com>
2020-05-23 18:07:00 +0530

    Add solution for problem `A - Divisibility Problem` Link: https://codeforces.com/contest/1328/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1328/submission/81014797

---
+++ codeforces/1328/A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int a, b;
+        cin >> a >> b;
+        int m = a % b;
+        if(m > 0){
+            m = b - m;
+        }
+        cout << m << "\n";
+    }
+}
