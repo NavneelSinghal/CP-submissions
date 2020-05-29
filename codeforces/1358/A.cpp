56cbf2a87711fdf6c7edc35004f3cad2c1a98d82
Navneel Singhal <navneel.singhal@ymail.com>
2020-05-29 19:49:00 +0530

    Add solution for problem `A - Park Lighting` Link: https://codeforces.com/contest/1358/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1358/submission/81893510

---
+++ codeforces/1358/A.cpp
@@ -0,0 +1,19 @@
+ #include <bits/stdc++.h>
+ using namespace std;
+ 
+ #define int long long
+ 
+ #define INF 1e9
+ 
+ signed main() {
+ 
+     ios_base::sync_with_stdio(0);
+     cin.tie(0); cout.tie(0);
+     int t;
+     cin >> t;
+     while(t--){
+     int n, m;
+     cin >> n >> m;
+     cout << (m*n + 1)/2 << endl;
+     }
+ }
