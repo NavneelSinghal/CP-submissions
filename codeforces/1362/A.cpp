49eee15adefbaa03ce0ccc56fd0082f2e596c8cf
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2020-06-04 20:11:00 +0530

    Add solution for problem `A - Johnny and Ancient Computer` Link: https://codeforces.com/contest/1362/problem/A Tags: implementation, *1000 Ref: https://codeforces.com/contest/1362/submission/82493322

---
+++ codeforces/1362/A.cpp
@@ -0,0 +1,84 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <ratio>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define pii pair<int, int>
+#define vpii vector<pair<int, int>> 
+#define mii map<int, int>
+#define rep(i, n) for(int i = 0; i < n; i++)
+#define repd(i, n) for(int i = n-1; i >= 0; i--)
+#define F first
+#define S second
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define pb push_back
+#define mp make_pair
+#define eb emplace_back
+#define all(v) v.begin(),v.end()
+#define INF 1e9
+
+int v2(int a){
+    int ans = 0;
+    while(a % 2 == 0){
+        ans++;
+        a /= 2;
+    }
+    return ans;
+}
+
+void solve(){
+    int a, b;
+    cin >> a >> b;
+    int ansa = 0;
+    while(a % 2 == 0){
+        ansa ++;
+        a /= 2;
+    }
+    int ansb = 0;
+    while(b % 2 == 0){
+        ansb ++;
+        b /= 2;
+    }
+    if(a == b)
+    cout << (abs(ansa - ansb) + 2) / 3 << "\n";
+    else cout << "-1\n";
+}
+
+signed main(){
+    fastio;
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+} 
