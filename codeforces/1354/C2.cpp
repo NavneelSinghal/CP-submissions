7d74e7c5c7975e960ea17c40a15df691aeade870
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2020-06-05 11:39:00 +0530

    Add solution for problem `C2 - Not So Simple Polygon Embedding` Link: https://codeforces.com/contest/1354/problem/C2 Tags: binary search, brute force, geometry, math, *2000 Ref: https://codeforces.com/contest/1354/submission/82600490

---
+++ codeforces/1354/C2.cpp
@@ -0,0 +1,70 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <ratio>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define pii pair<int, int>
+#define vpii vector<pair<int, int>> 
+#define mii map<int, int>
+#define rep(i, n) for(int i = 0; i < n; i++)
+#define repd(i, n) for(int i = n-1; i >= 0; i--)
+#define F first
+#define S second
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define pb push_back
+#define mp make_pair
+#define eb emplace_back
+#define all(v) v.begin(),v.end()
+#define INF 1e9
+#define EPS 1e-9
+#define PI acosl(-1)
+
+void solve(){
+    int n;
+    cin >> n;
+    n /= 2;
+    double theta = PI/(4*n+2);
+    double d = 1/sin(theta);
+    double ans = d*(sqrtl(2.0) * sin(n * theta) + cos(PI/4 + n * theta));
+    cout << ans << endl;
+}
+
+signed main(){
+    fastio;
+    cout << setprecision(20) << fixed;
+    int t = 1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+} 
