0369079c9b3c58e86c1de39e99c768eacb4bf258
Navneel Singhal <navneel.singhal@ymail.com>
2020-06-05 13:41:00 +0530

    Add solution for problem `B - Math` Link: https://codeforces.com/contest/1062/problem/B Tags: greedy, math, number theory, *1500 Ref: https://codeforces.com/contest/1062/submission/82610542

---
+++ codeforces/1062/B.cpp
@@ -0,0 +1,100 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <ratio>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define pii pair<int, int>
+#define vpii vector<pii> 
+#define vvpii vector<vpii>
+#define mii map<int, int>
+#define rep(i, n) for(int i = 0; i < n; i++)
+#define repd(i, n) for(int i = n-1; i >= 0; i--)
+#define F first
+#define S second
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define pb push_back
+#define mp make_pair
+#define eb emplace_back
+#define all(v) v.begin(),v.end()
+#define INF 1e9
+#define EPS 1e-9
+#define PI acosl(-1)
+
+int digs(int n){
+    int ans = 0;
+    while(n){
+        n /= 2;
+        ans++;
+    }
+    return ans;
+}
+
+void solve(){
+    int n;
+    cin >> n;
+    int acc = 0;
+    int ans = 1;
+    vi v;
+    int mx = 0;
+    rep(i, n+1){
+        if(i > 1 && n % i == 0){
+            int a = 0;
+            ans *= i;
+            while(n % i == 0){
+                a++;
+                n /= i;
+            }
+            v.pb(a);
+            mx = max(mx, digs(a-1));
+        }
+    }
+    cout << ans << " ";
+    int steps = mx;
+    for(auto x : v){
+        if(x != (1 << mx)){
+            steps = 1 + mx;
+            break;
+        }
+    }
+    cout << steps << endl;
+}
+
+signed main(){
+    fastio;
+    cout << setprecision(20) << fixed;
+    int t = 1;
+    //cin >> t;
+    while(t--){
+        solve();
+    }
+} 
