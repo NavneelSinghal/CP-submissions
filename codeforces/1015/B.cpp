9ba7782dbe97b8f7005a9d67886ab64f58a0014e
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2020-06-07 19:48:00 +0530

    Add solution for problem `B - Obtaining the String` Link: https://codeforces.com/contest/1015/problem/B Tags: implementation, *1200 Ref: https://codeforces.com/contest/1015/submission/82785618

---
+++ codeforces/1015/B.cpp
@@ -0,0 +1,92 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <ratio>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define pii pair<int, int>
+#define vpii vector<pii> 
+#define vvpii vector<vpii>
+#define mii map<int, int>
+#define rep(i, n) for(int i = 0; i < n; i++)
+#define rep2(i, a, b) for(int i = a; i < b; i++)
+#define repd2(i, a, b) for(int i = b - 1; i >= a; i--)
+#define repd(i, n) for(int i = n-1; i >= 0; i--)
+#define F first
+#define S second
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define pb push_back
+#define mp make_pair
+#define eb emplace_back
+#define all(v) v.begin(),v.end()
+#define INF 1e9
+#define EPS 1e-9
+#define PI acosl(-1)
+
+void solve(){
+    int n;
+    cin >> n;
+    string s, t;
+    cin >> s >> t;
+    vi ans;
+    rep(i, n){
+        if(s[i] == t[i]) continue;
+        int idx = -1;
+        for(int j = i + 1; j < n; j++){
+            if(s[j] == t[i]){
+                idx = j;
+                break;
+            }
+        }
+        if(idx == -1) {
+            cout << -1 << endl;
+            return;
+        }
+        for(int j = idx; j >= i + 1; j--){
+            ans.pb(j);
+            swap(s[j], s[j - 1]);
+        }
+    }
+    cout << ans.size() << endl;
+    for(auto x : ans) cout << x << " ";
+    cout << endl;
+}
+
+signed main(){
+    fastio;
+    cout << setprecision(20) << fixed;
+    int t = 1;
+    //cin >> t;
+    while(t--){
+        solve();
+    }
+} 
