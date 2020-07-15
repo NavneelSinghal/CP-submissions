a64e93ec5d822871946d28afce86f035a02b72aa
Navneel Singhal <navneel.singhal@ymail.com>
2020-07-16 03:55:00 +0530

    Add solution for problem `E - Compatible Numbers` Link: https://codeforces.com/contest/165/problem/E Tags: bitmasks, brute force, dfs and similar, dp, *2200 Ref: https://codeforces.com/contest/165/submission/86946304

--- codeforces/165/E.cpp
+++ codeforces/165/E.cpp
@@ -1,28 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int m = 22;
-const int M = 1 << m;
-
 int main () {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
     int n; cin >> n;
-    vector<unsigned> dp((1 << m), UINT_MAX), a(n);
+    int m = 22;
+    vector<unsigned> dp((1 << m), UINT_MAX);
+    vector<int> a(n);
     for (int i = 0; i < n; ++i) {
         cin >> a[i];
         dp[a[i]] = a[i];
     }
     for (int i = 0; i < m; ++i) {
-        for (int mask = 0; mask < M; ++mask) {
-            if (~dp[mask]) {
-                dp[mask | (1 << i)] = dp[mask];
-            }
+        for (int mask = 0; mask < (1 << m); ++mask) {
+            dp[mask | (1 << i)] = min(dp[mask], dp[mask | (1 << i)]);
         }
     }
     for (int i = 0; i < n; ++i) {
-        cout << (int) dp[(~a[i]) & (M - 1)] << " ";
+        cout << (int) dp[~a[i] & ((1 << m) - 1)] << " ";
     }
     cout << endl;
 }
