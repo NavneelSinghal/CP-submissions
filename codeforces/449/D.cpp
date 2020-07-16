22b56336b427ae520be791df0fb24113f59f6b0c
Navneel Singhal <navneel.singhal@ymail.com>
2020-07-16 05:42:00 +0530

    Add solution for problem `D - Jzzhu and Numbers` Link: https://codeforces.com/contest/449/problem/D Tags: bitmasks, combinatorics, dp, *2400 Ref: https://codeforces.com/contest/449/submission/86948283

---
+++ codeforces/449/D.cpp
@@ -0,0 +1,134 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int m = 20;
+const int M = 1 << m;
+vector<int> dp(M), p2(M);
+const int mod = 1e9 + 7;
+
+namespace IO {
+    const int BUFFER_SIZE = 1 << 15;
+    char input_buffer[BUFFER_SIZE];
+    int input_pos = 0, input_len = 0;
+    char output_buffer[BUFFER_SIZE];
+    int output_pos = 0;
+    char number_buffer[100];
+    uint8_t lookup[100];
+    void _update_input_buffer() {
+        input_len = fread(input_buffer, sizeof(char), BUFFER_SIZE, stdin);
+        input_pos = 0;
+        if (input_len == 0)
+            input_buffer[0] = EOF;
+    }
+    inline char next_char(bool advance = true) {
+        if (input_pos >= input_len)
+            _update_input_buffer();
+
+        return input_buffer[advance ? input_pos++ : input_pos];
+    }
+
+    template<typename T>
+        inline void read_int(T &number) {
+            bool negative = false;
+            number = 0;
+
+            while (!isdigit(next_char(false)))
+                if (next_char() == '-')
+                    negative = true;
+
+            do {
+                number = 10 * number + (next_char() - '0');
+            } while (isdigit(next_char(false)));
+
+            if (negative)
+                number = -number;
+        }
+
+    template<typename T, typename... Args>
+        inline void read_int(T &number, Args &... args) {
+            read_int(number);
+            read_int(args...);
+        }
+
+    void _flush_output() {
+        fwrite(output_buffer, sizeof(char), output_pos, stdout);
+        output_pos = 0;
+    }
+
+    inline void write_char(char c) {
+        if (output_pos == BUFFER_SIZE)
+            _flush_output();
+
+        output_buffer[output_pos++] = c;
+    }
+
+    template<typename T>
+        inline void write_int(T number, char after = '\0') {
+            if (number < 0) {
+                write_char('-');
+                number = -number;
+            }
+            int length = 0;
+            while (number >= 10) {
+                uint8_t lookup_value = lookup[number % 100];
+                number /= 100;
+                number_buffer[length++] = (lookup_value & 15) + '0';
+                number_buffer[length++] = (lookup_value >> 4) + '0';
+            }
+            if (number != 0 || length == 0)
+                write_char(number + '0');
+            for (int i = length - 1; i >= 0; i--)
+                write_char(number_buffer[i]);
+            if (after)
+                write_char(after);
+        }
+    void IOinit() {
+        // Make sure _flush_output() is called at the end of the program.
+        bool exit_success = atexit(_flush_output) == 0;
+        assert(exit_success);
+        for (int i = 0; i < 100; i++)
+            lookup[i] = (i / 10 << 4) + i % 10;
+    }
+}
+
+using namespace IO;
+
+int main () {
+
+    IOinit();
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int n, x;
+    read_int(n);
+
+    for (int i = 0; i < n; ++i) {
+        read_int(x);
+        dp[x]++;
+    }
+    for (int i = 1; i < M; i <<= 1) {
+        for (int mask = 0; mask < M; ++mask) {
+            if (mask & i) {
+                dp[i ^ mask] += dp[mask];
+            }
+        }
+    }
+    
+    int cur = 1;
+    for (int i = 0; i < M; ++i) {
+        p2[i] = cur;
+        cur = (cur << 1) % mod;
+    }
+
+    int ans = 0;
+
+    for (int i = 0; i < M; i++) {
+        ans += ((__builtin_popcount(i) & 1) ? -1 : 1) * p2[dp[i]];
+        if (ans < 0) ans += mod;
+        else if (ans >= mod) ans -= mod;
+    }
+
+    write_int(ans, '\n');
+    _flush_output();
+}
