6eed6562274141f09aa5831a0189a4e6def4405e
Navneel Singhal <navneel.singhal@ymail.com>
2020-09-02 14:38:00 +0530

    Add solution for problem `B - Kefa and Company` Link: https://codeforces.com/contest/580/problem/B Tags: binary search, sortings, two pointers, *1500 Ref: https://codeforces.com/contest/580/submission/91626749

--- codeforces/580/B.ml
+++ codeforces/580/B.ml
@@ -1,24 +1,25 @@
-let read_int64 () = Scanf.bscanf Scanf.Scanning.stdin " %d " (fun x -> Int64.of_int x);;
+let read_int64 () = Scanf.bscanf Scanf.Scanning.stdin " %s " (fun x -> Big_int.big_int_of_string x);;
 let read_int () = Scanf.bscanf Scanf.Scanning.stdin " %d " (fun x -> x);;
 let read_pair _ = let x = read_int64 () in let y = read_int64 () in (x, y);;
-let comp (a, b) (c, d) = if (Int64.compare a c) = 0 then Int64.compare b d else Int64.compare a c;; 
+let comp (a, b) (c, d) = if (Big_int.compare_big_int a c) = 0 then Big_int.compare_big_int b d else Big_int.compare_big_int a c;; 
 let main () = 
     let n = read_int () in
     let d = read_int64 () in
     let a = Array.init n read_pair in
     let _ = Array.sort comp a in
-    let (i, j, k, c) = (ref 0, ref 0, ref 0L, ref 0L) in (
+    let (i, j, k, c) = (ref 0, ref 0, ref (Big_int.big_int_of_int 0), ref (Big_int.big_int_of_int 0)) in (
         while !i < n do (
             match (a.(!i), a.(!j)) with
             | ((x, y), (z, w)) -> (
-                if Int64.compare (Int64.sub x z) d < 0 then
-                    (k := (Int64.add !k y); c := (max (!c) (!k)); incr i)
+                if Big_int.compare_big_int (Big_int.sub_big_int x z) d < 0 then
+                    (k := (Big_int.add_big_int !k y); c := (Big_int.max_big_int (!c) (!k)); incr i)
                 else 
-                    (k := (Int64.sub !k w); incr j)
+                    (k := (Big_int.sub_big_int !k w); incr j)
             )
         )
         done;
-        Printf.printf "%s\n" (Int64.to_string !c)
+        Printf.printf "%s\n" (Big_int.string_of_big_int !c)
     )
 ;;
+
 main ();;
