823e04f96b6ff88f60acea85d40a62bf70efdc0d
Navneel Singhal <navneel.singhal@ymail.com>
2020-10-11 16:11:00 +0530

    Add solution for problem `F - Knapsack for All Subsets` Link: https://atcoder.jp/contests/abc169/tasks/abc169_f Tags: AtCoder, *600 Ref: https://atcoder.jp/contests/abc169/submissions/17333350

--- atcoder/abc169/F.cpp
+++ atcoder/abc169/F.cpp
@@ -1,18 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using i32 = int_fast32_t;
-using i64 = int_fast64_t;
-
 namespace NTT {
+  
+    const int64_t MOD = (119 << 23) + 1; // 998244353
+    const int64_t ROOT = 62;
 
-    // possible mods - 5 << 25, 7 << 26, 479 << 21, 483 << 21 with the same ROOT
-    
-    const i64 MOD = (119 << 23) + 1; // 998244353
-    const i64 ROOT = 62;
-
-    i64 modpow(i64 a, i64 n) {
-        i64 ans = 1;
+    int64_t modpow(int64_t a, int64_t n) {
+        int64_t ans = 1;
         while (n) {
             if (n & 1) ans = ans * a % MOD;
             a = a * a % MOD;
@@ -21,35 +16,35 @@ namespace NTT {
         return ans;
     }
 
-    void ntt (vector<i64> &a) {
+    void ntt (vector<int64_t> &a) {
         
-        i32 n = a.size();
-        i32 L = 31 - __builtin_clz(n);
+        int32_t n = a.size();
+        int32_t L = 31 - __builtin_clz(n);
 
-        static vector<i64> rt(2, 1);
+        static vector<int64_t> rt(2, 1);
 
-        for (static i32 k = 2, s = 2; k < n; k <<= 1, ++s) {
+        for (static int32_t k = 2, s = 2; k < n; k <<= 1, ++s) {
             rt.resize(n);
-            i64 z[] = {1, modpow(ROOT, MOD >> s)};
+            int64_t z[] = {1, modpow(ROOT, MOD >> s)};
             for (int i = k; i < (k << 1); ++i) {
                 rt[i] = rt[i >> 1] * z[i & 1] % MOD;
             }
         }
 
-        vector<i32> rev(n);
-        for (i32 i = 0; i < n; ++i) {
+        vector<int32_t> rev(n);
+        for (int32_t i = 0; i < n; ++i) {
             rev[i] = (rev[i >> 1] | ((i & 1) << L)) >> 1;
         }
 
-        for (i32 i = 0; i < n; ++i) {
+        for (int32_t i = 0; i < n; ++i) {
             if (i < rev[i]) swap(a[i], a[rev[i]]);
         }
 
-        for (i32 k = 1; k < n; k <<= 1) {
-            for (i32 i = 0; i < n; i += (k << 1)) {
-                for (i32 j = 0; j < k; ++j) {
-                    i64 z = rt[j + k] * a[i + j + k] % MOD;
-                    i64 &ai = a[i + j];
+        for (int32_t k = 1; k < n; k <<= 1) {
+            for (int32_t i = 0; i < n; i += (k << 1)) {
+                for (int32_t j = 0; j < k; ++j) {
+                    int64_t z = rt[j + k] * a[i + j + k] % MOD;
+                    int64_t &ai = a[i + j];
                     a[i + j + k] = ai - z + ((z > ai) ? MOD : 0);
                     ai += ((ai + z >= MOD) ? (z - MOD) : z);
                 }
@@ -58,43 +53,36 @@ namespace NTT {
 
     }
 
-    vector<i64> conv(const vector<i64>& a, const vector<i64>& b, const i32 max_pow = -1) {
-        
+    vector<int64_t> conv(const vector<int64_t>& a, const vector<int64_t>& b, const int32_t max_pow = -1) {
         if (a.empty() || b.empty()) return {};
-        
-        i32 s = a.size() + b.size() - 1,
-                B = 32 - __builtin_clz(s),
-                n = 1 << B,
-                inv = modpow(n, MOD - 2);
-        
-        vector<i64> L(a), R(b), out(n);
+        int32_t s = a.size() + b.size() - 1;
+        int32_t B = 32 - __builtin_clz(s);
+        int32_t n = 1 << B;
+        int32_t inv = modpow(n, MOD - 2);
+        vector<int64_t> L(a), R(b), out(n);
         L.resize(n), R.resize(n);
-        
-        ntt(L), ntt(R);
-        
-        for (i32 i = 0; i < n; ++i) {
-            out[(-i) & (n - 1)] = ((((i64)L[i] * R[i]) % MOD) * inv) % MOD;
+        ntt(L);
+        ntt(R);
+        for (int32_t i = 0; i < n; ++i) {
+            out[(-i) & (n - 1)] = ((((int64_t)L[i] * R[i]) % MOD) * inv) % MOD;
         }
-        
         ntt(out);
-        
         if (max_pow >= 0) s = min(s, max_pow + 1);
-        
         return {out.begin(), out.begin() + s};
     }
 
 }
 
 int main() {
-    i32 n, s;
+    int32_t n, s;
     cin >> n >> s;
-    vector<i32> a(n);
+    vector<int32_t> a(n);
     for (auto &x : a) cin >> x;
 
-    function<vector<i64>(i32, i32)> multiply_many = [&] (i32 l, i32 r) {
-        if (l > r) return vector<i64>(1, 1);
+    function<vector<int64_t>(int32_t, int32_t)> multiply_many = [&] (int32_t l, int32_t r) {
+        if (l > r) return vector<int64_t>(1, 1);
         if (l == r) {
-            vector<i64> w(a[l] + 1);
+            vector<int64_t> w(a[l] + 1);
             w[0] = 1;
             w[a[l]] = NTT::modpow(2, NTT::MOD - 2);
             return w;
