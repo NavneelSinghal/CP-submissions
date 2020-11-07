15f2d064034ada1915863bac9239147d20763090
Navneel Singhal <navneel.singhal@ymail.com>
2020-11-07 20:21:00 +0530

    Add solution for problem `E - Long Permutation` Link: https://codeforces.com/contest/1443/problem/E Tags: brute force, math, two pointers, *2400 Ref: https://codeforces.com/contest/1443/submission/97853184

--- codeforces/1443/E.cpp
+++ codeforces/1443/E.cpp
@@ -3,10 +3,7 @@
 #pragma GCC optimize("unroll-loops")
 
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
 
-using namespace __gnu_pbds;
 using namespace std;
 
 void setIO(string name = "") {
@@ -19,10 +16,6 @@ void setIO(string name = "") {
     FILE *fout = freopen((name + ".out").c_str(), "w", stdout);
     cout << setprecision(10) << fixed;
 }
-
-typedef tree <int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
-//order_of_key(k) - number of elements e such that func(e, k) returns true, where func is less or less_equal
-//find_by_order(k) - kth element in the set counting from 0
 
 void solve(int case_no) {
 
@@ -51,16 +44,14 @@ void solve(int case_no) {
         if (typ == 1) {
 
             int position = lft;
-            ordered_set pos;
-            for (int i = 1; i <= iter; ++i) pos.insert(i);
+            vector<int> pos(iter);
+            iota(pos.begin(), pos.end(), 1);
             old_x = x;
-            int found_element;
             for (int i = iter - 1; ~i; --i) {
                 long long w = x / fact[i];
                 x %= fact[i];
-                found_element = *pos.find_by_order(w);
-                a[position++] = found_element + lft;
-                pos.erase(found_element);
+                a[position++] = pos[w] + lft;
+                pos.erase(pos.begin() + w);
             }
             x = old_x;
             
