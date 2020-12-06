ba5d70e35f142fda2ffb80d352bceceae4ebefb6
Navneel Singhal <navneel.singhal@ymail.com>
2020-12-07 01:47:00 +0530

    Add solution for problem `D - Rating Compression` Link: https://codeforces.com/contest/1450/problem/D Tags: binary search, data structures, greedy, implementation, two pointers, *1800 Ref: https://codeforces.com/contest/1450/submission/100588191

--- codeforces/1450/D.cpp
+++ codeforces/1450/D.cpp
@@ -33,34 +33,60 @@ long long pwr(long long a, int n) {
     return ans;
 }
 
+template<typename T>
+struct sparse_table {
+    int n, l;
+    vector<vector<T>> s;
+    sparse_table(vector<T>& a) {
+        n = a.size();
+        l = 2 + __lg(n + 1);
+        s.assign(l, vector<T>(n));
+        s[0] = a;
+        for (int j = 0; j < l - 1; ++j) {
+            for (int i = 0; i + (2 << j) <= n; ++i) {
+                s[j + 1][i] = min(s[j][i], s[j][i + (1 << j)]);
+            }
+        }
+    }
+    // [l, r)
+    T query(int l, int r) {
+        int sz = __lg(r - l);
+        return min(s[sz][l], s[sz][r - (1 << sz)]);
+    }
+};
+
 void solve(int _) {
     int n;
     cin >> n;
-    vector<int> a(n), freq(n + 1);
-    for (auto &x : a) {
-        cin >> x;
-        freq[--x]++;
-    }
-    vector<int> later = freq;
-    int l = 0, r = n - 1, id = n, curmin = 0;
+    vector<int> a(n);
+    for (auto &x : a) cin >> x, --x;
+    sparse_table<int> s(a);
+    int l = 0, r = n - 1;
+    int fail = -1;
     for (int i = 0; i < n; ++i) {
-        if (a[l] == i) freq[a[l++]]--;
-        else if (a[r] == i) freq[a[r--]]--;
-        else {
-            id = i;
+        if (a[l] == i) {
+            l++;
+        } else if (a[r] == i) {
+            r--;
+        } else {
+            fail = i;
             break;
         }
-        while (curmin < n && freq[curmin] == 0) curmin++;
-        //cout << curmin << '\n';
-        if (curmin != i + 1) {
-            id = i;
+        if (s.query(l, r + 1) != i + 1) {
+            fail = i;
             break;
         }
     }
-    vector<int> ans(n, 1);
-    for (int i = 0; i < n; ++i) ans[0] &= (later[i] == 1);
-    for (int i = 1; i < n - 1; ++i) ans[i] = (i >= n - id - 1);
-    ans[n - 1] = *min_element(a.begin(), a.end()) == 0;
+    vector<int> ans(n);
+    sort(a.begin(), a.end());
+    ans[0] = 1;
+    for (int i = 0; i < n; ++i) {
+        ans[0] &= (a[i] == i);
+    }
+    for (int i = 1; i < n; ++i) {
+        ans[i] = (i >= n - fail - 1);
+    }
+    ans[n - 1] = a[0] == 0;
     for (auto &x : ans) cout << x;
     cout << '\n';
 }
