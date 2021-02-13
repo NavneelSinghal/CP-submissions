7d8de03ff8298175331ca85f53c504ca4e69a27f
Navneel Singhal <navneel.singhal@ymail.com>
2021-02-13 10:10:00 +0530

    Add solution for problem `C - Floor and Mod` Link: https://codeforces.com/contest/1485/problem/C Tags: binary search, brute force, data structures, math, number theory, *1700 Ref: https://codeforces.com/contest/1485/submission/107253553

---
+++ codeforces/1485/C.cpp
@@ -0,0 +1,171 @@
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx")
+// #pragma GCC optimize("unroll-loops")
+
+#include "bits/stdc++.h"
+#include "ext/pb_ds/assoc_container.hpp"
+#include "ext/pb_ds/tree_policy.hpp"
+//#include <ext/rope>
+
+using namespace __gnu_pbds;
+// using namespace __gnu_cxx;
+using namespace std;
+
+using ll = long long;
+
+// #define int long long
+#define unordered_map gp_hash_table
+
+template <typename T>
+void ignore_unused(const T &) {}
+#ifdef DEBUG
+#define debug(args...)                               \
+    {                                                \
+        std::string _s = #args;                      \
+        replace(_s.begin(), _s.end(), ',', ' ');     \
+        std::stringstream _ss(_s);                   \
+        std::istream_iterator<std::string> _it(_ss); \
+        err(_it, args);                              \
+    }
+#define print_container(v)          \
+    {                               \
+        bool first = true;          \
+        os << "[";                  \
+        for (auto x : v) {          \
+            if (!first) os << ", "; \
+            os << x;                \
+            first = false;          \
+        }                           \
+        return os << "]";           \
+    }
+void err(std::istream_iterator<std::string> it) { ignore_unused(it); }
+template <typename T, typename... Args>
+void err(std::istream_iterator<std::string> it, T a, Args... args) {
+    std::cerr << *it << " = " << a << std::endl;
+    err(++it, args...);
+}
+
+// clang-format off
+
+template <typename T1, typename T2> inline std::ostream& operator << (std::ostream& os, const std::pair<T1, T2>& p) { return os << "(" << p.first << ", " << p.second << ")"; }
+template <typename T> inline std::ostream &operator << (std::ostream & os, const std::vector<T>& v) { print_container(v); }
+template <typename T> inline std::ostream &operator << (std::ostream & os, const std::set<T>& v) { print_container(v); }
+template <typename T1, typename T2> inline std::ostream &operator << (std::ostream & os, const std::map<T1, T2>& v) { print_container(v); }
+template <typename T1, typename T2, class C> inline std::ostream &operator << (std::ostream & os, const unordered_map<T1, T2, C>& v) { print_container(v); }
+template <typename T, class C> inline std::ostream &operator << (std::ostream & os, const unordered_set<T, C>& v) { print_container(v); }
+template <typename T1, typename T2> inline std::ostream &operator << (std::ostream & os, const std::multimap<T1, T2>& v) { print_container(v); }
+template <typename T> inline std::ostream &operator << (std::ostream & os, const std::multiset<T>& v) { print_container(v); }
+#else
+#define debug(args...) 0
+#endif
+
+template <typename X, typename Y> X &remin(X &x, const Y &y) { return x = (y < x) ? y : x; }
+template <typename X, typename Y> X &remax(X &x, const Y &y) { return x = (x < y) ? y : x; }
+template <typename X, typename Y> [[nodiscard]] bool ckmin(X &x, const Y &y) { return (y < x) ? (x = y, 1) : 0; }
+template <typename X, typename Y> [[nodiscard]] bool ckmax(X &x, const Y &y) { return (x < y) ? (x = y, 1) : 0; }
+template <typename T> inline T sq(T a) { return a * a; }
+template <typename T> inline T sq_dist(pair<T, T> &a, pair<T, T> &b) { return sq(a.first - b.first) + sq(a.second - b.second); }
+template <typename T> inline long double dist(pair<T, T> &a, pair<T, T> &b) { return sqrtl(sq_dist(a, b)); }
+inline long long isqrt(long long n) {
+    long long sq = (long long)sqrtl((long double)n) - 2;
+    sq = max(sq, 0LL);
+    while (sq * sq < n) sq++;
+    if ((sq * sq) == n) return sq;
+    return sq - 1;
+}
+inline bool is_sq(int n) { int w = (int)isqrt(n); return w * w == n; }
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cout << setprecision(10) << fixed;
+    if (name.size() == 0) return;
+    FILE *fin = freopen((name + ".in").c_str(), "r", stdin);
+    FILE *fout = freopen((name + ".out").c_str(), "w", stdout);
+    ignore_unused(fin);
+    ignore_unused(fout);
+}
+
+// clang-format on
+
+// constexpr int mod = 998244353;
+constexpr int mod = 1e9 + 7;
+constexpr int64_t linf = 3e18;
+constexpr long double pi = acosl(-1.0);
+
+[[nodiscard]] int64_t pwr_mod(int64_t a, int64_t n) {
+    int64_t ans = 1;
+    while (n) {
+        if (n & 1) ans = (ans * a) % mod;
+        a = (a * a) % mod;
+        n >>= 1;
+    }
+    return ans;
+}
+
+// when using integers, keep overflow in mind
+template <typename T>
+T pwr(T a, int64_t n) {
+    T ans(1);
+    while (n) {
+        if (n & 1) ans *= a;
+        if (n > 1) a *= a;
+        n >>= 1;
+    }
+    return ans;
+}
+
+void precompute() {}
+
+long long sum(int x, int n) {
+    long long ans = 0;
+    for (int i = 1, la; i <= n; i = la + 1) {
+        if (x < i) break;
+        la = x / (x / i);
+        // n / x yields the same value for i <= x <= la.
+        ans += 1LL * (min(la, n) + 1 - i) * (x / i);
+    }
+    return ans;
+}
+
+void solve(int) {
+    // a = (a / b) * b + (a % b) = c * (b + 1)
+    // c = 1 to b - 1
+    // a = (b + 1), 2 * (b + 1) ..., (b - 1) * (b + 1)
+    // \sum_{b = 1}^y min(b - 1, x / (b + 1))
+    int x, y;
+    cin >> x >> y;
+    long long ans = 0;
+    int b = 1;
+    while (b * b - 1 <= x && b <= y) {
+        ans += b - 1;
+        ++b;
+    }
+    // sum of x / (t + 1) from t = b to t = y
+    // sum of x / t from t = b + 1 to t = y + 1
+    cout << ans + sum(x, y + 1) - sum(x, b) << '\n';
+}
+
+void brute(int) {}
+
+signed main() {
+    setIO();
+    precompute();
+    int t = 1;
+    cin >> t;
+    for (int _t = 1; _t <= t; _t++) {
+        // cout << "Case #" << _t << ": ";
+        solve(_t);
+        // brute(_t);
+    }
+    return 0;
+}
+
+/* stuff you should look for
+ * int overflow, array bounds
+ * special cases (n = 1?)
+ * do smth instead of nothing and stay organized
+ * WRITE STUFF DOWN
+ * DON'T GET STUCK ON ONE APPROACH
+ */
