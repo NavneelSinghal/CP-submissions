83f9d4809f21812f717f288345c8e4e18f81cfd1
Navneel Singhal <navneel.singhal@ymail.com>
2021-03-02 11:51:00 +0530

    Add solution for problem `E - Fib-tree` Link: https://codeforces.com/contest/1491/problem/E Tags: brute force, dfs and similar, divide and conquer, number theory, trees, *2400 Ref: https://codeforces.com/contest/1491/submission/108852073

--- codeforces/1491/E.cpp
+++ codeforces/1491/E.cpp
@@ -331,11 +331,10 @@ using namespace IO;
 using mint = Modular<mod>;
 
 void solve(int) {
-    
     int n;
     read_int(n);
     vector<vector<int>> g(n);
-    
+
     for (int i = 1; i < n; ++i) {
         int u, v;
         read_int(u, v);
@@ -343,83 +342,84 @@ void solve(int) {
         g[u].push_back(v);
         g[v].push_back(u);
     }
-    
+
     const int N = 46;
     vector<int> fib(N);
     fib[0] = 1;
     fib[1] = 1;
-    for (int i = 2; i < N; ++i)
-        fib[i] = fib[i - 1] + fib[i - 2];
+    for (int i = 2; i < N; ++i) fib[i] = fib[i - 1] + fib[i - 2];
 
     // call iff n > 1 else memory alloc issues
-    function<bool(vector<vector<int>>&, int)> works = [&](vector<vector<int>>&g, int f) {
-        if (f == 0 || f == 1) return true;
-        int n = g.size();
-        vector<int> siz(n), par(n, -1);
-        function<void(int, int)> get_size = [&](int v, int p) {
-            siz[v] = 1;
-            par[v] = p;
-            for (auto u : g[v]) {
-                if (u != p) {
-                    get_size(u, v);
-                    siz[v] += siz[u];
+    function<bool(vector<vector<int>> &, int)> works =
+        [&](vector<vector<int>> &g, int &&f) {
+            if (f == 0 || f == 1) return true;
+            int n = g.size();
+            vector<int> siz(n), par(n, -1);
+            function<void(int, int)> get_size = [&](int&& v, int&& p) {
+                siz[v] = 1;
+                par[v] = p;
+                for (auto &&u : g[v]) {
+                    if (u != p) {
+                        get_size(u, v);
+                        siz[v] += siz[u];
+                    }
+                }
+            };
+            get_size(0, -1);
+            int fib_l = fib[f - 1], fib_r = fib[f - 2];
+            for (int i = 0; i < n; ++i) {
+                if (siz[i] == fib_l || siz[i] == fib_r) {
+                    int fl = f - 1, fr = f - 2;
+                    if (siz[i] == fib_r) swap(fl, fr);
+                    // create left and right graphs
+                    vector<int> subtree(n);
+                    function<void(int, int)> dfs = [&](int v, int p) {
+                        subtree[v] = 1;
+                        for (auto &&u : g[v]) {
+                            if (u != p) {
+                                dfs(u, v);
+                            }
+                        }
+                    };
+                    dfs(i, par[i]);
+                    vector<int> map_left(n, -1), map_right(n, -1);
+                    int left_idx = 0, right_idx = 0;
+                    for (int i = 0; i < n; ++i) {
+                        if (subtree[i]) {
+                            map_left[i] = left_idx++;
+                        } else {
+                            map_right[i] = right_idx++;
+                        }
+                    }
+                    vector<vector<int>> gl(left_idx), gr(right_idx);
+                    for (int i = 0; i < n; ++i) {
+                        if (subtree[i]) {
+                            auto &adj_i = gl[map_left[i]];
+                            for (auto u : g[i]) {
+                                if (subtree[u]) {
+                                    adj_i.push_back(map_left[u]);
+                                }
+                            }
+                        } else {
+                            auto &adj_i = gr[map_right[i]];
+                            for (auto u : g[i]) {
+                                if (!subtree[u]) {
+                                    adj_i.push_back(map_right[u]);
+                                }
+                            }
+                        }
+                    }
+                    vector<int>().swap(map_left);
+                    vector<int>().swap(map_right);
+                    vector<int>().swap(subtree);
+                    vector<int>().swap(siz);
+                    vector<int>().swap(par);
+                    vector<vector<int>>().swap(g);
+                    return works(gl, fl) && works(gr, fr);
                 }
             }
+            return false;
         };
-        get_size(0, -1);
-        for (int i = 0; i < n; ++i) {
-            if (siz[i] == fib[f - 1] || siz[i] == fib[f - 2]) {
-                int fl = f - 1, fr = f - 2;
-                if (siz[i] == fib[f - 2]) swap(fl, fr);
-                // create left and right graphs
-                vector<int> subtree(n);
-                function<void(int, int)> dfs = [&](int v, int p) {
-                    subtree[v] = 1;
-                    for (auto u : g[v]) {
-                        if (u != p) {
-                            dfs(u, v);
-                        }
-                    }
-                };
-                dfs(i, par[i]);
-                vector<int> map_left(n, -1), map_right(n, -1);
-                int left_idx = 0, right_idx = 0;
-                for (int i = 0; i < n; ++i) {
-                    if (subtree[i]) {
-                        map_left[i] = left_idx++;
-                    } else {
-                        map_right[i] = right_idx++;
-                    }
-                }
-                vector<vector<int>> gl(left_idx), gr(right_idx);
-                for (int i = 0; i < n; ++i) {
-                    if (subtree[i]) {
-                        auto &adj_i = gl[map_left[i]];
-                        for (auto u : g[i]) {
-                            if (subtree[u]) {
-                                adj_i.push_back(map_left[u]);
-                            }
-                        }
-                    } else {
-                        auto &adj_i = gr[map_right[i]];
-                        for (auto u : g[i]) {
-                            if (!subtree[u]) {
-                                adj_i.push_back(map_right[u]);
-                            }
-                        }
-                    }
-                }
-                vector<int>().swap(map_left);
-                vector<int>().swap(map_right);
-                vector<int>().swap(subtree);
-                vector<int>().swap(siz);
-                vector<int>().swap(par);
-                vector<vector<int>>().swap(g);
-                return works(gl, fl) && works(gr, fr);
-            }
-        }
-        return false;
-    };
 
     if (n == 1) {
         write_str("YES\n");
@@ -464,5 +464,4 @@ signed main() {
  * WRITE STUFF DOWN
  * DON'T GET STUCK ON ONE APPROACH
  */
- 
 
