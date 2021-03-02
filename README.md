564d57dfbcccf1e1f8353d823893495d1353a94c
Navneel Singhal <navneel.singhal@ymail.com>
2021-03-02 23:33:00 +0530

    Add solution for problem `E - A-Z Graph` Link: https://codeforces.com/contest/1494/problem/E Tags: constructive algorithms, data structures, graphs Ref: https://codeforces.com/contest/1494/submission/108952774

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ A repository to keep track of problem solving practice, containing solutions fro
 | # | Title | Solution | Tags | Submitted |
 |---| ----- | -------- | ---- | --------- |
 892 | [E - Fib-tree](https://codeforces.com/contest/1491/problem/E) | [GNU C++17 (64)](./codeforces/1491/E.cpp) | `brute force` `dfs and similar` `divide and conquer` `number theory` `trees` `*2400` | Mar/03/2021 01:08 | 
-891 | [E - A-Z Graph](https://codeforces.com/contest/1494/problem/E) | [GNU C++17 (64)](./codeforces/1494/E.cpp) | `constructive algorithms` `data structures` `graphs` | Mar/02/2021 23:23 | 
+891 | [E - A-Z Graph](https://codeforces.com/contest/1494/problem/E) | [GNU C++17 (64)](./codeforces/1494/E.cpp) | `constructive algorithms` `data structures` `graphs` | Mar/02/2021 23:33 | 
 890 | [D - Dogeforces](https://codeforces.com/contest/1494/problem/D) | [GNU C++17 (64)](./codeforces/1494/D.cpp) | `constructive algorithms` `dsu` `greedy` `trees` | Mar/02/2021 22:57 | 
 889 | [C - 1D Sokoban](https://codeforces.com/contest/1494/problem/C) | [GNU C++17 (64)](./codeforces/1494/C.cpp) | `binary search` `implementation` `two pointers` | Mar/02/2021 22:28 | 
 888 | [B - Berland Crossword](https://codeforces.com/contest/1494/problem/B) | [GNU C++17 (64)](./codeforces/1494/B.cpp) | `binary search` `bitmasks` `brute force` `implementation` | Mar/02/2021 21:28 | 
--- codeforces/1494/E.cpp
+++ codeforces/1494/E.cpp
@@ -129,211 +129,12 @@ constexpr int mod = 1e9 + 7;
 constexpr int64_t linf = 3e18;
 constexpr long double pi = acosl(-1.0);
 
-[[nodiscard]] int64_t pwr_mod(int64_t a, int64_t n, int mod = ::mod) {
-    int64_t ans = 1;
-    while (n) {
-        if (n & 1) ans = (ans * a) % mod;
-        a = (a * a) % mod;
-        n >>= 1;
-    }
-    return ans;
-}
-
-// when using integers, keep overflow in mind
-template <typename T>
-T pwr(T a, int64_t n) {
-    T ans(1);
-    while (n) {
-        if (n & 1) ans *= a;
-        if (n > 1) a *= a;
-        n >>= 1;
-    }
-    return ans;
-}
-
 void precompute() {}
-
-// modular int library
-template <int32_t MOD = 998'244'353>
-struct Modular {
-    int32_t value;
-    static const int32_t MOD_value = MOD;
-
-    Modular(long long v = 0) {
-        value = v % MOD;
-        if (value < 0) value += MOD;
-    }
-    Modular(long long a, long long b) : value(0) {
-        *this += a;
-        *this /= b;
-    }
-
-    Modular &operator+=(Modular const &b) {
-        value += b.value;
-        if (value >= MOD) value -= MOD;
-        return *this;
-    }
-    Modular &operator-=(Modular const &b) {
-        value -= b.value;
-        if (value < 0) value += MOD;
-        return *this;
-    }
-    Modular &operator*=(Modular const &b) {
-        value = (long long)value * b.value % MOD;
-        return *this;
-    }
-
-    friend Modular mexp(Modular a, long long e) {
-        Modular res = 1;
-        while (e) {
-            if (e & 1) res *= a;
-            a *= a;
-            e >>= 1;
-        }
-        return res;
-    }
-    friend Modular inverse(Modular a) { return mexp(a, MOD - 2); }
-
-    Modular &operator/=(Modular const &b) { return *this *= inverse(b); }
-    friend Modular operator+(Modular a, Modular const b) { return a += b; }
-    friend Modular operator-(Modular a, Modular const b) { return a -= b; }
-    friend Modular operator-(Modular const a) { return 0 - a; }
-    friend Modular operator*(Modular a, Modular const b) { return a *= b; }
-    friend Modular operator/(Modular a, Modular const b) { return a /= b; }
-    friend std::ostream &operator<<(std::ostream &os, Modular const &a) {
-        return os << a.value;
-    }
-    friend bool operator==(Modular const &a, Modular const &b) {
-        return a.value == b.value;
-    }
-    friend bool operator!=(Modular const &a, Modular const &b) {
-        return a.value != b.value;
-    }
-};
-
-namespace IO {
-const int BUFFER_SIZE = 1 << 15;
-
-char input_buffer[BUFFER_SIZE];
-size_t input_pos = 0, input_len = 0;
-
-char output_buffer[BUFFER_SIZE];
-int output_pos = 0;
-
-char number_buffer[100];
-uint8_t lookup[100];
-
-void _update_input_buffer() {
-    input_len = fread(input_buffer, sizeof(char), BUFFER_SIZE, stdin);
-    input_pos = 0;
-
-    if (input_len == 0) input_buffer[0] = EOF;
-}
-
-inline char next_char(bool advance = true) {
-    if (input_pos >= input_len) _update_input_buffer();
-
-    return input_buffer[advance ? input_pos++ : input_pos];
-}
-
-inline bool isspace(char c) {
-    return (unsigned char)(c - '\t') < 5 || c == ' ';
-}
-
-inline void read_char(char &c) {
-    while (isspace(next_char(false))) next_char();
-
-    c = next_char();
-}
-
-template <typename T>
-inline void read_int(T &number) {
-    bool negative = false;
-    number = 0;
-
-    while (!isdigit(next_char(false)))
-        if (next_char() == '-') negative = true;
-
-    do {
-        number = 10 * number + (next_char() - '0');
-    } while (isdigit(next_char(false)));
-
-    if (negative) number = -number;
-}
-
-template <typename T, typename... Args>
-inline void read_int(T &number, Args &... args) {
-    read_int(number);
-    read_int(args...);
-}
-
-inline void read_str(string &str) {
-    while (isspace(next_char(false))) next_char();
-
-    str.clear();
-
-    do {
-        str += next_char();
-    } while (!isspace(next_char(false)));
-}
-
-void _flush_output() {
-    fwrite(output_buffer, sizeof(char), output_pos, stdout);
-    output_pos = 0;
-}
-
-inline void write_char(char c) {
-    if (output_pos == BUFFER_SIZE) _flush_output();
-
-    output_buffer[output_pos++] = c;
-}
-
-template <typename T>
-inline void write_int(T number, char after = '\0') {
-    if (number < 0) {
-        write_char('-');
-        number = -number;
-    }
-
-    int length = 0;
-
-    while (number >= 10) {
-        uint8_t lookup_value = lookup[number % 100];
-        number /= 100;
-        number_buffer[length++] = char((lookup_value & 15) + '0');
-        number_buffer[length++] = char((lookup_value >> 4) + '0');
-    }
-
-    if (number != 0 || length == 0) write_char(char(number + '0'));
-
-    for (int i = length - 1; i >= 0; i--) write_char(number_buffer[i]);
-
-    if (after) write_char(after);
-}
-
-inline void write_str(const string &str, char after = '\0') {
-    for (char c : str) write_char(c);
-
-    if (after) write_char(after);
-}
-
-void IOinit() {
-    // Make sure _flush_output() is called at the end of the program.
-    bool exit_success = atexit(_flush_output) == 0;
-    assert(exit_success);
-
-    for (int i = 0; i < 100; i++) lookup[i] = uint8_t((i / 10 << 4) + i % 10);
-}
-}  // namespace IO
-
-using namespace IO;
-
-using mint = Modular<mod>;
 
 void solve(int) {
     int n, m;
     cin >> n >> m;
-    map<pair<int, int>, char> edges;
+    unordered_map<pair<int, int>, char, pair_hash> edges;
     int odd = 0, even = 0;
     for (int i = 0; i < m; ++i) {
         char c;
@@ -342,7 +143,7 @@ void solve(int) {
             int u, v;
             char d;
             cin >> u >> v >> d;
-            edges.emplace(make_pair(u, v), d);
+            edges[make_pair(u, v)] = d;
             auto it_rev = edges.find(make_pair(v, u));
             if (it_rev != edges.end()) {
                 ++odd;
@@ -357,7 +158,7 @@ void solve(int) {
                 --odd;
                 if (it_rev->second == it->second) --even;
             }
-            edges.erase(it);
+            edges.erase(make_pair(u, v));
         } else {
             int k;
             cin >> k;
