2171dce8366f113ceda4d52b8bedad7e531e3389
Navneel Singhal <navneel.singhal@ymail.com>
2021-03-16 00:26:00 +0530

    Add solution for problem `E - Addition on Segments` Link: https://codeforces.com/contest/981/problem/E Tags: bitmasks, data structures, divide and conquer, dp, *2200 Ref: https://codeforces.com/contest/981/submission/110063979

--- codeforces/981/E.cpp
+++ codeforces/981/E.cpp
@@ -206,127 +206,11 @@ template <typename T>
 
 void precompute() {}
 
-namespace IO {
-const int BUFFER_SIZE = 1 << 15;
-
-char input_buffer[BUFFER_SIZE];
-size_t input_pos = 0, input_len = 0;
-
-char output_buffer[BUFFER_SIZE];
-int output_pos = 0;
-
-char number_buffer[100];
-uint8_t lookup[100];
-
-void _update_input_buffer() {
-    input_len = fread(input_buffer, sizeof(char), BUFFER_SIZE, stdin);
-    input_pos = 0;
-
-    if (input_len == 0) input_buffer[0] = EOF;
-}
-
-inline char next_char(bool advance = true) {
-    if (input_pos >= input_len) _update_input_buffer();
-
-    return input_buffer[advance ? input_pos++ : input_pos];
-}
-
-inline bool isspace(char c) {
-    return (unsigned char)(c - '\t') < 5 || c == ' ';
-}
-
-inline void read_char(char &c) {
-    while (isspace(next_char(false))) next_char();
-
-    c = next_char();
-}
-
-template <typename T>
-inline void read_int(T &number) {
-    bool negative = false;
-    number = 0;
-
-    while (!isdigit(next_char(false)))
-        if (next_char() == '-') negative = true;
-
-    do {
-        number = 10 * number + (next_char() - '0');
-    } while (isdigit(next_char(false)));
-
-    if (negative) number = -number;
-}
-
-template <typename T, typename... Args>
-inline void read_int(T &number, Args &... args) {
-    read_int(number);
-    read_int(args...);
-}
-
-inline void read_str(string &str) {
-    while (isspace(next_char(false))) next_char();
-
-    str.clear();
-
-    do {
-        str += next_char();
-    } while (!isspace(next_char(false)));
-}
-
-void _flush_output() {
-    fwrite(output_buffer, sizeof(char), output_pos, stdout);
-    output_pos = 0;
-}
-
-inline void write_char(char c) {
-    if (output_pos == BUFFER_SIZE) _flush_output();
-
-    output_buffer[output_pos++] = c;
-}
-
-template <typename T>
-inline void write_int(T number, char after = '\0') {
-    if (number < 0) {
-        write_char('-');
-        number = -number;
-    }
-
-    int length = 0;
-
-    while (number >= 10) {
-        uint8_t lookup_value = lookup[number % 100];
-        number /= 100;
-        number_buffer[length++] = char((lookup_value & 15) + '0');
-        number_buffer[length++] = char((lookup_value >> 4) + '0');
-    }
-
-    if (number != 0 || length == 0) write_char(char(number + '0'));
-
-    for (int i = length - 1; i >= 0; i--) write_char(number_buffer[i]);
-
-    if (after) write_char(after);
-}
-
-inline void write_str(const string &str, char after = '\0') {
-    for (char c : str) write_char(c);
-
-    if (after) write_char(after);
-}
-
-void IOinit() {
-    // Make sure _flush_output() is called at the end of the program.
-    bool exit_success = atexit(_flush_output) == 0;
-    assert(exit_success);
-
-    for (int i = 0; i < 100; i++) lookup[i] = uint8_t((i / 10 << 4) + i % 10);
-}
-}  // namespace IO
-
 constexpr int N = 10005;
 
 void solve(int) {
-    IO::IOinit();
     int n, q;
-    IO::read_int(n, q);
+    cin >> n >> q;
     vector<vector<int>> t(N << 1);
     auto insert = [&](int l, int r, int x) {
         for (l += N - 1, r += N; l < r; l >>= 1, r >>= 1) {
@@ -336,7 +220,7 @@ void solve(int) {
     };
     for (int i = 0; i < q; ++i) {
         int l, r, x;
-        IO::read_int(l, r, x);
+        cin >> l >> r >> x;
         insert(l, r, x);
     }
     bitset<N> x, y;
@@ -353,10 +237,10 @@ void solve(int) {
     int ans = 0;
     for (int i = 1; i <= n; ++i)
         if (y[i]) ans++;
-    IO::write_int(ans, '\n');
+    cout << ans << '\n';
     for (int i = 1; i <= n; ++i)
-        if (y[i]) IO::write_int(i, ' ');
-    IO::write_char('\n');
+        if (y[i]) cout << i << ' ';
+    cout << '\n';
 }
 
 void brute(int) {}
