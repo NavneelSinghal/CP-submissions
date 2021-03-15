76614a6268a88d0bbf35a500bde2b2d5b4d3f0ff
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2021-03-16 00:31:00 +0530

    Add solution for problem `E - Addition on Segments` Link: https://codeforces.com/contest/981/problem/E Tags: bitmasks, data structures, divide and conquer, dp, *2200 Ref: https://codeforces.com/contest/981/submission/110064214

---
+++ codeforces/981/E.cpp
@@ -0,0 +1,375 @@
+// clang-format off
+
+/* optimizing pragmas begin - comment for fast compilation */
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx")
+// #pragma GCC optimize("unroll-loops")
+/* optimizing pragmas end */
+
+/*
+ * 64-bit bitset that might break stuff
+ */
+#include <string>
+#include <bits/cxxabi_forced.h>
+#include <bits/functexcept.h>
+#include <bits/functional_hash.h>
+#include <iosfwd>
+#pragma push_macro("__SIZEOF_LONG__")
+#pragma push_macro("__cplusplus")
+#define __SIZEOF_LONG__ __SIZEOF_LONG_LONG__
+#define unsigned unsigned long
+#define __cplusplus 201102L
+#define __builtin_popcountl __builtin_popcountll
+#define __builtin_ctzl __builtin_ctzll
+#include <bitset>
+#pragma pop_macro("__cplusplus")
+#pragma pop_macro("__SIZEOF_LONG__")
+#undef unsigned
+#undef __builtin_popcountl
+#undef __builtin_ctzl
+/* 64-bit bitset ends */
+
+/*
+ * usual includes
+ */
+#include "bits/stdc++.h"
+#include "ext/pb_ds/assoc_container.hpp"
+#include "ext/pb_ds/tree_policy.hpp"
+// #include <ext/rope>
+
+/* includes end */
+
+/*
+ * namespaces
+ */
+using namespace std;
+using namespace __gnu_pbds;
+// using namespace __gnu_cxx;
+
+using ll = long long;
+#define unordered_map gp_hash_table
+
+/* to avoid unused variable warnings */
+template <typename T>
+void ignore_unused(const T &) {}
+
+/*
+ * debugging template
+ */
+#ifdef DEBUG
+#define debug(args...)                               \
+    {                                                \
+        std::string _s = #args;                      \
+        replace(_s.begin(), _s.end(), ',', ' ');     \
+        std::stringstream _ss(_s);                   \
+        std::istream_iterator<std::string> _it(_ss); \
+        err(_it, args);                              \
+    }
+#define print_container(v)          \
+    {                               \
+        bool first = true;          \
+        os << "[";                  \
+        for (auto x : v) {          \
+            if (!first) os << ", "; \
+            os << x;                \
+            first = false;          \
+        }                           \
+        return os << "]";           \
+    }
+void err(std::istream_iterator<std::string> it) { ignore_unused(it); }
+template <typename T, typename... Args>
+void err(std::istream_iterator<std::string> it, T a, Args... args) {
+    std::cerr << *it << " = " << a << std::endl;
+    err(++it, args...);
+}
+// clang-format off
+template <typename T1, typename T2> inline std::ostream& operator << (std::ostream& os, const std::pair<T1, T2>& p) { return os << "(" << p.first << "," << p.second << ")"; }
+template <typename T> inline std::ostream &operator << (std::ostream & os, const std::vector<T>& v) { print_container(v); }
+template <typename T> inline std::ostream &operator << (std::ostream & os, const std::set<T>& v) { print_container(v); }
+template <typename T1, typename T2> inline std::ostream &operator << (std::ostream & os, const std::map<T1, T2>& v) { print_container(v); }
+template <typename T1, typename T2, class C> inline std::ostream &operator << (std::ostream & os, const unordered_map<T1, T2, C>& v) { print_container(v); }
+template <typename T, class C> inline std::ostream &operator << (std::ostream & os, const unordered_set<T, C>& v) { print_container(v); }
+template <typename T1, typename T2> inline std::ostream &operator << (std::ostream & os, const std::multimap<T1, T2>& v) { print_container(v); }
+template <typename T> inline std::ostream &operator << (std::ostream & os, const std::multiset<T>& v) { print_container(v); }
+#else
+#define debug(args...) 0
+#endif
+
+/* utility functions */
+template <typename X, typename Y> X &remin(X &x, const Y &y) { return x = (y < x) ? y : x; }
+template <typename X, typename Y> X &remax(X &x, const Y &y) { return x = (x < y) ? y : x; }
+template <typename X, typename Y> [[nodiscard]] bool ckmin(X &x, const Y &y) { return (y < x) ? (x = y, 1) : 0; }
+template <typename X, typename Y> [[nodiscard]] bool ckmax(X &x, const Y &y) { return (x < y) ? (x = y, 1) : 0; }
+template <typename T> inline T sq(T a) { return a * a; }
+template <typename T> inline T sq_dist(pair<T, T> &a, pair<T, T> &b) { return sq(a.first - b.first) + sq(a.second - b.second); }
+template <typename T> inline long double dist(pair<T, T> &a, pair<T, T> &b) { return sqrtl(sq_dist(a, b)); }
+inline long long isqrt(long long n) {
+    long long sq = (long long)sqrtl((long double)n) - 2;
+    sq = max(sq, 0LL);
+    while (sq * sq < n) sq++;
+    if ((sq * sq) == n) return sq;
+    return sq - 1;
+}
+inline bool is_sq(int n) { int w = (int)isqrt(n); return w * w == n; }
+
+/* good hash function */
+struct custom_hash {
+    // http://xorshift.di.unimi.it/splitmix64.c
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM =
+            chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+struct pair_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM =
+            chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+    size_t operator()(pair<int, int> p) const {
+        static const uint64_t FIXED_RANDOM =
+            chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(p.first * 31 + p.second + FIXED_RANDOM);
+    }
+};
+
+/* y_combinator to avoid writing out the whole signature */
+template <class Fun>
+class y_combinator_result {
+    Fun fun_;
+   public:
+    template <class T>
+    explicit y_combinator_result(T &&fun) : fun_(std::forward<T>(fun)) {}
+    template <class... Args>
+    decltype(auto) operator()(Args &&... args) {
+        return fun_(std::ref(*this), std::forward<Args>(args)...);
+    }
+};
+template <class Fun>
+decltype(auto) y_combinator(Fun &&fun) {
+    return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun));
+}
+
+/* input/output stuff */
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin.exceptions(cin.failbit);
+    cout << setprecision(10) << fixed;
+    if (name.size() == 0) return;
+    FILE *fin = freopen((name + ".in").c_str(), "r", stdin);
+    FILE *fout = freopen((name + ".out").c_str(), "w", stdout);
+    ignore_unused(fin); ignore_unused(fout);
+}
+
+// clang-format on
+
+// constexpr int mod = 998244353;
+constexpr int mod = 1e9 + 7;
+constexpr int64_t linf = 3e18;
+const long double pi = acosl(-1.0);
+constexpr long double eps = 1e-9;
+
+[[nodiscard]] int64_t pwr_mod(int64_t a, int64_t n, int mod = ::mod) {
+    int64_t ans = 1;
+    while (n) {
+        if (n & 1) ans = (ans * a) % mod;
+        a = (a * a) % mod;
+        n >>= 1;
+    }
+    return ans;
+}
+
+// when using integers, keep overflow in mind
+template <typename T>
+[[nodiscard]] T pwr(T a, int64_t n) {
+    T ans(1);
+    while (n) {
+        if (n & 1) ans *= a;
+        if (n > 1) a *= a;
+        n >>= 1;
+    }
+    return ans;
+}
+
+void precompute() {}
+
+namespace IO {
+const int BUFFER_SIZE = 1 << 15;
+
+char input_buffer[BUFFER_SIZE];
+size_t input_pos = 0, input_len = 0;
+
+char output_buffer[BUFFER_SIZE];
+int output_pos = 0;
+
+char number_buffer[100];
+uint8_t lookup[100];
+
+void _update_input_buffer() {
+    input_len = fread(input_buffer, sizeof(char), BUFFER_SIZE, stdin);
+    input_pos = 0;
+
+    if (input_len == 0) input_buffer[0] = EOF;
+}
+
+inline char next_char(bool advance = true) {
+    if (input_pos >= input_len) _update_input_buffer();
+
+    return input_buffer[advance ? input_pos++ : input_pos];
+}
+
+inline bool isspace(char c) {
+    return (unsigned char)(c - '\t') < 5 || c == ' ';
+}
+
+inline void read_char(char &c) {
+    while (isspace(next_char(false))) next_char();
+
+    c = next_char();
+}
+
+template <typename T>
+inline void read_int(T &number) {
+    bool negative = false;
+    number = 0;
+
+    while (!isdigit(next_char(false)))
+        if (next_char() == '-') negative = true;
+
+    do {
+        number = 10 * number + (next_char() - '0');
+    } while (isdigit(next_char(false)));
+
+    if (negative) number = -number;
+}
+
+template <typename T, typename... Args>
+inline void read_int(T &number, Args &... args) {
+    read_int(number);
+    read_int(args...);
+}
+
+inline void read_str(string &str) {
+    while (isspace(next_char(false))) next_char();
+
+    str.clear();
+
+    do {
+        str += next_char();
+    } while (!isspace(next_char(false)));
+}
+
+void _flush_output() {
+    fwrite(output_buffer, sizeof(char), output_pos, stdout);
+    output_pos = 0;
+}
+
+inline void write_char(char c) {
+    if (output_pos == BUFFER_SIZE) _flush_output();
+
+    output_buffer[output_pos++] = c;
+}
+
+template <typename T>
+inline void write_int(T number, char after = '\0') {
+    if (number < 0) {
+        write_char('-');
+        number = -number;
+    }
+
+    int length = 0;
+
+    while (number >= 10) {
+        uint8_t lookup_value = lookup[number % 100];
+        number /= 100;
+        number_buffer[length++] = char((lookup_value & 15) + '0');
+        number_buffer[length++] = char((lookup_value >> 4) + '0');
+    }
+
+    if (number != 0 || length == 0) write_char(char(number + '0'));
+
+    for (int i = length - 1; i >= 0; i--) write_char(number_buffer[i]);
+
+    if (after) write_char(after);
+}
+
+inline void write_str(const string &str, char after = '\0') {
+    for (char c : str) write_char(c);
+
+    if (after) write_char(after);
+}
+
+void IOinit() {
+    // Make sure _flush_output() is called at the end of the program.
+    bool exit_success = atexit(_flush_output) == 0;
+    assert(exit_success);
+
+    for (int i = 0; i < 100; i++) lookup[i] = uint8_t((i / 10 << 4) + i % 10);
+}
+}  // namespace IO
+
+constexpr int N = 10005;
+
+void solve(int) {
+    IO::IOinit();
+    int n, q;
+    IO::read_int(n, q);
+    vector<vector<int>> t(N << 1);
+    auto insert = [&](int& l, int& r, int& x) {
+        for (l += N - 1, r += N; l < r; l >>= 1, r >>= 1) {
+            if (l & 1) t[l++].push_back(x);
+            if (r & 1) t[--r].push_back(x);
+        }
+    };
+    for (int i = 0; i < q; ++i) {
+        int l, r, x;
+        IO::read_int(l, r, x);
+        insert(l, r, x);
+    }
+    bitset<N> x, y;
+    x[0] = 1;
+    y_combinator([&](auto rec, int v, bitset<N> w) -> void {
+        for (auto shift : t[v]) w |= (w << shift);
+        if (v >= N)
+            y |= w;
+        else {
+            rec(v << 1, w);
+            rec((v << 1) | 1, w);
+        }
+    })(1, x);
+    int ans = 0;
+    for (int i = 1; i <= n; ++i)
+        if (y[i]) ans++;
+    IO::write_int(ans, '\n');
+    for (int i = 1; i <= n; ++i)
+        if (y[i]) IO::write_int(i, ' ');
+    IO::write_char('\n');
+}
+
+void brute(int) {}
+
+signed main() {
+    setIO();
+    precompute();
+    int t = 1;
+    // cin >> t;
+    for (int _t = 1; _t <= t; _t++) {
+        // cout << "Case #" << _t << ": ";
+        solve(_t);
+        // brute(_t);
+    }
+    return 0;
+}
