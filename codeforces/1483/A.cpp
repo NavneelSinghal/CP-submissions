d4c479fb5270336285fbf454c84eeb1143c1018d
Navneel Singhal <navneel.singhal@ymail.com>
2021-03-24 00:14:00 +0530

    Add solution for problem `A - Basic Diplomacy` Link: https://codeforces.com/contest/1483/problem/A Tags: constructive algorithms, greedy, implementation, *1600 Ref: https://codeforces.com/contest/1483/submission/110840849

---
+++ codeforces/1483/A.cpp
@@ -0,0 +1,396 @@
+// clang-format off
+
+/* optimizing pragmas begin - comment for fast compilation */
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx")
+// #pragma GCC optimize("unroll-loops")
+/* optimizing pragmas end */
+
+/*
+ * 64-bit bitset that might break stuff
+ */
+// #include <string>
+// #include <bits/cxxabi_forced.h>
+// #include <bits/functexcept.h>
+// #include <bits/functional_hash.h>
+// #include <iosfwd>
+// #pragma push_macro("__SIZEOF_LONG__")
+// #pragma push_macro("__cplusplus")
+// #define __SIZEOF_LONG__ __SIZEOF_LONG_LONG__
+// #define unsigned unsigned long
+// #define __cplusplus 201102L
+// #define __builtin_popcountl __builtin_popcountll
+// #define __builtin_ctzl __builtin_ctzll
+// #include <bitset>
+// #pragma pop_macro("__cplusplus")
+// #pragma pop_macro("__SIZEOF_LONG__")
+// #undef unsigned
+// #undef __builtin_popcountl
+// #undef __builtin_ctzl
+/* 64-bit bitset ends */
+
+/*
+ * usual includes
+ */
+#include "bits/stdc++.h"
+#include "ext/pb_ds/assoc_container.hpp"
+#include "ext/pb_ds/tree_policy.hpp"
+// #include <ext/rope>
+
+/* includes end */
+
+/*
+ * namespaces
+ */
+using namespace std;
+using namespace __gnu_pbds;
+// using namespace __gnu_cxx;
+
+using ll = long long;
+#define unordered_map gp_hash_table
+
+/* to avoid unused variable warnings */
+template <typename T>
+void ignore_unused(const T &) {}
+
+/*
+ * debugging template
+ */
+#ifdef DEBUG
+#define debug(args...)                               \
+    {                                                \
+        std::string _s = #args;                      \
+        replace(_s.begin(), _s.end(), ',', ' ');     \
+        std::stringstream _ss(_s);                   \
+        std::istream_iterator<std::string> _it(_ss); \
+        err(_it, args);                              \
+    }
+#define print_container(v)          \
+    {                               \
+        bool first = true;          \
+        os << "[";                  \
+        for (auto x : v) {          \
+            if (!first) os << ", "; \
+            os << x;                \
+            first = false;          \
+        }                           \
+        return os << "]";           \
+    }
+void err(std::istream_iterator<std::string> it) { ignore_unused(it); }
+template <typename T, typename... Args>
+void err(std::istream_iterator<std::string> it, T a, Args... args) {
+    std::cerr << *it << " = " << a << std::endl;
+    err(++it, args...);
+}
+// clang-format off
+template <typename T1, typename T2> inline std::ostream& operator << (std::ostream& os, const std::pair<T1, T2>& p) { return os << "(" << p.first << "," << p.second << ")"; }
+template <typename T> inline std::ostream &operator << (std::ostream & os, const std::vector<T>& v) { print_container(v); }
+template <typename T> inline std::ostream &operator << (std::ostream & os, const std::set<T>& v) { print_container(v); }
+template <typename T1, typename T2> inline std::ostream &operator << (std::ostream & os, const std::map<T1, T2>& v) { print_container(v); }
+template <typename T1, typename T2, class C> inline std::ostream &operator << (std::ostream & os, const unordered_map<T1, T2, C>& v) { print_container(v); }
+template <typename T, class C> inline std::ostream &operator << (std::ostream & os, const unordered_set<T, C>& v) { print_container(v); }
+template <typename T1, typename T2> inline std::ostream &operator << (std::ostream & os, const std::multimap<T1, T2>& v) { print_container(v); }
+template <typename T> inline std::ostream &operator << (std::ostream & os, const std::multiset<T>& v) { print_container(v); }
+#else
+#define debug(args...) 0
+#endif
+
+/* utility functions */
+template <typename X, typename Y> X &remin(X &x, const Y &y) { return x = (y < x) ? y : x; }
+template <typename X, typename Y> X &remax(X &x, const Y &y) { return x = (x < y) ? y : x; }
+template <typename X, typename Y> [[nodiscard]] bool ckmin(X &x, const Y &y) { return (y < x) ? (x = y, 1) : 0; }
+template <typename X, typename Y> [[nodiscard]] bool ckmax(X &x, const Y &y) { return (x < y) ? (x = y, 1) : 0; }
+template <typename T> inline T sq(T a) { return a * a; }
+template <typename T> inline T sq_dist(pair<T, T> &a, pair<T, T> &b) { return sq(a.first - b.first) + sq(a.second - b.second); }
+template <typename T> inline long double dist(pair<T, T> &a, pair<T, T> &b) { return sqrtl(sq_dist(a, b)); }
+inline long long isqrt(long long n) {
+    long long sq = (long long)sqrtl((long double)n) - 2;
+    sq = max(sq, 0LL);
+    while (sq * sq < n) sq++;
+    if ((sq * sq) == n) return sq;
+    return sq - 1;
+}
+inline bool is_sq(int n) { int w = (int)isqrt(n); return w * w == n; }
+
+/* good hash function */
+struct custom_hash {
+    // http://xorshift.di.unimi.it/splitmix64.c
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM =
+            chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+struct pair_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM =
+            chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+    size_t operator()(pair<int, int> p) const {
+        static const uint64_t FIXED_RANDOM =
+            chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(p.first * 31 + p.second + FIXED_RANDOM);
+    }
+};
+
+/* y_combinator to avoid writing out the whole signature */
+template <class Fun>
+class y_combinator_result {
+    Fun fun_;
+   public:
+    template <class T>
+    explicit y_combinator_result(T &&fun) : fun_(std::forward<T>(fun)) {}
+    template <class... Args>
+    decltype(auto) operator()(Args &&... args) {
+        return fun_(std::ref(*this), std::forward<Args>(args)...);
+    }
+};
+template <class Fun>
+decltype(auto) y_combinator(Fun &&fun) {
+    return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun));
+}
+
+/* input/output stuff */
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin.exceptions(cin.failbit);
+    cout << setprecision(10) << fixed;
+    if (name.size() == 0) return;
+    FILE *fin = freopen((name + ".in").c_str(), "r", stdin);
+    FILE *fout = freopen((name + ".out").c_str(), "w", stdout);
+    ignore_unused(fin); ignore_unused(fout);
+}
+
+// clang-format on
+
+// constexpr int mod = 998244353;
+constexpr int mod = 1e9 + 7;
+constexpr int64_t linf = 3e18;
+const long double pi = acosl(-1.0);
+constexpr long double eps = 1e-9;
+
+[[nodiscard]] int64_t pwr_mod(int64_t a, int64_t n, int mod = ::mod) {
+    int64_t ans = 1;
+    while (n) {
+        if (n & 1) ans = (ans * a) % mod;
+        a = (a * a) % mod;
+        n >>= 1;
+    }
+    return ans;
+}
+
+// when using integers, keep overflow in mind
+template <typename T>
+[[nodiscard]] T pwr(T a, int64_t n) {
+    T ans(1);
+    while (n) {
+        if (n & 1) ans *= a;
+        if (n > 1) a *= a;
+        n >>= 1;
+    }
+    return ans;
+}
+
+template <int32_t MOD = 998'244'353>
+struct Modular {
+    int32_t value;
+    static const int32_t MOD_value = MOD;
+
+    Modular(long long v = 0) {
+        value = v % MOD;
+        if (value < 0) value += MOD;
+    }
+    Modular(long long a, long long b) : value(0) {
+        *this += a;
+        *this /= b;
+    }
+
+    Modular &operator+=(Modular const &b) {
+        value += b.value;
+        if (value >= MOD) value -= MOD;
+        return *this;
+    }
+    Modular &operator-=(Modular const &b) {
+        value -= b.value;
+        if (value < 0) value += MOD;
+        return *this;
+    }
+    Modular &operator*=(Modular const &b) {
+        value = (long long)value * b.value % MOD;
+        return *this;
+    }
+
+    friend Modular mexp(Modular a, long long e) {
+        Modular res = 1;
+        while (e) {
+            if (e & 1) res *= a;
+            a *= a;
+            e >>= 1;
+        }
+        return res;
+    }
+    friend Modular inverse(Modular a) { return mexp(a, MOD - 2); }
+
+    Modular &operator/=(Modular const &b) { return *this *= inverse(b); }
+    friend Modular operator+(Modular a, Modular const b) { return a += b; }
+    friend Modular operator-(Modular a, Modular const b) { return a -= b; }
+    friend Modular operator-(Modular const a) { return 0 - a; }
+    friend Modular operator*(Modular a, Modular const b) { return a *= b; }
+    friend Modular operator/(Modular a, Modular const b) { return a /= b; }
+    friend std::ostream &operator<<(std::ostream &os, Modular const &a) {
+        return os << a.value;
+    }
+    friend bool operator==(Modular const &a, Modular const &b) {
+        return a.value == b.value;
+    }
+    friend bool operator!=(Modular const &a, Modular const &b) {
+        return a.value != b.value;
+    }
+};
+
+using mint = Modular<>;
+
+const int inf = 1e9;
+
+void precompute() {}
+
+struct FlowEdge {
+    int v, u;
+    long long cap, flow = 0;
+    FlowEdge(int v, int u, long long cap) : v(v), u(u), cap(cap) {}
+};
+
+struct Dinic {
+    const long long flow_inf = 1e18;
+    vector<FlowEdge> edges;
+    vector<vector<int>> adj;
+    int n, m = 0;
+    int s, t;
+    vector<int> level, ptr;
+    queue<int> q;
+
+    Dinic(int n) : n(n), s(-1), t(-1) {
+        adj.resize(n);
+        level.resize(n);
+        ptr.resize(n);
+    }
+
+    void add_edge(int v, int u, long long cap) {
+        edges.emplace_back(v, u, cap);
+        edges.emplace_back(u, v, 0);
+        adj[v].push_back(m);
+        adj[u].push_back(m + 1);
+        m += 2;
+    }
+
+    bool bfs() {
+        while (!q.empty()) {
+            int v = q.front();
+            q.pop();
+            for (int id : adj[v]) {
+                if (edges[id].cap - edges[id].flow < 1) continue;
+                if (level[edges[id].u] != -1) continue;
+                level[edges[id].u] = level[v] + 1;
+                q.push(edges[id].u);
+            }
+        }
+        return level[t] != -1;
+    }
+
+    long long dfs(int v, long long pushed) {
+        if (pushed == 0) return 0;
+        if (v == t) return pushed;
+        for (int &cid = ptr[v]; cid < (int)adj[v].size(); cid++) {
+            int id = adj[v][cid];
+            int u = edges[id].u;
+            if (level[v] + 1 != level[u] || edges[id].cap - edges[id].flow < 1)
+                continue;
+            long long tr = dfs(u, min(pushed, edges[id].cap - edges[id].flow));
+            if (tr == 0) continue;
+            edges[id].flow += tr;
+            edges[id ^ 1].flow -= tr;
+            return tr;
+        }
+        return 0;
+    }
+
+    long long flow(int s, int t) {
+        this->s = s;
+        this->t = t;
+        long long f = 0;
+        while (true) {
+            fill(level.begin(), level.end(), -1);
+            level[s] = 0;
+            q.push(s);
+            if (!bfs()) break;
+            fill(ptr.begin(), ptr.end(), 0);
+            while (long long pushed = dfs(s, flow_inf)) {
+                f += pushed;
+            }
+        }
+        return f;
+    }
+};
+
+void solve(int) {
+    int n, m;
+    cin >> n >> m;
+    Dinic d(n + m + 2);
+    for (int i = 0; i < m; ++i) {
+        int k;
+        cin >> k;
+        d.add_edge(0, i + 1, 1);
+        while (k--) {
+            int u;
+            cin >> u;
+            d.add_edge(i + 1, u + m, 1);
+        }
+    }
+    for (int i = 1; i <= n; ++i) {
+        d.add_edge(i + m, n + m + 1, (m + 1) / 2);
+    }
+    if (d.flow(0, n + m + 1) != m) {
+        cout << "NO\n";
+        return;
+    }
+    vector<int> chosen(m + 1, -1);
+    for (auto e : d.edges) {
+        if (e.u >= 1 && e.u <= m && e.v != 0 && e.flow) {
+            chosen[e.u] = e.v - m;
+        }
+    }
+    cout << "YES\n";
+    for (int i = 0; i < m; ++i) {
+        cout << chosen[i + 1] << ' ';
+    }
+    cout << '\n';
+}
+
+void brute(int) {}
+
+signed main() {
+    setIO();
+    precompute();
+    int t = 1;
+    cin >> t;
+    for (int _t = 1; _t <= t; _t++) {
+        // cout << "Case #" << _t << ": ";
+        solve(_t);
+        // brute(_t);
+    }
+    return 0;
+}
+
