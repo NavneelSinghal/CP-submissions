b0da8709bf822db48a2a12fa60a882197fd343c4
Navneel Singhal <navneel.singhal@ymail.com>
2021-04-20 15:36:00 +0530

    Add solution for problem `D - Cut and Stick` Link: https://codeforces.com/contest/1514/problem/D Tags: binary search, data structures, greedy, probabilities, sortings, *2000 Ref: https://codeforces.com/contest/1514/submission/113600556

--- codeforces/1514/D.cpp
+++ codeforces/1514/D.cpp
@@ -440,14 +440,6 @@ using namespace __gnu_pbds;
 
 /* main code starts */
 
-static char buf[450 << 20];
-void* operator new(size_t s) {
-	static size_t i = sizeof buf;
-	assert(s < i);
-	return (void*)&buf[i -= s];
-}
-void operator delete(void*) {}
-
 template <typename T>
 struct wavelet_tree {
     struct node {
