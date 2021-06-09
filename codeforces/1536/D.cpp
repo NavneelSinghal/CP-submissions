5c3c9d094dc9d5f1373b784a1fbfa7b545794fda
Navneel Singhal <navneel.singhal@ymail.com>
2021-06-09 18:26:00 +0530

    Add solution for problem `D - Omkar and Medians` Link: https://codeforces.com/contest/1536/problem/D Tags: data structures, greedy, implementation, *2000 Ref: https://codeforces.com/contest/1536/submission/118886532

---
+++ codeforces/1536/D.cpp
@@ -0,0 +1,58 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("avx,avx2,sse,sse2,sse3,sse4,popcnt")
+#include<bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+#define vec vector
+#define arr array
+#define pr pair
+#define tp tuple
+#define str string
+#define pb push_back
+#define eb emplace_back
+#define rep(i, a, b, inc) for (int i = int(a); i <= int(b); i += int(inc))
+#define repl(i, a, b, inc) for (ll i = ll(a); i <= ll(b); i += ll(inc))
+#define repd(i, a, b, inc) for (int i = int(a); i >= (b); i -= int(inc))
+#define repdl(i, a, b, inc) for (ll i = ll(a); i >= ll(b); i -= ll(inc))
+#define all(v) begin(v), end(v)
+#define each(x, v) for (auto&& x : v)
+#define each2(x, y, v) for (auto &&[x, y] : v)
+#define each3(x, y, z, v) for (auto &&[x, y, z] : v)
+
+const int mod = 1e9+7;
+
+int main(){
+ cin.tie(0)->sync_with_stdio(false);
+ int t; cin >> t; while (t--) {
+  int n; cin >> n;
+  int works = true;
+  set<int> s;
+  int x; cin >> x;
+  int old;
+  s.insert(x);
+  rep(i, 2, n, 1) {
+   old = x;
+   cin >> x;
+   if (old < x) {
+    auto it = s.upper_bound(old);
+    if (it != s.end() && (*it) < x) {
+      works = false;
+    }
+   } else if (old > x) {
+    auto it = s.lower_bound(old);
+    if (it != s.begin()) {
+     --it; if ((*it) > x) works = false;
+    }
+   }
+   s.insert(x);
+  }
+  if (works) {
+   cout << "yes\n";
+  } else {
+   cout << "no\n";
+  }
+ }
+}
