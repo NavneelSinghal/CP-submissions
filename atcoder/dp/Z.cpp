d94cc5a4a94d966c71441409413cd40618c9f323
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2021-07-17 23:02:00 +0530

    Add solution for problem `Z - Frog 3` Link: https://atcoder.jp/contests/dp/tasks/dp_z Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/24337428

--- atcoder/dp/Z.cpp
+++ atcoder/dp/Z.cpp
@@ -13,17 +13,15 @@ struct LiChaoTree {
     struct Line {
         T a, b;
         Line(T a, T b) : a(a), b(b) {}
-        Line() {
-        }  // only for exploiting uninitialized optimizations for the buffer
-        inline T operator()(T x) const { return a * x + b; }
+        Line() {}
+        inline T get(T x) const { return a * x + b; }
     };
 
     struct Node {
         Line x;
         Node *l, *r;
         Node(const Line& x) : x{x}, l{nullptr}, r{nullptr} {}
-        Node() {
-        }  // only for exploiting uninitialized optimizations for the buffer
+        Node() {}
     };
 
     template <class U>
@@ -42,7 +40,7 @@ struct LiChaoTree {
     Node* update(Node* t, Line& x, const T& l, const T& r, const T& x_l,
                  const T& x_r) {
         if (!t) return newNode(x);
-        auto t_l = t->x(l), t_r = t->x(r);
+        auto t_l = t->x.get(l), t_r = t->x.get(r);
         if (t_l <= x_l && t_r <= x_r) {
             return t;
         } else if (t_l >= x_l && t_r >= x_r) {
@@ -51,7 +49,7 @@ struct LiChaoTree {
             return t;
         } else {
             T m = (l + r) / 2;
-            auto t_m = t->x(m), x_m = x(m);
+            auto t_m = t->x.get(m), x_m = x.get(m);
             t = newNode(*t);
             if (t_m > x_m) {
                 std::swap(t->x, x);
@@ -71,17 +69,17 @@ struct LiChaoTree {
 
     void update(const T& a, const T& b) {
         Line x(a, b);
-        root = update(root, x, x_low, x_high, x(x_low), x(x_high));
+        root = update(root, x, x_low, x_high, x.get(x_low), x.get(x_high));
     }
 
     T query(const Node* t, const T& l, const T& r, const T& x) const {
         if (!t) return id;
-        if (l == r) return t->x(x);
+        if (l == r) return t->x.get(x);
         T m = (l + r) / 2;
         if (x <= m)
-            return std::min(t->x(x), query(t->l, l, m, x));
+            return std::min(t->x.get(x), query(t->l, l, m, x));
         else
-            return std::min(t->x(x), query(t->r, m + 1, r, x));
+            return std::min(t->x.get(x), query(t->r, m + 1, r, x));
     }
 
     T query(const T& x) const { return query(root, x_low, x_high, x); }
