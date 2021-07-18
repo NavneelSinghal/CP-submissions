ffbbcb285e6c5463f117ae21098c326a421c49cd
Navneel Singhal <navneel.singhal@ymail.com>
2021-07-18 15:48:00 +0530

    Add solution for problem `F - Bingo` Link: https://codeforces.com/contest/1530/problem/F Tags: bitmasks, combinatorics, dp, math, probabilities, *2600 Ref: https://codeforces.com/contest/1530/submission/122917993

--- codeforces/1530/F.cpp
+++ codeforces/1530/F.cpp
@@ -3,128 +3,81 @@
 
 #include <bits/stdc++.h>
 
-using namespace std;
-
 constexpr int mod = 31607;
 
-template <std::uint32_t P>
-struct ModInt32 {
+template <uint32_t Modulus>
+class Modular {
+    using M = Modular;
+
    public:
-    using i32 = std::int32_t;
-    using u32 = std::uint32_t;
-    using i64 = std::int64_t;
-    using u64 = std::uint64_t;
-    using m32 = ModInt32;
+    static_assert(int(Modulus) >= 1, "Modulus must be in the range [1, 2^31)");
+    static constexpr int modulus() { return Modulus; }
+    static M raw(uint32_t v) { return *reinterpret_cast<M*>(&v); }
+    Modular() : v_(0) {}
+    Modular(int64_t v) : v_((v %= Modulus) < 0 ? v + Modulus : v) {}
+    template <class T>
+    explicit operator T() const {
+        return v_;
+    }
+    M& operator++() { return v_ = ++v_ == Modulus ? 0 : v_, *this; }
+    M& operator--() { return --(v_ ? v_ : v_ = Modulus), *this; }
+    M& operator*=(M o) { return v_ = uint64_t(v_) * o.v_ % Modulus, *this; }
+    M& operator/=(M o) {
+        auto [inv, gcd] = extgcd(o.v_, Modulus);
+        assert(gcd == 1);
+        return *this *= inv;
+    }
+    M& operator+=(M o) {
+        return v_ = int(v_ += o.v_ - Modulus) < 0 ? v_ + Modulus : v_, *this;
+    }
+    M& operator-=(M o) {
+        return v_ = int(v_ -= o.v_) < 0 ? v_ + Modulus : v_, *this;
+    }
+    friend M operator++(M& a, int) { return std::exchange(a, ++M(a)); }
+    friend M operator--(M& a, int) { return std::exchange(a, --M(a)); }
+    friend M operator+(M a) { return a; }
+    friend M operator-(M a) { return a.v_ = a.v_ ? Modulus - a.v_ : 0, a; }
+    friend M operator*(M a, M b) { return a *= b; }
+    friend M operator/(M a, M b) { return a /= b; }
+    friend M operator+(M a, M b) { return a += b; }
+    friend M operator-(M a, M b) { return a -= b; }
+    template <class T>
+    friend T& operator>>(T& is, M& x) {
+        int64_t v;
+        return is >> v, x = v, is;
+    }
+    template <class T>
+    friend T& operator<<(T& os, M x) {
+        return os << x.v_;
+    }
+    friend bool operator==(M a, M b) { return a.v_ == b.v_; }
+    friend bool operator!=(M a, M b) { return a.v_ != b.v_; }
 
    private:
-    u32 v;
-    static constexpr u32 get_r() {
-        u32 iv = P;
-        for (u32 i = 0; i != 4; ++i) iv *= 2U - P * iv;
-        return -iv;
+    static std::pair<int, int> extgcd(int a, int b) {
+        std::array<int, 2> x{1, 0};
+        while (b) std::swap(x[0] -= a / b * x[1], x[1]), std::swap(a %= b, b);
+        return {x[0], a};
     }
-    static constexpr u32 r = get_r(), r2 = -u64(P) % P;
-    static_assert((P & 1) == 1);
-    static_assert(-r * P == 1);
-    static_assert(P < (1 << 30));
-    static constexpr u32 pow_mod(u32 x, u64 y) {
-        u32 res = 1;
-        for (; y != 0; y >>= 1, x = u64(x) * x % P)
-            if (y & 1) res = u64(res) * x % P;
-        return res;
-    }
-    static constexpr u32 reduce(u64 x) { return x + u64(u32(x) * r) * P >> 32; }
-    static constexpr u32 norm(u32 x) { return x - (P & -(x >= P)); }
-
-   public:
-    static constexpr u32 get_pr() {
-        u32 tmp[32] = {}, cnt = 0;
-        const u64 phi = P - 1;
-        u64 m = phi;
-        for (u64 i = 2; i * i <= m; ++i)
-            if (m % i == 0) {
-                tmp[cnt++] = i;
-                while (m % i == 0) m /= i;
-            }
-        if (m != 1) tmp[cnt++] = m;
-        for (u64 res = 2; res != P; ++res) {
-            bool flag = true;
-            for (u32 i = 0; i != cnt && flag; ++i)
-                flag &= pow_mod(res, phi / tmp[i]) != 1;
-            if (flag) return res;
-        }
-        return 0;
-    }
-    constexpr ModInt32() : v(0){};
-    ~ModInt32() = default;
-    constexpr ModInt32(u32 v) : v(reduce(u64(v) * r2)) {}
-    constexpr ModInt32(const m32& rhs) : v(rhs.v) {}
-    constexpr u32 get() const { return norm(reduce(v)); }
-    explicit constexpr operator u32() const { return get(); }
-    explicit constexpr operator i32() const { return i32(get()); }
-    constexpr m32& operator=(const m32& rhs) { return v = rhs.v, *this; }
-    constexpr m32 operator-() const {
-        m32 res;
-        return res.v = (P << 1 & -(v != 0)) - v, res;
-    }
-    constexpr m32 inv() const { return pow(-1); }
-    constexpr m32& operator+=(const m32& rhs) {
-        return v += rhs.v - (P << 1), v += P << 1 & -(v >> 31), *this;
-    }
-    constexpr m32& operator-=(const m32& rhs) {
-        return v -= rhs.v, v += P << 1 & -(v >> 31), *this;
-    }
-    constexpr m32& operator*=(const m32& rhs) {
-        return v = reduce(u64(v) * rhs.v), *this;
-    }
-    constexpr m32& operator/=(const m32& rhs) {
-        return this->operator*=(rhs.inv());
-    }
-    friend m32 operator+(const m32& lhs, const m32& rhs) {
-        return m32(lhs) += rhs;
-    }
-    friend m32 operator-(const m32& lhs, const m32& rhs) {
-        return m32(lhs) -= rhs;
-    }
-    friend m32 operator*(const m32& lhs, const m32& rhs) {
-        return m32(lhs) *= rhs;
-    }
-    friend m32 operator/(const m32& lhs, const m32& rhs) {
-        return m32(lhs) /= rhs;
-    }
-    friend bool operator==(const m32& lhs, const m32& rhs) {
-        return norm(lhs.v) == norm(rhs.v);
-    }
-    friend bool operator!=(const m32& lhs, const m32& rhs) {
-        return norm(lhs.v) != norm(rhs.v);
-    }
-    friend std::istream& operator>>(std::istream& is, m32& rhs) {
-        return is >> rhs.v, rhs.v = reduce(u64(rhs.v) * r2), is;
-    }
-    friend std::ostream& operator<<(std::ostream& os, const m32& rhs) {
-        return os << rhs.get();
-    }
-    constexpr m32 pow(i64 y) const {
-        if ((y %= P - 1) < 0)
-            y += P - 1;  // phi(P) = P - 1, assume P is a prime number
-        m32 res(1), x(*this);
-        for (; y != 0; y >>= 1, x *= x)
-            if (y & 1) res *= x;
-        return res;
-    }
+    uint32_t v_;
 };
 
-using mint = ModInt32<mod>;
+using mint = Modular<mod>;
+
+using namespace std;
 
 template <bool inv = false>
 void transform(mint a[], int n) {
     for (int len = 1; len < n; len <<= 1) {
-        for (int i = 0; i < n; i += len << 1)
+        for (int i = 0; i < n; i += len << 1) {
 #pragma GCC ivdep
             for (int j = i; j < i + len; ++j) {
-                auto v = a[j + len];
-                a[j] += inv ? -v : v;
+                if constexpr (inv)
+                    a[j] -= a[j + len];
+                else
+                    a[j] += a[j + len];
             }
+        }
     }
 }
 
@@ -137,10 +90,9 @@ void mul(vector<mint>& x, vector<mint>& y) {
 }
 
 int main() {
-    ios_base::sync_with_stdio(0), cin.tie(0);
+    cin.tie(nullptr)->sync_with_stdio(false);
 
-    mint inv = 1/mint(10000);
-
+    mint inv = 1 / mint(10000);
     int n;
     cin >> n;
     const int sz = 1 << n;
@@ -158,8 +110,7 @@ int main() {
     vector<mint> x, x1, x2, x12;
     for (int j = 0; j < n; ++j) {
         vector<mint> ii(n);
-        for (int i = 0; i < n; ++i)
-            ii[i] = a[i][j] / (One - a[i][j]);
+        for (int i = 0; i < n; ++i) ii[i] = a[i][j] / (One - a[i][j]);
         vector<mint> y(sz), y1, y2, y12;
         y[0] = One;
         for (int i = 0; i < n; ++i) y[0] *= (One - a[i][j]);
@@ -181,7 +132,7 @@ int main() {
     }
     itrans(x), itrans(x1), itrans(x2), itrans(x12);
     mint ans = (x[0] - x1[0] - x2[0] + x12[0]);
-    cout << (1 - ans).get() << "\n";
+    cout << int(1 - ans) << "\n";
 
     return 0;
 }
