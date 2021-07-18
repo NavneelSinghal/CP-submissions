137c2b527b2014151f17407b8ff71b8e3204f0f3
Navneel Singhal <navneel.singhal@ymail.com>
2021-07-18 15:36:00 +0530

    Add solution for problem `F - Bingo` Link: https://codeforces.com/contest/1530/problem/F Tags: bitmasks, combinatorics, dp, math, probabilities, *2600 Ref: https://codeforces.com/contest/1530/submission/122916996

--- codeforces/1530/F.cpp
+++ codeforces/1530/F.cpp
@@ -5,64 +5,280 @@
 
 constexpr int mod = 31607;
 
-template <uint32_t Modulus>
-class Modular {
-    using M = Modular;
-
+template <std::uint32_t P>
+struct ModInt32 {
    public:
-    static_assert(int(Modulus) >= 1, "Modulus must be in the range [1, 2^31)");
-    static constexpr int modulus() { return Modulus; }
-    static M raw(uint32_t v) { return *reinterpret_cast<M*>(&v); }
-    Modular() : v_(0) {}
-    Modular(int64_t v) : v_((v %= Modulus) < 0 ? v + Modulus : v) {}
-    template <class T>
-    explicit operator T() const {
-        return v_;
-    }
-    M& operator++() { return v_ = ++v_ == Modulus ? 0 : v_, *this; }
-    M& operator--() { return --(v_ ? v_ : v_ = Modulus), *this; }
-    M& operator*=(M o) { return v_ = uint64_t(v_) * o.v_ % Modulus, *this; }
-    M& operator/=(M o) {
-        auto [inv, gcd] = extgcd(o.v_, Modulus);
-        assert(gcd == 1);
-        return *this *= inv;
-    }
-    M& operator+=(M o) {
-        return v_ = int(v_ += o.v_ - Modulus) < 0 ? v_ + Modulus : v_, *this;
-    }
-    M& operator-=(M o) {
-        return v_ = int(v_ -= o.v_) < 0 ? v_ + Modulus : v_, *this;
-    }
-    friend M operator++(M& a, int) { return std::exchange(a, ++M(a)); }
-    friend M operator--(M& a, int) { return std::exchange(a, --M(a)); }
-    friend M operator+(M a) { return a; }
-    friend M operator-(M a) { return a.v_ = a.v_ ? Modulus - a.v_ : 0, a; }
-    friend M operator*(M a, M b) { return a *= b; }
-    friend M operator/(M a, M b) { return a /= b; }
-    friend M operator+(M a, M b) { return a += b; }
-    friend M operator-(M a, M b) { return a -= b; }
-    template <class T>
-    friend T& operator>>(T& is, M& x) {
-        int64_t v;
-        return is >> v, x = v, is;
-    }
-    template <class T>
-    friend T& operator<<(T& os, M x) {
-        return os << x.v_;
-    }
-    friend bool operator==(M a, M b) { return a.v_ == b.v_; }
-    friend bool operator!=(M a, M b) { return a.v_ != b.v_; }
+    using i32 = std::int32_t;
+    using u32 = std::uint32_t;
+    using i64 = std::int64_t;
+    using u64 = std::uint64_t;
+    using m32 = ModInt32;
 
    private:
-    static std::pair<int, int> extgcd(int a, int b) {
-        std::array<int, 2> x{1, 0};
-        while (b) std::swap(x[0] -= a / b * x[1], x[1]), std::swap(a %= b, b);
-        return {x[0], a};
+    u32 v;
+    static constexpr u32 get_r() {
+        u32 iv = P;
+        for (u32 i = 0; i != 4; ++i) iv *= 2U - P * iv;
+        return -iv;
     }
-    uint32_t v_;
+    static constexpr u32 r = get_r(), r2 = -u64(P) % P;
+    static_assert((P & 1) == 1);
+    static_assert(-r * P == 1);
+    static_assert(P < (1 << 30));
+    static constexpr u32 pow_mod(u32 x, u64 y) {
+        u32 res = 1;
+        for (; y != 0; y >>= 1, x = u64(x) * x % P)
+            if (y & 1) res = u64(res) * x % P;
+        return res;
+    }
+    static constexpr u32 reduce(u64 x) { return x + u64(u32(x) * r) * P >> 32; }
+    static constexpr u32 norm(u32 x) { return x - (P & -(x >= P)); }
+
+   public:
+    static constexpr u32 get_pr() {
+        u32 tmp[32] = {}, cnt = 0;
+        const u64 phi = P - 1;
+        u64 m = phi;
+        for (u64 i = 2; i * i <= m; ++i)
+            if (m % i == 0) {
+                tmp[cnt++] = i;
+                while (m % i == 0) m /= i;
+            }
+        if (m != 1) tmp[cnt++] = m;
+        for (u64 res = 2; res != P; ++res) {
+            bool flag = true;
+            for (u32 i = 0; i != cnt && flag; ++i)
+                flag &= pow_mod(res, phi / tmp[i]) != 1;
+            if (flag) return res;
+        }
+        return 0;
+    }
+    constexpr ModInt32() : v(0){};
+    ~ModInt32() = default;
+    constexpr ModInt32(u32 v) : v(reduce(u64(v) * r2)) {}
+    constexpr ModInt32(const m32& rhs) : v(rhs.v) {}
+    constexpr u32 get() const { return norm(reduce(v)); }
+    explicit constexpr operator u32() const { return get(); }
+    explicit constexpr operator i32() const { return i32(get()); }
+    constexpr m32& operator=(const m32& rhs) { return v = rhs.v, *this; }
+    constexpr m32 operator-() const {
+        m32 res;
+        return res.v = (P << 1 & -(v != 0)) - v, res;
+    }
+    constexpr m32 inv() const { return pow(-1); }
+    constexpr m32& operator+=(const m32& rhs) {
+        return v += rhs.v - (P << 1), v += P << 1 & -(v >> 31), *this;
+    }
+    constexpr m32& operator-=(const m32& rhs) {
+        return v -= rhs.v, v += P << 1 & -(v >> 31), *this;
+    }
+    constexpr m32& operator*=(const m32& rhs) {
+        return v = reduce(u64(v) * rhs.v), *this;
+    }
+    constexpr m32& operator/=(const m32& rhs) {
+        return this->operator*=(rhs.inv());
+    }
+    friend m32 operator+(const m32& lhs, const m32& rhs) {
+        return m32(lhs) += rhs;
+    }
+    friend m32 operator-(const m32& lhs, const m32& rhs) {
+        return m32(lhs) -= rhs;
+    }
+    friend m32 operator*(const m32& lhs, const m32& rhs) {
+        return m32(lhs) *= rhs;
+    }
+    friend m32 operator/(const m32& lhs, const m32& rhs) {
+        return m32(lhs) /= rhs;
+    }
+    friend bool operator==(const m32& lhs, const m32& rhs) {
+        return norm(lhs.v) == norm(rhs.v);
+    }
+    friend bool operator!=(const m32& lhs, const m32& rhs) {
+        return norm(lhs.v) != norm(rhs.v);
+    }
+    friend std::istream& operator>>(std::istream& is, m32& rhs) {
+        return is >> rhs.v, rhs.v = reduce(u64(rhs.v) * r2), is;
+    }
+    friend std::ostream& operator<<(std::ostream& os, const m32& rhs) {
+        return os << rhs.get();
+    }
+    constexpr m32 pow(i64 y) const {
+        if ((y %= P - 1) < 0)
+            y += P - 1;  // phi(P) = P - 1, assume P is a prime number
+        m32 res(1), x(*this);
+        for (; y != 0; y >>= 1, x *= x)
+            if (y & 1) res *= x;
+        return res;
+    }
 };
 
-using mint = Modular<mod>;
+using mint = ModInt32<mod>;
+
+namespace IO {
+    constexpr bool UNSAFE = false;
+    constexpr int GLOB_BUF_SIZE = 1 << 15;
+#ifndef DEBUG
+    #define CHANGE_DEFAULT_STREAMS
+    static struct FastInput {
+        FastInput() {
+            if constexpr (UNSAFE) {
+                chars_read = fread(buf, 1, BUF_SIZE, in);
+                buf_pos = 0;
+                buf[0] = (chars_read == 0 ? -1 : buf[0]);
+            }
+        }
+        static constexpr int BUF_SIZE = GLOB_BUF_SIZE;
+        char buf[BUF_SIZE];
+        size_t chars_read = 0;
+        size_t buf_pos = 0;
+        FILE* in = stdin;
+        char cur = 0;
+        inline char get_char() {
+            if constexpr (!UNSAFE) {
+                if (buf_pos >= chars_read) {
+                    chars_read = fread(buf, 1, BUF_SIZE, in);
+                    buf_pos = 0;
+                    buf[0] = (chars_read == 0 ? -1 : buf[0]);
+                }
+            }
+            return cur = buf[buf_pos++];
+        }
+        template <typename T>
+        inline FastInput* tie(T) { return this; }
+        inline void sync_with_stdio(bool) {}
+        inline explicit operator bool() { return cur != -1; }
+        inline static bool is_blank(char c) { return c <= ' '; }
+        inline bool skip_blanks() {
+            while (is_blank(cur) && cur != -1) get_char();
+            return cur != -1;
+        }
+        inline FastInput& operator>>(char& c) {
+            skip_blanks();
+            c = cur;
+            return *this;
+        }
+        inline FastInput& operator>>(std::string& s) {
+            if (skip_blanks()) {
+                s.clear();
+                do {
+                    s += cur;
+                } while (!is_blank(get_char()));
+            }
+            return *this;
+        }
+        template <typename T>
+        inline FastInput& read_integer(T& n) {
+            // unsafe, doesn't check that characters are actually digits
+            n = 0;
+            if (skip_blanks()) {
+                int sign = +1;
+                if (cur == '-') {
+                    sign = -1;
+                    get_char();
+                }
+                do {
+                    n += n + (n << 3) + cur - '0';
+                } while (!is_blank(get_char()));
+                n *= sign;
+            }
+            return *this;
+        }
+        template <typename T>
+        inline typename std::enable_if<std::is_integral<T>::value,
+                                       FastInput&>::type
+        operator>>(T& n) {
+            return read_integer(n);
+        }
+    #if !defined(_WIN32) || defined(_WIN64)
+        inline FastInput& operator>>(__int128& n) { return read_integer(n); }
+    #endif
+        template <typename T>
+        inline typename std::enable_if<std::is_floating_point<T>::value,
+                                       FastInput&>::type
+        operator>>(T& n) {
+            // not sure if really fast, for compatibility only
+            n = 0;
+            if (skip_blanks()) {
+                std::string s;
+                (*this) >> s;
+                sscanf(s.c_str(), "%lf", &n);
+            }
+            return *this;
+        }
+    } fast_input;
+    #define cin IO::fast_input
+    static struct FastOutput {
+        static constexpr int BUF_SIZE = GLOB_BUF_SIZE;
+        char buf[BUF_SIZE];
+        size_t buf_pos = 0;
+        static constexpr int TMP_SIZE = GLOB_BUF_SIZE;
+        char tmp[TMP_SIZE];
+        FILE* out = stdout;
+        inline void put_char(char c) {
+            buf[buf_pos++] = c;
+            if (buf_pos == BUF_SIZE) {
+                fwrite(buf, 1, buf_pos, out);
+                buf_pos = 0;
+            }
+        }
+        ~FastOutput() { fwrite(buf, 1, buf_pos, out); }
+        inline FastOutput& operator<<(char c) {
+            put_char(c);
+            return *this;
+        }
+        inline FastOutput& operator<<(const char* s) {
+            while (*s) put_char(*s++);
+            return *this;
+        }
+        inline FastOutput& operator<<(const std::string& s) {
+            for (auto x : s) put_char(x);
+            return *this;
+        }
+        template <typename T>
+        inline char* integer_to_string(T n) {
+            // beware of TMP_SIZE
+            char* p = tmp + TMP_SIZE - 1;
+            if (n == 0)
+                *--p = '0';
+            else {
+                bool is_negative = false;
+                if (n < 0) {
+                    is_negative = true;
+                    n = -n;
+                }
+                while (n > 0) {
+                    *--p = (char)('0' + n % 10);
+                    n /= 10;
+                }
+                if (is_negative) *--p = '-';
+            }
+            return p;
+        }
+        template <typename T>
+        inline typename std::enable_if<std::is_integral<T>::value, char*>::type
+        stringify(T n) {
+            return integer_to_string(n);
+        }
+    #if !defined(_WIN32) || defined(_WIN64)
+        inline char* stringify(__int128 n) { return integer_to_string(n); }
+    #endif
+        template <typename T>
+        inline typename std::enable_if<std::is_floating_point<T>::value,
+                                       char*>::type
+        stringify(T n) {
+            sprintf(tmp, "%.17f", n);
+            return tmp;
+        }
+        template <typename T>
+        inline FastOutput& operator<<(const T& n) {
+            auto p = stringify(n);
+            for (; *p != 0; p++) put_char(*p);
+            return *this;
+        }
+    } fast_output;
+    #define cout IO::fast_output
+#endif
+}  // namespace IO
 
 using namespace std;
 
@@ -70,7 +286,6 @@ template <bool inv = false>
 void transform(mint a[], int n) {
     for (int len = 1; len < n; len <<= 1) {
         for (int i = 0; i < n; i += len << 1) {
-#pragma GCC ivdep
             for (int j = i; j < i + len; ++j) {
                 if constexpr (inv)
                     a[j] -= a[j + len];
@@ -132,7 +347,7 @@ int main() {
     }
     itrans(x), itrans(x1), itrans(x2), itrans(x12);
     mint ans = (x[0] - x1[0] - x2[0] + x12[0]);
-    cout << int(1 - ans) << "\n";
+    cout << (1 - ans).get() << "\n";
 
     return 0;
 }
