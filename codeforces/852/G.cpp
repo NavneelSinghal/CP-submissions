207dea6d9cc8d5cc492add86fb38754d1f927f54
Navneel Singhal <navneel.singhal@ymail.com>
2021-07-21 02:09:00 +0530

    Add solution for problem `G - Bathroom terminal` Link: https://codeforces.com/contest/852/problem/G Tags: implementation, *1700 Ref: https://codeforces.com/contest/852/submission/123137046

--- codeforces/852/G.cpp
+++ codeforces/852/G.cpp
@@ -12,6 +12,67 @@
 using ll = int64_t;
 using namespace std;
 
+template <bool multi = true, char A = 'a', int CHARSET = 5>
+struct Trie {
+    static const int ROOT = 0;
+    int sz;
+    vector<vector<int>> trie;
+    vector<int> freq;
+
+    Trie(int n) {
+        sz = 1;
+        trie.assign(CHARSET, vector<int>(n + 2, -1));
+        freq.assign(n + 2, 0);
+    }
+
+    void insert(string& s) {
+        int node = ROOT;
+        for (auto c : s) {
+            int& next_node = trie[c - A][node];
+            if (next_node == -1) {
+                next_node = sz++;
+            }
+            node = next_node;
+        }
+        if constexpr (multi) {
+            freq[node]++;
+        } else {
+            freq[node] = 1;
+        }
+    }
+
+    int getfreq(string& s) {
+        int node = ROOT;
+        for (auto c : s) {
+            int& next_node = trie[c - A][node];
+            if (next_node == -1) {
+                return 0;
+            }
+            node = next_node;
+        }
+        return freq[node];
+    }
+
+    vector<string> listall() {
+        vector<string> ans;
+        const auto rec = [&](const auto& self, int node, string& s) -> void {
+            if (freq[node]) {
+                for (int i = 0; i < freq[node]; ++i) ans.push_back(s);
+            }
+            for (int i = 0; i < CHARSET; ++i) {
+                if (trie[i][node] != -1) {
+                    s += A + i;
+                    self(self, trie[i][node], s);
+                    s.pop_back();
+                }
+            }
+        };
+        string s = "";
+        rec(rec, ROOT, s);
+        return ans;
+    }
+};
+
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
     int t = 1;
@@ -21,13 +82,17 @@ int main() {
         int n, m;
         cin >> n >> m;
         vector<string> a(n);
-        for (auto& x : a) cin >> x;
-        map<string, int> freq;
-        for (auto& x : a) freq[x]++;
+        int sz = 0;
+        for (auto& x : a) cin >> x, sz += x.size();
+        Trie<true> freq(sz);
+        for (auto& x : a) freq.insert(x);
         for (int i = 0; i < m; ++i) {
             string s;
             cin >> s;
-            set<string> ans;
+            int sz = 1;
+            for (auto x : s)
+                if (x == '?') sz *= 6;
+            Trie<false> ans(sz * s.size());
             auto solve = [&](const auto& self, int i) -> void {
                 if (i == (int)s.size()) {
                     ans.insert(s);
@@ -50,9 +115,8 @@ int main() {
             };
             solve(solve, 0);
             int w = 0;
-            for (auto& x : ans) w += freq[x];
+            for (auto& x : ans.listall()) w += freq.getfreq(x);
             cout << w << '\n';
         }
     }
 }
-
