6bf6f9dba3749e4a9e75eb466de3a5837f4bb2a7
Navneel Singhal <navneel.singhal@ymail.com>
2021-07-26 10:19:00 +0530

    Add solution for problem `C - Manhattan Subarrays` Link: https://codeforces.com/contest/1550/problem/C Tags: brute force, geometry, greedy, implementation, *1700 Ref: https://codeforces.com/contest/1550/submission/123797069

--- codeforces/1550/C.cpp
+++ codeforces/1550/C.cpp
@@ -4,6 +4,7 @@
 #include <bits/stdc++.h>
 
 using ll = int64_t;
+
 using namespace std;
 
 template <class T>
@@ -21,6 +22,172 @@ auto compress(const vector<T>& a) {
     }
     return make_pair(compressed, vals);
 }
+
+namespace IO {
+    constexpr bool UNSAFE = false;
+    constexpr int GLOB_BUF_SIZE = 1 << 15;
+#ifndef DEBUG
+    #define CHANGE_DEFAULT_STREAMS
+    static struct FastInput {
+        FastInput() {
+            if constexpr (UNSAFE) {
+                chars_read = fread(buf, 1, BUF_SIZE, in);
+                buf_pos = 0;
+                buf[0] = (chars_read == 0 ? -1 : buf[0]);
+            }
+        }
+        static constexpr int BUF_SIZE = GLOB_BUF_SIZE;
+        char buf[BUF_SIZE];
+        size_t chars_read = 0;
+        size_t buf_pos = 0;
+        FILE* in = stdin;
+        char cur = 0;
+        inline char get_char() {
+            if constexpr (!UNSAFE) {
+                if (buf_pos >= chars_read) {
+                    chars_read = fread(buf, 1, BUF_SIZE, in);
+                    buf_pos = 0;
+                    buf[0] = (chars_read == 0 ? -1 : buf[0]);
+                }
+            }
+            return cur = buf[buf_pos++];
+        }
+        template <typename T>
+        inline FastInput* tie(T) { return this; }
+        inline void sync_with_stdio(bool) {}
+        inline explicit operator bool() { return cur != -1; }
+        inline static bool is_blank(char c) { return c <= ' '; }
+        inline bool skip_blanks() {
+            while (is_blank(cur) && cur != -1) get_char();
+            return cur != -1;
+        }
+        inline FastInput& operator>>(char& c) {
+            skip_blanks();
+            c = cur;
+            return *this;
+        }
+        inline FastInput& operator>>(std::string& s) {
+            if (skip_blanks()) {
+                s.clear();
+                do {
+                    s += cur;
+                } while (!is_blank(get_char()));
+            }
+            return *this;
+        }
+        template <typename T>
+        inline FastInput& read_integer(T& n) {
+            // unsafe, doesn't check that characters are actually digits
+            n = 0;
+            if (skip_blanks()) {
+                int sign = +1;
+                if (cur == '-') {
+                    sign = -1;
+                    get_char();
+                }
+                do {
+                    n += n + (n << 3) + cur - '0';
+                } while (!is_blank(get_char()));
+                n *= sign;
+            }
+            return *this;
+        }
+        template <typename T>
+        inline typename std::enable_if<std::is_integral<T>::value,
+                                       FastInput&>::type
+        operator>>(T& n) {
+            return read_integer(n);
+        }
+    #if !defined(_WIN32) || defined(_WIN64)
+        inline FastInput& operator>>(__int128& n) { return read_integer(n); }
+    #endif
+        template <typename T>
+        inline typename std::enable_if<std::is_floating_point<T>::value,
+                                       FastInput&>::type
+        operator>>(T& n) {
+            // not sure if really fast, for compatibility only
+            n = 0;
+            if (skip_blanks()) {
+                std::string s;
+                (*this) >> s;
+                sscanf(s.c_str(), "%lf", &n);
+            }
+            return *this;
+        }
+    } fast_input;
+    #define cin IO::fast_input
+    static struct FastOutput {
+        static constexpr int BUF_SIZE = GLOB_BUF_SIZE;
+        char buf[BUF_SIZE];
+        size_t buf_pos = 0;
+        static constexpr int TMP_SIZE = GLOB_BUF_SIZE;
+        char tmp[TMP_SIZE];
+        FILE* out = stdout;
+        inline void put_char(char c) {
+            buf[buf_pos++] = c;
+            if (buf_pos == BUF_SIZE) {
+                fwrite(buf, 1, buf_pos, out);
+                buf_pos = 0;
+            }
+        }
+        ~FastOutput() { fwrite(buf, 1, buf_pos, out); }
+        inline FastOutput& operator<<(char c) {
+            put_char(c);
+            return *this;
+        }
+        inline FastOutput& operator<<(const char* s) {
+            while (*s) put_char(*s++);
+            return *this;
+        }
+        inline FastOutput& operator<<(const std::string& s) {
+            for (auto x : s) put_char(x);
+            return *this;
+        }
+        template <typename T>
+        inline char* integer_to_string(T n) {
+            // beware of TMP_SIZE
+            char* p = tmp + TMP_SIZE - 1;
+            if (n == 0)
+                *--p = '0';
+            else {
+                bool is_negative = false;
+                if (n < 0) {
+                    is_negative = true;
+                    n = -n;
+                }
+                while (n > 0) {
+                    *--p = (char)('0' + n % 10);
+                    n /= 10;
+                }
+                if (is_negative) *--p = '-';
+            }
+            return p;
+        }
+        template <typename T>
+        inline typename std::enable_if<std::is_integral<T>::value, char*>::type
+        stringify(T n) {
+            return integer_to_string(n);
+        }
+    #if !defined(_WIN32) || defined(_WIN64)
+        inline char* stringify(__int128 n) { return integer_to_string(n); }
+    #endif
+        template <typename T>
+        inline typename std::enable_if<std::is_floating_point<T>::value,
+                                       char*>::type
+        stringify(T n) {
+            sprintf(tmp, "%.17f", n);
+            return tmp;
+        }
+        template <typename T>
+        inline FastOutput& operator<<(const T& n) {
+            auto p = stringify(n);
+            for (; *p != 0; p++) put_char(*p);
+            return *this;
+        }
+    } fast_output;
+    #define cout IO::fast_output
+#endif
+}  // namespace IO
 
 // clang-format off
 template <class Base,
@@ -74,14 +241,14 @@ struct lazy_segtree {
         for (int i = 1; i <= log; ++i) update(p >> i);
     }
 
-    Node get(int p) const {
+    Node get(int p) {
         p += size;
         if constexpr (is_lazy)
             for (int i = log; i >= 1; i--) push(p >> i);
         return d[p];
     }
 
-    Node query(int l, int r) const {
+    Node query(int l, int r) {
         if (l == r) return id_node;
         l += size, r += size;
         if constexpr (is_lazy) {
@@ -98,7 +265,9 @@ struct lazy_segtree {
         }
         return combine(sml, smr);
     }
+    
     Node all_query() const { return d[1]; }
+    
     void update(int p, Update f) {
         p += size;
         if constexpr (is_lazy)
@@ -106,17 +275,18 @@ struct lazy_segtree {
         d[p] = apply_update(f, d[p]);
         for (int i = 1; i <= log; ++i) update(p >> i);
     }
+    
     void update(int l, int r, Update f) {
         if (l == r) return;
         l += size, r += size;
-        int l_ctz = __builtin_ctz(l);
-        int r_ctz = __builtin_ctz(r);
+        const int l_ctz = __builtin_ctz(l);
+        const int r_ctz = __builtin_ctz(r);
         if constexpr (is_lazy) {
             for (int i = log; i > l_ctz; --i) push(l >> i);
             for (int i = log; i > r_ctz; --i) push((r - 1) >> i);
         }
         {
-            int l2 = l, r2 = r;
+            const int l2 = l, r2 = r;
             while (l < r) {
                 if (l & 1) all_apply(l++, f);
                 if (r & 1) all_apply(--r, f);
@@ -128,12 +298,8 @@ struct lazy_segtree {
         for (int i = r_ctz + 1; i <= log; ++i) update((r - 1) >> i);
     }
 
-    template <bool (*g)(Node)>
-    int max_right(int l) const {
-        return max_right(l, [](Node x) { return g(x); });
-    }
     template <class G>
-    int max_right(int l, G g) const {
+    int max_right(int l, G g) {
         // assert(0 <= l && l <= _n);
         // assert(g(id_node));
         if (l == _n) return _n;
@@ -160,12 +326,8 @@ struct lazy_segtree {
         return _n;
     }
 
-    template <bool (*g)(Node)>
-    int min_left(int r) const {
-        return min_left(r, [](Node x) { return g(x); });
-    }
     template <class G>
-    int min_left(int r, G g) const {
+    int min_left(int r, G g) {
         // assert(0 <= r && r <= _n);
         // assert(g(id_node));
         if (r == 0) return 0;
@@ -194,8 +356,8 @@ struct lazy_segtree {
 
    private:
     int _n, size, log;
-    mutable std::vector<Node> d;
-    mutable std::vector<Update> lz;
+    std::vector<Node> d;
+    std::vector<Update> lz;
     MakeNode make_node;
     CombineNodes combine;
     Node id_node;
@@ -227,55 +389,81 @@ int main() {
         cin >> n;
         vector<int> a(n);
         for (auto& x : a) cin >> x;
-
-        vector<int> prv_ge(n, -1);
-        vector<int> prv_le(n, -1);
-
+        vector<int> nxt_ge(n, n);
+        vector<int> nxt_le(n, n);
         stack<int> s;
-        for (int i = 0; i < n; ++i) {
+        for (int i = n - 1; i >= 0; --i) {
             while (!s.empty() && a[s.top()] < a[i]) s.pop();
-            if (!s.empty()) prv_ge[i] = s.top();
+            if (!s.empty()) nxt_ge[i] = s.top();
             s.push(i);
         }
         stack<int>().swap(s);
-        for (int i = 0; i < n; ++i) {
+        for (int i = n - 1; i >= 0; --i) {
             while (!s.empty() && a[s.top()] > a[i]) s.pop();
-            if (!s.empty()) prv_le[i] = s.top();
+            if (!s.empty()) nxt_le[i] = s.top();
             s.push(i);
         }
-
+        vector<int> closest_ge(n, n);
+        vector<int> closest_le(n, n);
+        const auto rec = [&](const auto& self, int l, int r) -> void {
+            if (l >= r) return;
+            int m = (l + r + 1) / 2;
+            if (l < m - 1) self(self, l, m - 1);
+            if (m < r) self(self, m, r);
+            vector<int> temp(a.begin() + l, a.begin() + r + 1);
+            auto [compressed, vals] = compress(temp);
+            int sz = temp.size();
+            vector<int> min_nxt_le(sz, n);
+            vector<int> min_nxt_ge(sz, n);
+            for (int i = m; i <= r; ++i) {
+                int loc =
+                    lower_bound(begin(vals), end(vals), a[i]) - begin(vals);
+                min_nxt_le[loc] = min(min_nxt_le[loc], nxt_le[i]);
+                min_nxt_ge[loc] = min(min_nxt_ge[loc], nxt_ge[i]);
+            }
+            for (int i = 1; i < sz; ++i)
+                min_nxt_le[i] = min(min_nxt_le[i], min_nxt_le[i - 1]);
+            for (int i = sz - 2; i >= 0; --i)
+                min_nxt_ge[i] = min(min_nxt_ge[i], min_nxt_ge[i + 1]);
+            for (int i = l; i < m; ++i) {
+                int loc =
+                    lower_bound(begin(vals), end(vals), a[i]) - begin(vals);
+                closest_le[i] = min(closest_le[i], min_nxt_le[loc]);
+                closest_ge[i] = min(closest_ge[i], min_nxt_ge[loc]);
+            }
+        };
+        rec(rec, 0, n - 1);
         ll ans = 0;
 
-        vector<int> v(n, -1);
-        using Base = int;
-        using Node = int;
-        const Node id_node = -1;
-        auto make_node = [](const Base& b, int i) -> Node {
-            return b;
+        vector<pair<int, int>> v(n);
+        for (int i = 0; i < n; ++i) v[i] = {closest_le[i], closest_ge[i]};
+        using Base = pair<int, int>;
+        struct Node {
+            int le, ge;
         };
-        auto combine = [](const Node& n, const Node& m) -> Node {
-            return max(n, m);
+        const Node id_node = Node{n, n};
+        auto make_node = [](const Base& b, int i) {
+            return Node{b.first, b.second};
         };
-        using Update = Base;
-        const Update id_update = -1;
-        auto apply_update = [](const Update& u, const Node& n) -> Node {
-            if (u == id_update) return n;
-            return u;
+        auto combine = [](const Node& n, const Node& m) {
+            return Node{min(n.le, m.le), min(n.ge, m.ge)};
         };
-        lazy_segtree smaller(v, id_node, make_node, combine, id_update,
-                             apply_update);
-        lazy_segtree larger = smaller;
-        auto [compressed, vals] = compress(a);
-        int l = 0;
+        using Update = int;
+        const Update id_update = 0;
+        auto apply_update = [](const Update& u, const Node& n) {
+            return n;
+        };
+        lazy_segtree st(v, id_node, make_node, combine, id_update,
+                        apply_update);
+        int r = 0;
         for (int i = 0; i < n; ++i) {
-            auto closest_smaller = smaller.query(0, compressed[i] + 1) + 1;
-            auto closest_larger = larger.query(compressed[i], n) + 1;
-            l = max({l, closest_larger, closest_smaller});
-            ans += i - l + 1;
-            smaller.update(compressed[i], prv_le[i]);
-            larger.update(compressed[i], prv_ge[i]);
+            while (r < n) {
+                auto nd = st.query(i, r);
+                if (!(nd.le > r && nd.ge > r)) break;
+                ++r;
+            }
+            ans += r - i;
         }
         cout << ans << '\n';
     }
 }
-
