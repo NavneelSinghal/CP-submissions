a1d4f67426947c4b9363b4e9c152185ea1963bc1
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2021-08-02 13:35:00 +0530

    Add solution for problem `F - Remainder Problem` Link: https://codeforces.com/contest/1207/problem/F Tags: brute force, data structures, implementation, *2100 Ref: https://codeforces.com/contest/1207/submission/124661948

--- codeforces/1207/F.cpp
+++ codeforces/1207/F.cpp
@@ -183,7 +183,7 @@ namespace IO {
 constexpr int threshold = 319;
 constexpr int n = 500000;
 int a[n + 1000];
-int b[threshold + 1][threshold + 1];
+int b[threshold + 1][threshold];
 
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
