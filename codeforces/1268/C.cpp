f9d5aff2075957750cd7b2e1943aaa149a6debac
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2021-08-04 22:59:00 +0530

    Add solution for problem `C - K Integers` Link: https://codeforces.com/contest/1268/problem/C Tags: binary search, data structures, *2300 Ref: https://codeforces.com/contest/1268/submission/124888059

---
+++ codeforces/1268/C.cpp
@@ -0,0 +1,94 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx,avx2,sse,sse2,sse3,sse4,popcnt,bmi,bmi2,lzcnt")
+
+#include <bits/stdc++.h>
+
+#ifdef DEBUG
+    #include "../debug/debug.hpp"
+#else
+    #define debug(...) 0
+#endif
+
+using ll = int64_t;
+using namespace std;
+
+template <typename T>
+struct Fenwick {
+    int n;
+    vector<T> t;
+    Fenwick(int n) : n(n), t(n + 1) {}
+    T query(int i) {
+        T s = 0;
+        while (i) {
+            s += t[i];
+            i -= i & (-i);
+        }
+        return s;
+    }
+    T query(int l, int r) { return query(r) - query(l - 1); }
+    void update(int i, T v) {
+        while (i <= n) {
+            t[i] += v;
+            i += i & (-i);
+        }
+    }
+};
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    int t = 1;
+    // cin >> t;
+    for (int test = 1; test <= t; ++test) {
+        ll n;
+        cin >> n;
+        vector<int> loc(n);
+        for (int i = 0; i < n; ++i) {
+            int x;
+            cin >> x;
+            loc[x - 1] = i;
+        }
+        vector<ll> ans(n);
+        Fenwick<ll> f(n);
+        ll cur_inversions = 0;
+        vector<set<int>> s(2);
+        vector<ll> sum(2);
+        auto add = [&](int i, int x) {
+            s[i].insert(x);
+            sum[i] += x;
+        };
+        auto remove = [&](int i, int x) {
+            s[i].erase(x);
+            sum[i] -= x;
+        };
+        int mid = -1;
+        for (ll i = 0; i < n; ++i) {
+            cur_inversions += i - f.query(loc[i]);
+            ans[i] = cur_inversions;
+            f.update(loc[i] + 1, 1);
+            int l_max = -1, r_min = 1e9;
+            if (i >= 2) l_max = *s[0].rbegin(), r_min = *s[1].begin();
+            if (i & 1) {
+                int add1 = mid;
+                int add2 = loc[i];
+                if (add1 > add2) swap(add1, add2);
+                if (add2 < l_max)
+                    remove(0, l_max), add(1, l_max);
+                else if (add1 > r_min)
+                    remove(1, r_min), add(0, r_min);
+                add(add1 > r_min, add1);
+                add(add2 > l_max, add2);
+                ans[i] += sum[1] - sum[0] - (1LL * (i + 1) * (i + 1)) / 4;
+            } else {
+                if (loc[i] > r_min)
+                    mid = r_min, remove(1, r_min), add(1, loc[i]);
+                else if (loc[i] < l_max)
+                    mid = l_max, remove(0, l_max), add(0, loc[i]);
+                else
+                    mid = loc[i];
+                ans[i] += sum[1] - sum[0] - (1LL * (i + 1) * (i + 1) - 1) / 4;
+            }
+        }
+        for (int i = 0; i < n; ++i) cout << ans[i] << " \n"[i == n - 1];
+    }
+}
+
