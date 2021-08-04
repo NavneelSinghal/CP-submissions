63b8b19f163f7d24f2d456f30df0e2177baff60b
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2021-08-04 18:34:00 +0530

    Add solution for problem `D - Distinct Characters Queries` Link: https://codeforces.com/contest/1234/problem/D Tags: data structures, *1600 Ref: https://codeforces.com/contest/1234/submission/124862829

--- codeforces/1234/D.cpp
+++ codeforces/1234/D.cpp
@@ -12,6 +12,196 @@
 using ll = int64_t;
 using namespace std;
 
+// clang-format off
+template <class Base,
+          class Node,
+          class Update,
+          class MakeNode,
+          class CombineNodes,
+          class ApplyUpdate,
+          class ComposeUpdates = std::nullptr_t>
+struct lazy_segtree {
+    static constexpr bool is_lazy =
+        !std::is_same<ComposeUpdates, std::nullptr_t>::value;
+
+   public:
+    template <typename... T>
+    explicit lazy_segtree(int n, const Base& id_base, T... args)
+        : lazy_segtree(std::vector<Base>(n, id_base), args...) {}
+    explicit lazy_segtree(const std::vector<Base>& v,
+                          const Node& _id_node,
+                          const MakeNode& _make_node,
+                          const CombineNodes& _combine,
+                          const Update& _id_update,
+                          const ApplyUpdate& _apply_update,
+                          const ComposeUpdates& _compose_updates = nullptr)
+        : _n(int(v.size())),
+          make_node(_make_node),
+          combine(_combine),
+          id_node(_id_node),
+          apply_update(_apply_update),
+          id_update(_id_update),
+          compose_updates(_compose_updates) {
+        build(v);
+    }
+
+    void build(const std::vector<Base>& v) {
+        _n = int(v.size());
+        log = 0;
+        while ((1 << log) < _n) ++log;
+        size = 1 << log;
+        d = std::vector<Node>(2 * size, id_node);
+        if constexpr (is_lazy) lz = std::vector<Update>(size, id_update);
+        for (int i = 0; i < _n; i++) d[size + i] = make_node(v[i], i);
+        for (int i = size - 1; i >= 1; i--) update(i);
+    }
+    
+    void set(int p, Node x) {
+        p += size;
+        if constexpr (is_lazy)
+            for (int i = log; i >= 1; i--) push(p >> i);
+        d[p] = x;
+        for (int i = 1; i <= log; ++i) update(p >> i);
+    }
+
+    Node get(int p) {
+        p += size;
+        if constexpr (is_lazy)
+            for (int i = log; i >= 1; i--) push(p >> i);
+        return d[p];
+    }
+
+    Node query(int l, int r) {
+        if (l == r) return id_node;
+        l += size, r += size;
+        if constexpr (is_lazy) {
+            int l_ctz = __builtin_ctz(l);
+            int r_ctz = __builtin_ctz(r);
+            for (int i = log; i > l_ctz; --i) push(l >> i);
+            for (int i = log; i > r_ctz; --i) push((r - 1) >> i);
+        }
+        Node sml = id_node, smr = id_node;
+        while (l < r) {
+            if (l & 1) sml = combine(sml, d[l++]);
+            if (r & 1) smr = combine(d[--r], smr);
+            l >>= 1, r >>= 1;
+        }
+        return combine(sml, smr);
+    }
+    
+    Node all_query() const { return d[1]; }
+    
+    void update(int p, Update f) {
+        p += size;
+        if constexpr (is_lazy)
+            for (int i = log; i >= 1; i--) push(p >> i);
+        d[p] = apply_update(f, d[p]);
+        for (int i = 1; i <= log; ++i) update(p >> i);
+    }
+    
+    void update(int l, int r, Update f) {
+        if (l == r) return;
+        l += size, r += size;
+        const int l_ctz = __builtin_ctz(l);
+        const int r_ctz = __builtin_ctz(r);
+        if constexpr (is_lazy) {
+            for (int i = log; i > l_ctz; --i) push(l >> i);
+            for (int i = log; i > r_ctz; --i) push((r - 1) >> i);
+        }
+        {
+            const int l2 = l, r2 = r;
+            while (l < r) {
+                if (l & 1) all_apply(l++, f);
+                if (r & 1) all_apply(--r, f);
+                l >>= 1, r >>= 1;
+            }
+            l = l2, r = r2;
+        }
+        for (int i = l_ctz + 1; i <= log; ++i) update(l >> i);
+        for (int i = r_ctz + 1; i <= log; ++i) update((r - 1) >> i);
+    }
+
+    template <class G>
+    int max_right(int l, G g) {
+        // assert(0 <= l && l <= _n);
+        // assert(g(id_node));
+        if (l == _n) return _n;
+        l += size;
+        if constexpr (is_lazy)
+            for (int i = log; i >= 1; i--) push(l >> i);
+        Node sm = id_node;
+        do {
+            while (l % 2 == 0) l >>= 1;
+            if (!g(combine(sm, d[l]))) {
+                while (l < size) {
+                    if constexpr (is_lazy) push(l);
+                    l = (2 * l);
+                    if (g(combine(sm, d[l]))) {
+                        sm = combine(sm, d[l]);
+                        l++;
+                    }
+                }
+                return l - size;
+            }
+            sm = combine(sm, d[l]);
+            l++;
+        } while ((l & -l) != l);
+        return _n;
+    }
+
+    template <class G>
+    int min_left(int r, G g) {
+        // assert(0 <= r && r <= _n);
+        // assert(g(id_node));
+        if (r == 0) return 0;
+        r += size;
+        if constexpr (is_lazy)
+            for (int i = log; i >= 1; i--) push((r - 1) >> i);
+        Node sm = id_node;
+        do {
+            r--;
+            while (r > 1 && (r % 2)) r >>= 1;
+            if (!g(combine(d[r], sm))) {
+                while (r < size) {
+                    if constexpr (is_lazy) push(r);
+                    r = (2 * r + 1);
+                    if (g(combine(d[r], sm))) {
+                        sm = combine(d[r], sm);
+                        r--;
+                    }
+                }
+                return r + 1 - size;
+            }
+            sm = combine(d[r], sm);
+        } while ((r & -r) != r);
+        return 0;
+    }
+
+   private:
+    int _n, size, log;
+    std::vector<Node> d;
+    std::vector<Update> lz;
+    MakeNode make_node;
+    CombineNodes combine;
+    Node id_node;
+    ApplyUpdate apply_update;
+    Update id_update;
+    ComposeUpdates compose_updates;
+
+    void update(int k) { d[k] = combine(d[2 * k], d[2 * k + 1]); }
+    void all_apply(int k, Update f) {
+        d[k] = apply_update(f, d[k]);
+        if constexpr (is_lazy)
+            if (k < size) lz[k] = compose_updates(f, lz[k]);
+    }
+    void push(int k) {
+        all_apply(2 * k, lz[k]);
+        all_apply(2 * k + 1, lz[k]);
+        lz[k] = id_update;
+    }
+};
+// clang-format on
+
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
     int t = 1;
@@ -19,29 +209,32 @@ int main() {
     for (int test = 1; test <= t; ++test) {
         string s;
         cin >> s;
-        int n = s.size();
-        constexpr int block_size = 300;
-        vector<array<int, 26>> block_prefixes(s.size());
-        array<int, 26> freq_empty;
-        fill(begin(freq_empty), end(freq_empty), 0);
-        int total_blocks = (s.size() + block_size - 1) / block_size;
-        for (int i = 0; i < total_blocks; ++i) {
-            int left = i * block_size;
-            int right = (i + 1) * block_size - 1;
-            if (right >= n) right = n - 1;
-            auto freq = freq_empty;
-            for (int j = left; j <= right; ++j) freq[s[j] - 'a']++, block_prefixes[j] = freq;
-        }
-        auto join_blocks = [](const array<int, 26>& a, const array<int, 26>& b) {
-            auto c = a;
-            for (int i = 0; i < 26; ++i) c[i] += b[i];
+        vector<char> a(begin(s), end(s));
+        using Base = char;
+        using Node = std::array<int, 26>;
+        using Update = int;
+        auto empty_array = [] {
+            Node A;
+            fill(begin(A), end(A), 0);
+            return A;
+        };
+        const Node id_node = empty_array();
+        const auto make_node = [&id_node](const Base& b, const int& i) -> Node {
+            Node N = id_node;
+            N[b - 'a'] = 1;
+            return N;
+        };
+        const auto combine = [](const Node& a, const Node& b) -> Node {
+            Node c;
+            for (int i = 0; i < 26; ++i) c[i] = a[i] + b[i];
             return c;
         };
-        auto work = [](const array<int, 26>& a) {
-            int distinct = 0;
-            for (int i = 0; i < 26; ++i) distinct += (a[i] != 0);
-            return distinct;
+        const Update id_update = 0;
+        const auto apply_update = [](const Update& u, const Node& n) -> Node {
+            return n;
         };
+        lazy_segtree st(a, id_node, make_node, combine, id_update,
+                        apply_update);
         int q;
         cin >> q;
         while (q--) {
@@ -51,41 +244,20 @@ int main() {
                 int pos;
                 char c;
                 cin >> pos >> c;
-                pos--;
-                s[pos] = c;
-                int block_id = pos / block_size;
-                int block_end = min(n - 1, (block_id + 1) * block_size - 1);
-                auto prv = freq_empty;
-                if (pos % block_size) prv = block_prefixes[pos - 1];
-                for (int j = pos; j <= block_end; ++j) {
-                    prv[s[j] - 'a']++;
-                    block_prefixes[j] = prv;
-                }
+                --pos;
+                Node update = id_node;
+                update[c - 'a']++;
+                st.set(pos, update);
             } else {
                 int l, r;
                 cin >> l >> r;
-                --l, --r;
-                int block_id_l = l / block_size;
-                int block_id_r = r / block_size;
-                if (block_id_l == block_id_r) {
-                    auto freq = freq_empty;
-                    for (int i = l; i <= r; ++i) {
-                        freq[s[i] - 'a']++;
-                    }
-                    cout << work(freq) << '\n';
-                } else {
-                    auto chunk = freq_empty;
-                    for (int i = block_id_l + 1; i < block_id_r; ++i) {
-                        chunk = join_blocks(chunk, block_prefixes[(i + 1) * block_size - 1]);
-                    }
-                    chunk = join_blocks(chunk, block_prefixes[r]);
-                    auto pre = freq_empty;
-                    if (l % block_size) pre = block_prefixes[l - 1];
-                    auto left_thing = block_prefixes[(block_id_l + 1) * block_size - 1];
-                    for (int i = 0; i < 26; ++i) left_thing[i] -= pre[i];
-                    chunk = join_blocks(chunk, left_thing);
-                    cout << work(chunk) << '\n';
-                }
+                --l;
+                Node x = st.query(l, r);
+                debug(x);
+                int distinct = 0;
+                for (int i = 0; i < 26; ++i)
+                    if (x[i]) distinct++;
+                cout << distinct << '\n';
             }
         }
     }
