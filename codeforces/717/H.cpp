65a500c2b92e6cc6f898e8a52c83402754681f3d
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2021-08-07 00:04:00 +0530

    Add solution for problem `H - Pokermon League challenge` Link: https://codeforces.com/contest/717/problem/H Tags: math, probabilities, *2400 Ref: https://codeforces.com/contest/717/submission/125082509

---
+++ codeforces/717/H.cpp
@@ -0,0 +1,280 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx,avx2,sse,sse2,sse3,sse4,popcnt,bmi,bmi2,lzcnt")
+
+#include <bits/stdc++.h>
+
+#ifdef DEBUG
+    #include "../debug/debug.hpp"
+#else
+    #define debug(...) 0
+#endif
+
+using ll = int64_t;
+using namespace std;
+
+template <class T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+namespace IO {
+    constexpr bool UNSAFE = false;
+    constexpr int GLOB_BUF_SIZE = 1 << 15;
+#ifndef DEBUG
+    #define CHANGE_DEFAULT_STREAMS
+    static struct FastInput {
+        FastInput() {
+            if constexpr (UNSAFE) {
+                chars_read = fread(buf, 1, BUF_SIZE, in);
+                buf_pos = 0;
+                buf[0] = (chars_read == 0 ? -1 : buf[0]);
+            }
+        }
+        static constexpr int BUF_SIZE = GLOB_BUF_SIZE;
+        char buf[BUF_SIZE];
+        size_t chars_read = 0;
+        size_t buf_pos = 0;
+        FILE* in = stdin;
+        char cur = 0;
+        inline char get_char() {
+            if constexpr (!UNSAFE) {
+                if (buf_pos >= chars_read) {
+                    chars_read = fread(buf, 1, BUF_SIZE, in);
+                    buf_pos = 0;
+                    buf[0] = (chars_read == 0 ? -1 : buf[0]);
+                }
+            }
+            return cur = buf[buf_pos++];
+        }
+        template <typename T>
+        inline FastInput* tie(T) { return this; }
+        inline void sync_with_stdio(bool) {}
+        inline explicit operator bool() { return cur != -1; }
+        inline static bool is_blank(char c) { return c <= ' '; }
+        inline bool skip_blanks() {
+            while (is_blank(cur) && cur != -1) get_char();
+            return cur != -1;
+        }
+        inline FastInput& operator>>(char& c) {
+            skip_blanks();
+            c = cur;
+            return *this;
+        }
+        inline FastInput& operator>>(std::string& s) {
+            if (skip_blanks()) {
+                s.clear();
+                do {
+                    s += cur;
+                } while (!is_blank(get_char()));
+            }
+            return *this;
+        }
+        template <typename T>
+        inline FastInput& read_integer(T& n) {
+            // unsafe, doesn't check that characters are actually digits
+            n = 0;
+            if (skip_blanks()) {
+                int sign = +1;
+                if (cur == '-') {
+                    sign = -1;
+                    get_char();
+                }
+                do {
+                    n += n + (n << 3) + cur - '0';
+                } while (!is_blank(get_char()));
+                n *= sign;
+            }
+            return *this;
+        }
+        template <typename T>
+        inline typename std::enable_if<std::is_integral<T>::value,
+                                       FastInput&>::type
+        operator>>(T& n) {
+            return read_integer(n);
+        }
+    #if !defined(_WIN32) || defined(_WIN64)
+        inline FastInput& operator>>(__int128& n) { return read_integer(n); }
+    #endif
+        template <typename T>
+        inline typename std::enable_if<std::is_floating_point<T>::value,
+                                       FastInput&>::type
+        operator>>(T& n) {
+            // not sure if really fast, for compatibility only
+            n = 0;
+            if (skip_blanks()) {
+                std::string s;
+                (*this) >> s;
+                sscanf(s.c_str(), "%lf", &n);
+            }
+            return *this;
+        }
+    } fast_input;
+    #define cin IO::fast_input
+    static struct FastOutput {
+        static constexpr int BUF_SIZE = GLOB_BUF_SIZE;
+        char buf[BUF_SIZE];
+        size_t buf_pos = 0;
+        static constexpr int TMP_SIZE = GLOB_BUF_SIZE;
+        char tmp[TMP_SIZE];
+        FILE* out = stdout;
+        inline void put_char(char c) {
+            buf[buf_pos++] = c;
+            if (buf_pos == BUF_SIZE) {
+                fwrite(buf, 1, buf_pos, out);
+                buf_pos = 0;
+            }
+        }
+        ~FastOutput() { fwrite(buf, 1, buf_pos, out); }
+        inline FastOutput& operator<<(char c) {
+            put_char(c);
+            return *this;
+        }
+        inline FastOutput& operator<<(const char* s) {
+            while (*s) put_char(*s++);
+            return *this;
+        }
+        inline FastOutput& operator<<(const std::string& s) {
+            for (auto x : s) put_char(x);
+            return *this;
+        }
+        template <typename T>
+        inline char* integer_to_string(T n) {
+            // beware of TMP_SIZE
+            char* p = tmp + TMP_SIZE - 1;
+            if (n == 0)
+                *--p = '0';
+            else {
+                bool is_negative = false;
+                if (n < 0) {
+                    is_negative = true;
+                    n = -n;
+                }
+                while (n > 0) {
+                    *--p = (char)('0' + n % 10);
+                    n /= 10;
+                }
+                if (is_negative) *--p = '-';
+            }
+            return p;
+        }
+        template <typename T>
+        inline typename std::enable_if<std::is_integral<T>::value, char*>::type
+        stringify(T n) {
+            return integer_to_string(n);
+        }
+    #if !defined(_WIN32) || defined(_WIN64)
+        inline char* stringify(__int128 n) { return integer_to_string(n); }
+    #endif
+        template <typename T>
+        inline typename std::enable_if<std::is_floating_point<T>::value,
+                                       char*>::type
+        stringify(T n) {
+            sprintf(tmp, "%.17f", n);
+            return tmp;
+        }
+        template <typename T>
+        inline FastOutput& operator<<(const T& n) {
+            auto p = stringify(n);
+            for (; *p != 0; p++) put_char(*p);
+            return *this;
+        }
+    } fast_output;
+    #define cout IO::fast_output
+#endif
+}  // namespace IO
+
+struct Random : std::mt19937 {
+    using std::mt19937::mt19937;
+    using std::mt19937::operator();
+    static int64_t gen_seed() {
+        return std::chrono::steady_clock::now().time_since_epoch().count();
+    }
+    Random() : std::mt19937(gen_seed()) {}
+    template <class Int>
+    auto operator()(Int a, Int b)
+        -> std::enable_if_t<std::is_integral_v<Int>, Int> {
+        return std::uniform_int_distribution<Int>(a, b)(*this);
+    }
+    template <class Int>
+    auto operator()(Int a) -> std::enable_if_t<std::is_integral_v<Int>, Int> {
+        return std::uniform_int_distribution<Int>(0, a - 1)(*this);
+    }
+    template <class Real>
+    auto operator()(Real a, Real b)
+        -> std::enable_if_t<std::is_floating_point_v<Real>, Real> {
+        return std::uniform_real_distribution<Real>(a, b)(*this);
+    }
+};
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    int t = 1;
+    // cin >> t;
+    for (int test = 1; test <= t; ++test) {
+        int n, e;
+        cin >> n >> e;
+        vector<vector<int>> g(n);
+        for (int i = 0; i < e; ++i) {
+            int u, v;
+            cin >> u >> v;
+            --u, --v;
+            g[u].push_back(v);
+            g[v].push_back(u);
+        }
+        vector<int> conference(n, -1);
+        for (int i = 0; i < n; ++i) {
+            int cnt[2]{0, 0};
+            for (auto u : g[i])
+                if (conference[u] != -1) cnt[conference[u]]++;
+            if (cnt[1] >= cnt[0])
+                conference[i] = 0;
+            else
+                conference[i] = 1;
+        }
+        vector<vector<int>> wish(n);
+        vector<int> cnt(1'000'000);
+        int T = 0;
+        for (int i = 0; i < n; ++i) {
+            int x;
+            cin >> x;
+            while (x--) {
+                int u;
+                cin >> u;
+                wish[i].push_back(u - 1);
+                cnt[u - 1]++;
+                T = max(T, u);
+            }
+        }
+        vector<int> good_teams;
+        for (int i = 0; i < T; ++i)
+            if (cnt[i]) good_teams.push_back(i);
+        Random rng;
+        while (true) {
+            random_shuffle(begin(good_teams), end(good_teams), rng);
+            vector<int> location(T);
+            for (auto x : good_teams) location[x] = rng(0, 1);
+            vector<int> assignment(n, -1);
+            bool found = true;
+            for (int i = 0; i < n; ++i) {
+                for (auto team : wish[i]) {
+                    if (location[team] == conference[i]) {
+                        assignment[i] = team;
+                        break;
+                    }
+                }
+                if (assignment[i] == -1) {
+                    found = false;
+                    break;
+                }
+            }
+            if (found) {
+                for (int i = 0; i < n; ++i)
+                    cout << assignment[i] + 1 << " \n"[i == n - 1];
+                vector<int> teamconf(T);
+                for (int i = 0; i < n; ++i) teamconf[assignment[i]] = conference[i];
+                for (int i = 0; i < T; ++i)
+                    cout << teamconf[i] + 1 << " \n"[i == T - 1];
+                return 0;
+            }
+        }
+    }
+}
+
