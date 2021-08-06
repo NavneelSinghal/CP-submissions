124b117a562ddf37b9c3a102c5c6479b5cd77219
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2021-08-06 21:09:00 +0530

    Add solution for problem `B - Nick and Array` Link: https://codeforces.com/contest/1180/problem/B Tags: greedy, implementation, *1500 Ref: https://codeforces.com/contest/1180/submission/125068533

--- codeforces/1180/B.cpp
+++ codeforces/1180/B.cpp
@@ -1,26 +1,35 @@
-#include<bits/stdc++.h>
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx,avx2,sse,sse2,sse3,sse4,popcnt,bmi,bmi2,lzcnt")
+
+#include <bits/stdc++.h>
+
+#ifdef DEBUG
+    #include "../debug/debug.hpp"
+#else
+    #define debug(...) 0
+#endif
+
+using ll = int64_t;
 using namespace std;
-int main(){
-    int n;
-    cin>>n;vector<int> a(n);
-    for(int i = 0; i<n; i++){
-        cin>>a[i];
-        if(a[i]>=0){
-            a[i] = -a[i]-1;
+
+template <class T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    int t = 1;
+    // cin >> t;
+    for (int test = 1; test <= t; ++test) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (auto& x : a) {
+            cin >> x;
+            if (x >= 0) (x *= -1) -= 1;
         }
-    }
-    int mn = a[0];
-    int idx = 0;
-    if(n%2!=0){
-        for(int i = 0; i<n; i++){
-            if(a[i]<mn){
-                idx = i;
-                mn = min(mn, a[i]);
-            }
-        }
-        a[idx] = -a[idx] - 1;
-    }
-    for(int i; i<n; i++){
-        cout<<a[i]<<" ";
+        if (n & 1) (*min_element(begin(a), end(a)) *= -1) -= 1;
+        for (auto x : a) cout << x << ' ';
+        cout << '\n';
     }
 }
+
