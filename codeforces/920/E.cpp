f1e729b646b18a59014f41c708be1e7d3347aee1
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2021-08-12 20:11:00 +0530

    Add solution for problem `E - Connected Components?` Link: https://codeforces.com/contest/920/problem/E Tags: data structures, dfs and similar, dsu, graphs, *2100 Ref: https://codeforces.com/contest/920/submission/125679159

--- codeforces/920/E.cpp
+++ codeforces/920/E.cpp
@@ -7,57 +7,44 @@ int main() {
 
     int n, m;
     cin >> n >> m;
-    vector<vector<int>> g(n);
-    for (int i = 0; i < m; ++i) {
-        int u, v;
+
+    vector<set<int>> g(n);
+    set<int> not_visited;
+
+    for (int i = 0; i < n; ++i) not_visited.insert(i);
+    for (int i = 0, u, v; i < m; ++i) {
         cin >> u >> v;
         --u, --v;
-        g[u].push_back(v);
-        g[v].push_back(u);
+        g[u].insert(v);
+        g[v].insert(u);
     }
-
-    vector component(n, -1);
-    vector<int> component_sz;
-    int cur_component = -1;
     
-    vector<int> unvisited_vertices(n);
-    iota(begin(unvisited_vertices), end(unvisited_vertices), 0);
+    vector<vector<int>> components;
     
-    vector<int> unvisited_but_unreachable(n);
-    queue<int> q;
+    const auto dfs = [&](const auto& self, int u) {
+        components.back().push_back(u);
+        not_visited.erase(u);
+        if (not_visited.empty()) return;
+        auto it = not_visited.begin();
+        while (true) {
+            int v = *it;
+            if (g[u].find(v) == g[u].end()) self(self, v);
+            it = not_visited.upper_bound(v);
+            if (it == not_visited.end()) break;
+        }
+    };
     
     for (int i = 0; i < n; ++i) {
-        if (component[i] != -1) continue;
-        cur_component++;
-        component_sz.push_back(1);
-        component[i] = cur_component;
-        q.push(i);
-        while (!q.empty()) {
-            int u = q.front();
-            q.pop();
-            vector<int> remaining_to_visit;
-            for (auto v : g[u])
-                if (component[v] == -1)
-                    remaining_to_visit.push_back(v),
-                        unvisited_but_unreachable[v] = 1;
-            for (auto v : unvisited_vertices) {
-                if (u == v) continue;
-                if (unvisited_but_unreachable[v]) continue;
-                component[v] = cur_component;
-                ++component_sz.back();
-                q.push(v);
-            }
-            // clear out the buffer
-            for (auto v : remaining_to_visit) unvisited_but_unreachable[v] = 0;
-            // size of remaining_to_visit is at most deg(u)
-            // so complexity remains O(V + E)
-            unvisited_vertices = move(remaining_to_visit);
+        if (not_visited.find(i) != not_visited.end()) {
+            components.emplace_back();
+            dfs(dfs, i);
         }
     }
-
-    sort(begin(component_sz), end(component_sz));
-    cout << component_sz.size() << '\n';
-    for (auto x : component_sz) cout << x << ' ';
+    
+    cout << components.size() << '\n';
+    vector<int> sz;
+    for (auto& c : components) sz.push_back(c.size());
+    sort(begin(sz), end(sz));
+    for (auto x : sz) cout << x << ' ';
     cout << '\n';
 }
-
