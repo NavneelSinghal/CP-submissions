9209644a705a12c06436392e344ddd1312501567
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2021-08-12 17:12:00 +0530

    Add solution for problem `E - Connected Components?` Link: https://codeforces.com/contest/920/problem/E Tags: data structures, dfs and similar, dsu, graphs, *2100 Ref: https://codeforces.com/contest/920/submission/125661983

--- codeforces/920/E.cpp
+++ codeforces/920/E.cpp
@@ -12,6 +12,191 @@
 using ll = int64_t;
 using ld = long double;
 
+template <class T>
+struct simple_queue {
+    std::vector<T> payload;
+    int pos = 0;
+    void reserve(int n) { payload.reserve(n); }
+    int size() const { return int(payload.size()) - pos; }
+    bool empty() const { return pos == int(payload.size()); }
+    void push(const T& t) { payload.push_back(t); }
+    T& front() { return payload[pos]; }
+    void clear() {
+        payload.clear();
+        pos = 0;
+    }
+    void pop() { pos++; }
+};
+
+namespace IO {
+    constexpr bool UNSAFE = false;
+    constexpr int GLOB_BUF_SIZE = 1 << 15;
+#ifndef DEBUG
+    #define CHANGE_DEFAULT_STREAMS
+    static struct FastInput {
+        FastInput() {
+            if constexpr (UNSAFE) {
+                chars_read = fread(buf, 1, BUF_SIZE, in);
+                buf_pos = 0;
+                buf[0] = (chars_read == 0 ? -1 : buf[0]);
+            }
+        }
+        static constexpr int BUF_SIZE = GLOB_BUF_SIZE;
+        char buf[BUF_SIZE];
+        size_t chars_read = 0;
+        size_t buf_pos = 0;
+        FILE* in = stdin;
+        char cur = 0;
+        inline char get_char() {
+            if constexpr (!UNSAFE) {
+                if (buf_pos >= chars_read) {
+                    chars_read = fread(buf, 1, BUF_SIZE, in);
+                    buf_pos = 0;
+                    buf[0] = (chars_read == 0 ? -1 : buf[0]);
+                }
+            }
+            return cur = buf[buf_pos++];
+        }
+        template <typename T>
+        inline FastInput* tie(T) {
+            return this;
+        }
+        inline void sync_with_stdio(bool) {}
+        inline explicit operator bool() { return cur != -1; }
+        inline static bool is_blank(char c) { return c <= ' '; }
+        inline bool skip_blanks() {
+            while (is_blank(cur) && cur != -1) get_char();
+            return cur != -1;
+        }
+        inline FastInput& operator>>(char& c) {
+            skip_blanks();
+            c = cur;
+            get_char();
+            return *this;
+        }
+        inline FastInput& operator>>(std::string& s) {
+            if (skip_blanks()) {
+                s.clear();
+                do {
+                    s += cur;
+                } while (!is_blank(get_char()));
+            }
+            return *this;
+        }
+        template <typename T>
+        inline FastInput& read_integer(T& n) {
+            // unsafe, doesn't check that characters are actually digits
+            n = 0;
+            if (skip_blanks()) {
+                int sign = +1;
+                if (cur == '-') {
+                    sign = -1;
+                    get_char();
+                }
+                do {
+                    n += n + (n << 3) + cur - '0';
+                } while (!is_blank(get_char()));
+                n *= sign;
+            }
+            return *this;
+        }
+        template <typename T>
+        inline typename std::enable_if<std::is_integral<T>::value,
+                                       FastInput&>::type
+        operator>>(T& n) {
+            return read_integer(n);
+        }
+    #if !defined(_WIN32) || defined(_WIN64)
+        inline FastInput& operator>>(__int128& n) { return read_integer(n); }
+    #endif
+        template <typename T>
+        inline typename std::enable_if<std::is_floating_point<T>::value,
+                                       FastInput&>::type
+        operator>>(T& n) {
+            // not sure if really fast, for compatibility only
+            n = 0;
+            if (skip_blanks()) {
+                std::string s;
+                (*this) >> s;
+                sscanf(s.c_str(), "%lf", &n);
+            }
+            return *this;
+        }
+    } fast_input;
+    #define cin IO::fast_input
+    static struct FastOutput {
+        static constexpr int BUF_SIZE = GLOB_BUF_SIZE;
+        char buf[BUF_SIZE];
+        size_t buf_pos = 0;
+        static constexpr int TMP_SIZE = GLOB_BUF_SIZE;
+        char tmp[TMP_SIZE];
+        FILE* out = stdout;
+        inline void put_char(char c) {
+            buf[buf_pos++] = c;
+            if (buf_pos == BUF_SIZE) {
+                fwrite(buf, 1, buf_pos, out);
+                buf_pos = 0;
+            }
+        }
+        ~FastOutput() { fwrite(buf, 1, buf_pos, out); }
+        inline FastOutput& operator<<(char c) {
+            put_char(c);
+            return *this;
+        }
+        inline FastOutput& operator<<(const char* s) {
+            while (*s) put_char(*s++);
+            return *this;
+        }
+        inline FastOutput& operator<<(const std::string& s) {
+            for (auto x : s) put_char(x);
+            return *this;
+        }
+        template <typename T>
+        inline char* integer_to_string(T n) {
+            // beware of TMP_SIZE
+            char* p = tmp + TMP_SIZE - 1;
+            if (n == 0)
+                *--p = '0';
+            else {
+                bool is_negative = false;
+                if (n < 0) {
+                    is_negative = true;
+                    n = -n;
+                }
+                while (n > 0) {
+                    *--p = (char)('0' + n % 10);
+                    n /= 10;
+                }
+                if (is_negative) *--p = '-';
+            }
+            return p;
+        }
+        template <typename T>
+        inline typename std::enable_if<std::is_integral<T>::value, char*>::type
+        stringify(T n) {
+            return integer_to_string(n);
+        }
+    #if !defined(_WIN32) || defined(_WIN64)
+        inline char* stringify(__int128 n) { return integer_to_string(n); }
+    #endif
+        template <typename T>
+        inline typename std::enable_if<std::is_floating_point<T>::value,
+                                       char*>::type
+        stringify(T n) {
+            sprintf(tmp, "%.17f", n);
+            return tmp;
+        }
+        template <typename T>
+        inline FastOutput& operator<<(const T& n) {
+            auto p = stringify(n);
+            for (; *p != 0; p++) put_char(*p);
+            return *this;
+        }
+    } fast_output;
+    #define cout IO::fast_output
+#endif
+}  // namespace IO
+
 using namespace std;
 
 int main() {
@@ -21,47 +206,25 @@ int main() {
     for (int test = 1; test <= t; ++test) {
         int n, m;
         cin >> n >> m;
-        using edge = pair<int, int>;
-        vector<edge> edges;
+        vector<vector<int>> g(n);
         for (int i = 0; i < m; ++i) {
             int u, v;
             cin >> u >> v;
             --u, --v;
-            edges.emplace_back(u, v);
-            edges.emplace_back(v, u);
+            g[u].push_back(v);
+            g[v].push_back(u);
         }
-
-        auto get_sorted_graph = [](auto& edges, int n) {
-            // radix sort for edges
-            vector<int> cnt(n);
-            vector<edge> temp(edges.size());
-            for (auto [u, v] : edges) cnt[v]++;
-            for (int i = 1; i < n; ++i) cnt[i] += cnt[i - 1];
-            for (int i = (int)edges.size() - 1; i >= 0; --i)
-                temp[--cnt[edges[i].second]] = edges[i];
-            copy(begin(temp), end(temp), begin(edges));
-            fill(begin(cnt), end(cnt), 0);
-            for (auto [u, v] : edges) cnt[u]++;
-            for (int i = 1; i < n; ++i) cnt[i] += cnt[i - 1];
-            for (int i = (int)edges.size() - 1; i >= 0; --i)
-                temp[--cnt[edges[i].first]] = edges[i];
-            copy(begin(temp), end(temp), begin(edges));
-            vector<vector<int>> g(n);
-            for (auto [u, v] : edges) g[u].push_back(v);
-            return g;
-        };
-
-        auto g = get_sorted_graph(edges, n);
         vector component(n, -1);
         vector<int> component_sz;
         int cur_component = -1;
-        vector<int> unvisited(n);
-        iota(begin(unvisited), end(unvisited), 0);
-
+        vector<int> unvisited_vertices(n);
+        iota(begin(unvisited_vertices), end(unvisited_vertices), 0);
+        vector<int> unvisited_but_unreachable(n);
+        simple_queue<int> q;
+        q.reserve(n);
         for (int i = 0; i < n; ++i) {
             if (component[i] != -1) continue;
             // i is unvisited, so it must belong to a new component
-            queue<int> q;
             cur_component++;
             component_sz.push_back(1);
             component[i] = cur_component;
@@ -69,22 +232,24 @@ int main() {
             while (!q.empty()) {
                 int u = q.front();
                 q.pop();
-                int ptr = 0;
                 vector<int> remaining_to_visit;
-                for (auto v : unvisited) {
-                    if (v == u) continue;
-                    while (ptr < (int)g[u].size() && g[u][ptr] < v) ++ptr;
-                    if (ptr < (int)g[u].size() && v == g[u][ptr]) {
-                        // can't visit this now
-                        remaining_to_visit.push_back(v);
-                    } else {
-                        // visit v
-                        component[v] = cur_component;
-                        ++component_sz.back();
-                        q.push(v);
-                    }
+                for (auto v : g[u])
+                    if (component[v] == -1)
+                        remaining_to_visit.push_back(v),
+                            unvisited_but_unreachable[v] = 1;
+                for (auto v : unvisited_vertices) {
+                    if (u == v) continue;
+                    if (unvisited_but_unreachable[v]) continue;
+                    component[v] = cur_component;
+                    ++component_sz.back();
+                    q.push(v);
                 }
-                unvisited = move(remaining_to_visit);
+                // clear out the buffer
+                for (auto v : remaining_to_visit)
+                    unvisited_but_unreachable[v] = 0;
+                // size of remaining_to_visit is at most deg(u)
+                // so complexity remains O(V + E)
+                unvisited_vertices = move(remaining_to_visit);
             }
         }
         sort(begin(component_sz), end(component_sz));
