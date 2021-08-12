81b6aac835016f3304a4196e7b5ee296a00a3560
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2021-08-12 16:53:00 +0530

    Add solution for problem `E - Connected Components?` Link: https://codeforces.com/contest/920/problem/E Tags: data structures, dfs and similar, dsu, graphs, *2100 Ref: https://codeforces.com/contest/920/submission/125660267

--- codeforces/920/E.cpp
+++ codeforces/920/E.cpp
@@ -14,22 +14,6 @@ using ld = long double;
 
 using namespace std;
 
-template <class T>
-struct simple_queue {
-    std::vector<T> payload;
-    int pos = 0;
-    void reserve(int n) { payload.reserve(n); }
-    int size() const { return int(payload.size()) - pos; }
-    bool empty() const { return pos == int(payload.size()); }
-    void push(const T &t) { payload.push_back(t); }
-    T &front() { return payload[pos]; }
-    void clear() {
-        payload.clear();
-        pos = 0;
-    }
-    void pop() { pos++; }
-};
-
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
     int t = 1;
@@ -51,11 +35,10 @@ int main() {
         vector<int> unvisited_vertices(n);
         iota(begin(unvisited_vertices), end(unvisited_vertices), 0);
         vector<int> unvisited_but_unreachable(n);
-        simple_queue<int> q;
-        q.reserve(n);
         for (int i = 0; i < n; ++i) {
             if (component[i] != -1) continue;
             // i is unvisited, so it must belong to a new component
+            queue<int> q;
             cur_component++;
             component_sz.push_back(1);
             component[i] = cur_component;
