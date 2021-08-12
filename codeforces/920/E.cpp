0254f996ff9d1ace57257103f94b6d4e9c50475c
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2021-08-12 19:53:00 +0530

    Add solution for problem `E - Connected Components?` Link: https://codeforces.com/contest/920/problem/E Tags: data structures, dfs and similar, dsu, graphs, *2100 Ref: https://codeforces.com/contest/920/submission/125677543

--- codeforces/920/E.cpp
+++ codeforces/920/E.cpp
@@ -1,50 +1,94 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx,avx2,sse,sse2,sse3,sse4,popcnt,bmi,bmi2,lzcnt")
+
 #include <bits/stdc++.h>
+
+#ifdef DEBUG
+    #include "../debug/debug.hpp"
+#else
+    #define debug(...) 0
+#endif
+
+using ll = int64_t;
+using ld = long double;
 
 using namespace std;
 
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
-
-    int n, m;
-    cin >> n >> m;
-
-    vector<set<int>> g(n);
-    set<int> not_visited;
-
-    for (int i = 0; i < n; ++i) not_visited.insert(i);
-    for (int i = 0, u, v; i < m; ++i) {
-        cin >> u >> v;
-        --u, --v;
-        g[u].insert(v);
-        g[v].insert(u);
-    }
-    
-    vector<vector<int>> components;
-    
-    const auto dfs = [&](const auto& self, int u) {
-        components.back().push_back(u);
-        not_visited.erase(u);
-        if (not_visited.empty()) return;
-        auto it = not_visited.begin();
-        while (true) {
-            int v = *it;
-            if (g[u].find(v) == g[u].end()) self(self, v);
-            it = not_visited.upper_bound(v);
-            if (it == not_visited.end()) break;
+    int t = 1;
+    // cin >> t;
+    for (int test = 1; test <= t; ++test) {
+        int n, m;
+        cin >> n >> m;
+        using edge = pair<int, int>;
+        vector<edge> edges;
+        for (int i = 0; i < m; ++i) {
+            int u, v;
+            cin >> u >> v;
+            --u, --v;
+            edges.emplace_back(u, v);
+            edges.emplace_back(v, u);
         }
-    };
-    
-    for (int i = 0; i < n; ++i) {
-        if (not_visited.find(i) != not_visited.end()) {
-            components.emplace_back();
+
+        auto get_sorted_graph = [](auto& edges, int n) {
+            // radix sort for edges
+            vector<int> cnt(n);
+            vector<edge> temp(edges.size());
+            for (auto [u, v] : edges) cnt[v]++;
+            for (int i = 1; i < n; ++i) cnt[i] += cnt[i - 1];
+            for (int i = (int)edges.size() - 1; i >= 0; --i)
+                temp[--cnt[edges[i].second]] = edges[i];
+            copy(begin(temp), end(temp), begin(edges));
+            fill(begin(cnt), end(cnt), 0);
+            for (auto [u, v] : edges) cnt[u]++;
+            for (int i = 1; i < n; ++i) cnt[i] += cnt[i - 1];
+            for (int i = (int)edges.size() - 1; i >= 0; --i)
+                temp[--cnt[edges[i].first]] = edges[i];
+            copy(begin(temp), end(temp), begin(edges));
+            vector<vector<int>> g(n);
+            for (auto [u, v] : edges) g[u].push_back(v);
+            return g;
+        };
+
+        auto g = get_sorted_graph(edges, n);
+        vector component(n, -1);
+        vector<int> component_sz;
+        int cur_component = -1;
+        vector<int> chain_next(n + 1);
+        iota(begin(chain_next), end(chain_next), 0);
+
+        auto get_chain_end = [&chain_next](const auto& self, int u) -> int {
+            if (chain_next[u] == u) return u;
+            return chain_next[u] = self(self, chain_next[u]);
+        };
+
+        const auto dfs = [&](const auto& self, int u) -> void {
+            chain_next[u] = u + 1;
+            component[u] = cur_component;
+            component_sz.back()++;
+            int ptr = 0;
+            for (int v = get_chain_end(get_chain_end, 0); v < n;
+                 v = get_chain_end(get_chain_end, v + 1)) {
+                while (ptr < (int)g[u].size() && g[u][ptr] < v) ++ptr;
+                if (ptr < (int)g[u].size() && g[u][ptr] == v) {
+                    continue;
+                } else {
+                    self(self, v);
+                }
+            }
+        };
+
+        for (int i = 0; i < n; ++i) {
+            if (component[i] != -1) continue;
+            cur_component++;
+            component_sz.push_back(0);
             dfs(dfs, i);
         }
+        sort(begin(component_sz), end(component_sz));
+        cout << component_sz.size() << '\n';
+        for (auto x : component_sz) cout << x << ' ';
+        cout << '\n';
     }
-    
-    cout << components.size() << '\n';
-    vector<int> sz;
-    for (auto& c : components) sz.push_back(c.size());
-    sort(begin(sz), end(sz));
-    for (auto x : sz) cout << x << ' ';
-    cout << '\n';
 }
+
