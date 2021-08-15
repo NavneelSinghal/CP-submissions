1b7120dd826c099eacef31fdf9ad4f18b4f90b85
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2021-08-15 13:42:00 +0530

    Add solution for problem `F - One Occurrence` Link: https://codeforces.com/contest/1000/problem/F Tags: data structures, divide and conquer, *2400 Ref: https://codeforces.com/contest/1000/submission/125908193

--- codeforces/1000/F.cpp
+++ codeforces/1000/F.cpp
@@ -14,6 +14,196 @@ using ld = long double;
 
 using namespace std;
 
+// clang-format off
+template <class Base,
+          class Node,
+          class Update,
+          class MakeNode,
+          class CombineNodes,
+          class ApplyUpdate,
+          class ComposeUpdates = std::nullptr_t>
+struct lazy_segtree {
+    static constexpr bool is_lazy =
+        !std::is_same<ComposeUpdates, std::nullptr_t>::value;
+
+   public:
+    template <typename... T>
+    explicit lazy_segtree(int n, const Base& id_base, T... args)
+        : lazy_segtree(std::vector<Base>(n, id_base), args...) {}
+    explicit lazy_segtree(const std::vector<Base>& v,
+                          const Node& _id_node,
+                          const MakeNode& _make_node,
+                          const CombineNodes& _combine,
+                          const Update& _id_update,
+                          const ApplyUpdate& _apply_update,
+                          const ComposeUpdates& _compose_updates = nullptr)
+        : _n(int(v.size())),
+          make_node(_make_node),
+          combine(_combine),
+          id_node(_id_node),
+          apply_update(_apply_update),
+          id_update(_id_update),
+          compose_updates(_compose_updates) {
+        build(v);
+    }
+
+    void build(const std::vector<Base>& v) {
+        _n = int(v.size());
+        log = 0;
+        while ((1 << log) < _n) ++log;
+        size = 1 << log;
+        d = std::vector<Node>(2 * size, id_node);
+        if constexpr (is_lazy) lz = std::vector<Update>(size, id_update);
+        for (int i = 0; i < _n; i++) d[size + i] = make_node(v[i], i);
+        for (int i = size - 1; i >= 1; i--) update(i);
+    }
+    
+    void set(int p, Node x) {
+        p += size;
+        if constexpr (is_lazy)
+            for (int i = log; i >= 1; i--) push(p >> i);
+        d[p] = x;
+        for (int i = 1; i <= log; ++i) update(p >> i);
+    }
+
+    Node get(int p) {
+        p += size;
+        if constexpr (is_lazy)
+            for (int i = log; i >= 1; i--) push(p >> i);
+        return d[p];
+    }
+
+    Node query(int l, int r) {
+        if (l == r) return id_node;
+        l += size, r += size;
+        if constexpr (is_lazy) {
+            int l_ctz = __builtin_ctz(l);
+            int r_ctz = __builtin_ctz(r);
+            for (int i = log; i > l_ctz; --i) push(l >> i);
+            for (int i = log; i > r_ctz; --i) push((r - 1) >> i);
+        }
+        Node sml = id_node, smr = id_node;
+        while (l < r) {
+            if (l & 1) sml = combine(sml, d[l++]);
+            if (r & 1) smr = combine(d[--r], smr);
+            l >>= 1, r >>= 1;
+        }
+        return combine(sml, smr);
+    }
+    
+    Node all_query() const { return d[1]; }
+    
+    void update(int p, Update f) {
+        p += size;
+        if constexpr (is_lazy)
+            for (int i = log; i >= 1; i--) push(p >> i);
+        d[p] = apply_update(f, d[p]);
+        for (int i = 1; i <= log; ++i) update(p >> i);
+    }
+    
+    void update(int l, int r, Update f) {
+        if (l == r) return;
+        l += size, r += size;
+        const int l_ctz = __builtin_ctz(l);
+        const int r_ctz = __builtin_ctz(r);
+        if constexpr (is_lazy) {
+            for (int i = log; i > l_ctz; --i) push(l >> i);
+            for (int i = log; i > r_ctz; --i) push((r - 1) >> i);
+        }
+        {
+            const int l2 = l, r2 = r;
+            while (l < r) {
+                if (l & 1) all_apply(l++, f);
+                if (r & 1) all_apply(--r, f);
+                l >>= 1, r >>= 1;
+            }
+            l = l2, r = r2;
+        }
+        for (int i = l_ctz + 1; i <= log; ++i) update(l >> i);
+        for (int i = r_ctz + 1; i <= log; ++i) update((r - 1) >> i);
+    }
+
+    template <class G>
+    int max_right(int l, G g) {
+        // assert(0 <= l && l <= _n);
+        // assert(g(id_node));
+        if (l == _n) return _n;
+        l += size;
+        if constexpr (is_lazy)
+            for (int i = log; i >= 1; i--) push(l >> i);
+        Node sm = id_node;
+        do {
+            while (l % 2 == 0) l >>= 1;
+            if (!g(combine(sm, d[l]))) {
+                while (l < size) {
+                    if constexpr (is_lazy) push(l);
+                    l = (2 * l);
+                    if (g(combine(sm, d[l]))) {
+                        sm = combine(sm, d[l]);
+                        l++;
+                    }
+                }
+                return l - size;
+            }
+            sm = combine(sm, d[l]);
+            l++;
+        } while ((l & -l) != l);
+        return _n;
+    }
+
+    template <class G>
+    int min_left(int r, G g) {
+        // assert(0 <= r && r <= _n);
+        // assert(g(id_node));
+        if (r == 0) return 0;
+        r += size;
+        if constexpr (is_lazy)
+            for (int i = log; i >= 1; i--) push((r - 1) >> i);
+        Node sm = id_node;
+        do {
+            r--;
+            while (r > 1 && (r % 2)) r >>= 1;
+            if (!g(combine(d[r], sm))) {
+                while (r < size) {
+                    if constexpr (is_lazy) push(r);
+                    r = (2 * r + 1);
+                    if (g(combine(d[r], sm))) {
+                        sm = combine(d[r], sm);
+                        r--;
+                    }
+                }
+                return r + 1 - size;
+            }
+            sm = combine(d[r], sm);
+        } while ((r & -r) != r);
+        return 0;
+    }
+
+   private:
+    int _n, size, log;
+    std::vector<Node> d;
+    std::vector<Update> lz;
+    MakeNode make_node;
+    CombineNodes combine;
+    Node id_node;
+    ApplyUpdate apply_update;
+    Update id_update;
+    ComposeUpdates compose_updates;
+
+    void update(int k) { d[k] = combine(d[2 * k], d[2 * k + 1]); }
+    void all_apply(int k, Update f) {
+        d[k] = apply_update(f, d[k]);
+        if constexpr (is_lazy)
+            if (k < size) lz[k] = compose_updates(f, lz[k]);
+    }
+    void push(int k) {
+        all_apply(2 * k, lz[k]);
+        all_apply(2 * k + 1, lz[k]);
+        lz[k] = id_update;
+    }
+};
+// clang-format on
+
 namespace IO {
     constexpr bool UNSAFE = false;
     constexpr int GLOB_BUF_SIZE = 1 << 15;
@@ -44,9 +234,7 @@ namespace IO {
             return cur = buf[buf_pos++];
         }
         template <typename T>
-        inline FastInput* tie(T) {
-            return this;
-        }
+        inline FastInput* tie(T) { return this; }
         inline void sync_with_stdio(bool) {}
         inline explicit operator bool() { return cur != -1; }
         inline static bool is_blank(char c) { return c <= ' '; }
@@ -183,178 +371,72 @@ namespace IO {
 #endif
 }  // namespace IO
 
-template <class Node, class AL, class AR, class EL, class ER, class Compute>
-struct Mo {
-   private:
-    std::vector<Node> ans;
-    Node id_node;
-    AL add_left;
-    AR add_right;
-    EL erase_left;
-    ER erase_right;
-    Compute compute;
-    struct Query {
-        int l, r;
-        int64_t order;
-        Query() {}
-        inline int64_t hilbert_order(int x, int y, int p, int to_rotate) {
-            if (p == 0) return 0;
-            static constexpr int rotate_delta[4] = {3, 0, 0, 1};
-            int hp = 1 << (p - 1);
-            int seg = (x < hp) ? ((y < hp) ? 0 : 3) : ((y < hp) ? 1 : 2);
-            seg = (seg + to_rotate) & 3;
-            int nx = x & (x ^ hp), ny = y & (y ^ hp);
-            int nrot = (to_rotate + rotate_delta[seg]) & 3;
-            int64_t sub_square_size = int64_t(1) << (2 * p - 2);
-            int64_t ans = seg * sub_square_size;
-            int64_t add = hilbert_order(nx, ny, p - 1, nrot);
-            ans += (seg == 1 || seg == 2) ? add : (sub_square_size - add - 1);
-            return ans;
-        }
-        Query(int _l, int _r, int lgn) : l(_l), r(_r) {
-            order = hilbert_order(_l, _r, lgn, 0);
-        }
-        bool operator<(const Query& q) const { return order < q.order; }
-    };
-    template <class T, bool unsafe = false>
-    __attribute__((target("bmi,bmi2,popcnt,lzcnt"))) constexpr uint32_t clz(
-        T a) {
-        if constexpr (!unsafe) {
-            if (!a) return sizeof(T) * 8;
-        }
-        if constexpr (sizeof(T) <= sizeof(uint32_t)) {
-            return (uint32_t)(__builtin_clz((uint32_t)a));
-        } else if constexpr (sizeof(T) <= sizeof(uint64_t)) {
-            return (uint32_t)(__builtin_clzll((uint64_t)a));
-        } else {
-            static_assert(sizeof(T) == sizeof(uint64_t) * 2);
-            uint32_t l = clz((uint64_t)(a >> sizeof(uint64_t) * 8));
-            return l != sizeof(uint64_t) * 8 ? l : l + clz((uint64_t)a);
-        }
-    }
-    template <class T, bool unsafe = false>
-    __attribute__((target("bmi,bmi2,popcnt,lzcnt"))) constexpr uint32_t lg(
-        T a) {
-        if constexpr (sizeof(T) <= sizeof(uint32_t)) {
-            return sizeof(uint32_t) * 8 - 1 - clz<T, unsafe>(a);
-        } else if constexpr (sizeof(T) <= sizeof(uint64_t)) {
-            return sizeof(uint64_t) * 8 - 1 - clz<T, unsafe>(a);
-        } else {
-            return sizeof(uint64_t) * 16 - 1 - clz<T, unsafe>(a);
-        }
-    }
-
-   public:
-    Mo(int _n, const std::vector<std::pair<int, int>>& _queries,
-       const Node& _id_node, const AL& _add_left, const AR& _add_right,
-       const EL& _erase_left, const ER& _erase_right, const Compute& _compute)
-        : ans(_queries.size()),
-          id_node(_id_node),
-          add_left(_add_left),
-          add_right(_add_right),
-          erase_left(_erase_left),
-          erase_right(_erase_right),
-          compute(_compute) {
-        int q = (int)_queries.size();
-        int n = _n;
-        std::vector<Query> queries;
-        queries.reserve(q);
-        int lgn = lg(2 * n - 1);
-        for (int i = 0; i < q; ++i) {
-            auto [l, r] = _queries[i];
-            queries.push_back(Query(l, r, lgn));
-        }
-        std::vector<int> id(q);
-        std::iota(id.begin(), id.end(), 0);
-        std::sort(id.begin(), id.end(),
-                  [&queries](int i, int j) { return queries[i] < queries[j]; });
-        int l = 0, r = -1;
-        Node cur = id_node;
-        for (auto i : id) {
-            auto [ql, qr, _] = queries[i];
-            while (l > ql) add_left(--l, cur);
-            while (r < qr) add_right(++r, cur);
-            while (l < ql) erase_left(l++, cur);
-            while (r > qr) erase_right(r--, cur);
-            ans[i] = compute(cur);
-        }
-    }
-    std::vector<Node> get() { return ans; }
-};
-
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
     int t = 1;
     // cin >> t;
     for (int test = 1; test <= t; ++test) {
+        
         int n;
         cin >> n;
         vector<int> a(n);
         for (auto& x : a) cin >> x;
-
+        
+        const int inf = 1e9;
+        using Base = int;
+        struct Node {
+            int max_prev_idx;
+            int max_prev_at;
+        };
+        const Node id_node{inf, inf};
+        const auto make_node = [&id_node](const Base b, int i) {
+            return id_node;
+        };
+        const auto combine = [](const Node& a, const Node& b) -> Node {
+            if (a.max_prev_idx < b.max_prev_idx)
+                return a;
+            else
+                return b;
+        };
+        using Update = int;
+        const Update id_update = 0;
+        const auto apply_update = [&id_node](const Update u, const Node& n) {
+            return id_node;
+        };
+        lazy_segtree st(vector(n, 0), id_node, make_node, combine, id_update,
+                        apply_update);
+        
         int q;
         cin >> q;
-        vector<pair<int, int>> queries;
+        vector<vector<pair<int, int>>> queries(n + 1);
+        
         for (int i = 0; i < q; ++i) {
             int l, r;
             cin >> l >> r;
-            --l, --r;
-            queries.emplace_back(l, r);
+            --l;
+            queries[r].emplace_back(l, i);
         }
-
-        constexpr int block_sz = 800;
-        const int A = *max_element(begin(a), end(a));
-
-        vector<int> type(A + 1), active(A + 1), active_in_block(block_sz);
-        int total_unique = 0;
-
-        using Node = int;
-
-        auto add_left = [&](int i, Node x) {
-            auto ai = a[i];
-            ++type[ai];
-            if (type[ai] == 1) {
-                ++active[ai];
-                ++active_in_block[ai / block_sz];
-                ++total_unique;
-            } else if (type[ai] == 2) {
-                --active[ai];
-                --active_in_block[ai / block_sz];
-                --total_unique;
+        
+        vector<int> ans(q);
+        vector<int> pos(*max_element(begin(a), end(a)) + 1, -1);
+        
+        for (int i = 1; i <= n; ++i) {
+            if (pos[a[i - 1]] != -1) st.set(pos[a[i - 1]], id_node);
+            st.set(i - 1, Node{pos[a[i - 1]], i - 1});
+            pos[a[i - 1]] = i - 1;
+            for (auto [l, index] : queries[i]) {
+                auto [previous, ans_index] = st.query(l, i);
+                if (previous < l) {
+                    ans[index] = a[ans_index];
+                } else {
+                    ans[index] = 0;
+                }
             }
-        };
-
-        auto add_right = add_left;
-
-        auto erase_left = [&](int i, Node x) {
-            auto ai = a[i];
-            --type[ai];
-            if (type[ai] == 0) {
-                --active[ai];
-                --active_in_block[ai / block_sz];
-                --total_unique;
-            } else if (type[ai] == 1) {
-                ++active[ai];
-                ++active_in_block[ai / block_sz];
-                ++total_unique;
-            }
-        };
-
-        auto erase_right = erase_left;
-
-        auto compute = [&](Node x) {
-            if (total_unique == 0) return 0;
-            for (int i = 0; i < block_sz; ++i)
-                if (active_in_block[i] > 0)
-                    for (int j = i * block_sz;; ++j)
-                        if (active[j]) return j;
-            return 0;
-        };
-
-        Mo mo(n, queries, 0, add_left, add_right, erase_left, erase_right,
-              compute);
-
-        for (auto x : mo.get()) cout << x << '\n';
+        }
+        
+        for (auto x : ans) {
+            cout << x << '\n';
+        }
     }
 }
 
