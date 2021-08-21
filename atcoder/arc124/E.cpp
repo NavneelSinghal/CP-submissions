02b984ea9e226b41d60c82c095b09605570e139e
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2021-08-21 14:33:00 +0530

    Add solution for problem `E - Pass to Next` Link: https://atcoder.jp/contests/arc124/tasks/arc124_e Tags: AtCoder, *800 Ref: https://atcoder.jp/contests/arc124/submissions/25188044

--- atcoder/arc124/E.cpp
+++ atcoder/arc124/E.cpp
@@ -135,175 +135,6 @@ struct ModInt32 {
 
 using mint = ModInt32<nttmod>;
 
-namespace IO {
-    constexpr bool UNSAFE = false;
-    constexpr int GLOB_BUF_SIZE = 1 << 15;
-#ifndef DEBUG
-    #define CHANGE_DEFAULT_STREAMS
-    static struct FastInput {
-        FastInput() {
-            if constexpr (UNSAFE) {
-                chars_read = fread(buf, 1, BUF_SIZE, in);
-                buf_pos = 0;
-                buf[0] = (chars_read == 0 ? -1 : buf[0]);
-            }
-        }
-        static constexpr int BUF_SIZE = GLOB_BUF_SIZE;
-        char buf[BUF_SIZE];
-        size_t chars_read = 0;
-        size_t buf_pos = 0;
-        FILE* in = stdin;
-        char cur = 0;
-        inline char get_char() {
-            if constexpr (!UNSAFE) {
-                if (buf_pos >= chars_read) {
-                    chars_read = fread(buf, 1, BUF_SIZE, in);
-                    buf_pos = 0;
-                    buf[0] = (chars_read == 0 ? -1 : buf[0]);
-                }
-            }
-            return cur = buf[buf_pos++];
-        }
-        template <typename T>
-        inline FastInput* tie(T) {
-            return this;
-        }
-        inline void sync_with_stdio(bool) {}
-        inline explicit operator bool() { return cur != -1; }
-        inline static bool is_blank(char c) { return c <= ' '; }
-        inline bool skip_blanks() {
-            while (is_blank(cur) && cur != -1) get_char();
-            return cur != -1;
-        }
-        inline FastInput& operator>>(char& c) {
-            skip_blanks();
-            c = cur;
-            get_char();
-            return *this;
-        }
-        inline FastInput& operator>>(std::string& s) {
-            if (skip_blanks()) {
-                s.clear();
-                do {
-                    s += cur;
-                } while (!is_blank(get_char()));
-            }
-            return *this;
-        }
-        template <typename T>
-        inline FastInput& read_integer(T& n) {
-            // unsafe, doesn't check that characters are actually digits
-            n = 0;
-            if (skip_blanks()) {
-                int sign = +1;
-                if (cur == '-') {
-                    sign = -1;
-                    get_char();
-                }
-                do {
-                    n += n + (n << 3) + cur - '0';
-                } while (!is_blank(get_char()));
-                n *= sign;
-            }
-            return *this;
-        }
-        template <typename T>
-        inline typename std::enable_if<std::is_integral<T>::value,
-                                       FastInput&>::type
-        operator>>(T& n) {
-            return read_integer(n);
-        }
-    #if !defined(_WIN32) || defined(_WIN64)
-        inline FastInput& operator>>(__int128& n) { return read_integer(n); }
-    #endif
-        template <typename T>
-        inline typename std::enable_if<std::is_floating_point<T>::value,
-                                       FastInput&>::type
-        operator>>(T& n) {
-            // not sure if really fast, for compatibility only
-            n = 0;
-            if (skip_blanks()) {
-                std::string s;
-                (*this) >> s;
-                sscanf(s.c_str(), "%lf", &n);
-            }
-            return *this;
-        }
-    } fast_input;
-    #define cin IO::fast_input
-    static struct FastOutput {
-        static constexpr int BUF_SIZE = GLOB_BUF_SIZE;
-        char buf[BUF_SIZE];
-        size_t buf_pos = 0;
-        static constexpr int TMP_SIZE = GLOB_BUF_SIZE;
-        char tmp[TMP_SIZE];
-        FILE* out = stdout;
-        inline void put_char(char c) {
-            buf[buf_pos++] = c;
-            if (buf_pos == BUF_SIZE) {
-                fwrite(buf, 1, buf_pos, out);
-                buf_pos = 0;
-            }
-        }
-        ~FastOutput() { fwrite(buf, 1, buf_pos, out); }
-        inline FastOutput& operator<<(char c) {
-            put_char(c);
-            return *this;
-        }
-        inline FastOutput& operator<<(const char* s) {
-            while (*s) put_char(*s++);
-            return *this;
-        }
-        inline FastOutput& operator<<(const std::string& s) {
-            for (auto x : s) put_char(x);
-            return *this;
-        }
-        template <typename T>
-        inline char* integer_to_string(T n) {
-            // beware of TMP_SIZE
-            char* p = tmp + TMP_SIZE - 1;
-            if (n == 0)
-                *--p = '0';
-            else {
-                bool is_negative = false;
-                if (n < 0) {
-                    is_negative = true;
-                    n = -n;
-                }
-                while (n > 0) {
-                    *--p = (char)('0' + n % 10);
-                    n /= 10;
-                }
-                if (is_negative) *--p = '-';
-            }
-            return p;
-        }
-        template <typename T>
-        inline typename std::enable_if<std::is_integral<T>::value, char*>::type
-        stringify(T n) {
-            return integer_to_string(n);
-        }
-    #if !defined(_WIN32) || defined(_WIN64)
-        inline char* stringify(__int128 n) { return integer_to_string(n); }
-    #endif
-        template <typename T>
-        inline typename std::enable_if<std::is_floating_point<T>::value,
-                                       char*>::type
-        stringify(T n) {
-            sprintf(tmp, "%.17f", n);
-            return tmp;
-        }
-        template <typename T>
-        inline FastOutput& operator<<(const T& n) {
-            auto p = stringify(n);
-            for (; *p != 0; p++) put_char(*p);
-            return *this;
-        }
-    } fast_output;
-    #define cout IO::fast_output
-#endif
-}  // namespace IO
-
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
     int _tests = 1;
@@ -327,29 +158,28 @@ int main() {
             // dp[i][j][k]: number of ways to pick c[1..i-1]
             // j: 0 if we pick 1 from a[0]'s pool, 1 otherwise
             // k: 0 if we pick 1 from a[i]'s pool, 1 otherwise
-            vector dp(n + 1, array<array<mint, 2>, 2>{});
+            vector dp(n + 1, vector(2, vector(2, mint(0))));
             dp[0][0][0] = dp[0][1][1] = 1;
-            for (int i = 0; i < n; ++i) {
-                int ai = a[i];
-                for (auto first : {0, 1}) {
+            for (auto first : {0, 1}) {
+                for (int i = 0; i < n; ++i) {
                     for (auto donated : {0, 1}) {
                         for (auto received : {0, 1}) {
                             mint contribution = 0;
                             if (received) {
                                 if (donated) {
-                                    contribution = get_sum(ai);
+                                    contribution = get_sum(a[i]);
                                 } else {
-                                    contribution = ai - at_least + 1;
+                                    contribution = a[i] - at_least + 1;
                                 }
                             } else {
                                 if (donated) {
-                                    if (ai >= 1)
+                                    if (a[i] >= 1)
                                         contribution =
-                                            ai * get_sum(ai - 1) -
-                                            get_sum_squares(ai - 1);
+                                            a[i] * get_sum(a[i] - 1) -
+                                            get_sum_squares(a[i] - 1);
                                 } else {
-                                    if (ai >= at_least)
-                                        contribution = get_sum(ai - at_least);
+                                    if (a[i] >= at_least)
+                                        contribution = get_sum(a[i] - at_least);
                                 }
                             }
                             dp[i + 1][first][donated] +=
