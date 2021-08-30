c8a654cf015041d6a32c7e880759dca1ad6cd7fa
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2021-08-30 23:47:00 +0530

    Add solution for problem `F - Sports Betting` Link: https://codeforces.com/contest/1556/problem/F Tags: bitmasks, combinatorics, dp, graphs, math, probabilities, *2500 Ref: https://codeforces.com/contest/1556/submission/127471730

--- codeforces/1556/F.cpp
+++ codeforces/1556/F.cpp
@@ -1,6 +1,6 @@
 #ifndef LOCAL
     #pragma GCC optimize("O3,unroll-loops")
-    #pragma GCC target("avx2,bmi2")
+    #pragma GCC target("avx,avx2,sse,sse2,sse3,sse4,popcnt,bmi,bmi2,lzcnt")
 #endif
 
 #include "bits/stdc++.h"
@@ -8,126 +8,8 @@
 #ifdef DEBUG
     #include "includes/debug/debug.hpp"
 #else
-template <typename... T>
-void debug(T&&...) {}
+    #define debug(...) 0
 #endif
-
-using ll = int64_t;
-using ld = long double;
-
-struct IO {
-    IO() = default;
-    IO(const IO&) = delete;
-    IO(IO&&) = delete;
-    IO& operator=(const IO&) = delete;
-    IO& operator=(IO&&) = delete;
-    ~IO() {}
-
-    template <class T, class D = void>
-    struct is_default {
-        static constexpr bool value = false;
-    };
-    template <class T>
-    struct is_default<T,
-                      std::void_t<decltype(std::cout << std::declval<T>())>> {
-        static constexpr bool value = true;
-    };
-    template <class T, class D = void>
-    struct is_iterable {
-        static constexpr bool value = false;
-    };
-    template <class T>
-    struct is_iterable<T,
-                       std::void_t<decltype(std::begin(std::declval<T>()))>> {
-        static constexpr bool value = true;
-    };
-    template <class T, class D = void, class E = void>
-    struct is_applyable {
-        static constexpr bool value = false;
-    };
-    template <class T>
-    struct is_applyable<T, std::void_t<typename std::tuple_size<T>::type>,
-                        std::void_t<decltype(std::get<0>(std::declval<T>()))>> {
-        static constexpr bool value = true;
-    };
-
-    template <class T>
-    struct needs_newline {
-        static constexpr bool value =
-            (is_iterable<T>::value or is_applyable<T>::value) and
-            (not is_default<T>::value);
-    };
-
-    template <typename T, typename U>
-    struct any_needs_newline {};
-    template <typename T>
-    struct any_needs_newline<T, std::index_sequence<>> {
-        static constexpr bool value = false;
-    };
-    template <typename T, std::size_t I, std::size_t... Is>
-    struct any_needs_newline<T, std::index_sequence<I, Is...>> {
-        static constexpr bool value =
-            (needs_newline<decltype(std::get<I>(std::declval<T>()))>::value) ||
-            any_needs_newline<T, std::index_sequence<Is...>>::value;
-    };
-
-    template <typename T_>
-    IO& operator<<(T_&& x) {
-        using T = typename std::remove_cv<typename std::remove_reference<T_>::type>::type;
-        static_assert(is_default<T>::value or is_iterable<T>::value or
-                      is_applyable<T>::value);
-        if constexpr (is_default<T>::value) {
-            std::cout << x;
-        } else if constexpr (is_iterable<T>::value) {
-            // strings are immune
-            using E = decltype(*std::begin(x));
-            constexpr char sep = needs_newline<E>::value ? '\n' : ' ';
-            int i = 0;
-            for (const auto& y : x) {
-                if (i++) std::cout << sep;
-                operator<<(y);
-            }
-        } else if constexpr (is_applyable<T>::value) {
-            // strings are immune
-            constexpr char sep =
-                (any_needs_newline<
-                    T, std::make_index_sequence<std::tuple_size_v<T>>>::value)
-                    ? '\n'
-                    : ' ';
-            int i = 0;
-            std::apply(
-                [this, &sep, &i](auto const&... y) {
-                    ((std::cout << (i++ ? sep : '\0'), this->operator<<(y)),
-                     ...);
-                },
-                x);
-        }
-        return *this;
-    }
-    template <typename T>
-    IO& operator>>(T& x) {
-        static_assert(is_default<T>::value or is_iterable<T>::value or
-                      is_applyable<T>::value);
-        if constexpr (is_default<T>::value)
-            std::cin >> x;
-        else if constexpr (is_iterable<T>::value)
-            for (auto& y : x) operator>>(y);
-        else if constexpr (is_applyable<T>::value)
-            std::apply([this](auto const&... y) { ((operator>>(y)), ...); }, x);
-        return *this;
-    }
-
-    IO* tie(std::nullptr_t) {
-        std::cin.tie(nullptr);
-        return this;
-    }
-    void sync_with_stdio(bool b) { std::cin.sync_with_stdio(b); }
-};
-
-IO io;
-#define cin io
-#define cout io
-
 
 constexpr int mod = int(1e9) + 7;
 constexpr int nttmod = 998'244'353;
@@ -264,30 +146,32 @@ int main() {
         // cout << "Case #" << _test << ": ";
         int n;
         cin >> n;
-        vector<int> a(1 << n);
+        const auto max_mask = 1 << n;
+        vector<int> a(max_mask);
         vector p(n, vector<mint>(n));
         {
             vector<mint> s(n);
-            cin >> s;
+            for (auto& x : s) cin >> x;
             for (int i = 0; i < n; ++i)
                 for (int j = 0; j < n; ++j) p[i][j] = s[i] / (s[i] + s[j]);
         }
-        vector<mint> scc_prob(1 << n, 1);
-        vector win_against_mask_prob(1 << n, vector<mint>(n, 1));
+        vector<mint> scc_prob(max_mask, 1);
+        vector win_against_mask_prob(max_mask, array<mint, 14>{});
         for (int i = 0; i < n; ++i) {
-            for (int mask = 1; mask < (1 << n); ++mask) {
+            win_against_mask_prob[0][i] = 1;
+            for (int mask = 1; mask < max_mask; ++mask) {
                 int j = __lg(mask);
                 win_against_mask_prob[mask][i] =
                     win_against_mask_prob[mask ^ (1 << j)][i] * p[i][j];
             }
         }
         mint ans = 0;
-        for (int mask = 1; mask < (1 << n); ++mask) {
+        for (int mask = 1; mask < max_mask; ++mask) {
             for (int i = 0; i < n; ++i)
                 if (mask >> i & 1)
                     for (int j = 0; j < n; ++j)
                         if (!(mask >> j & 1)) scc_prob[mask] *= p[i][j];
-            int unmask = mask ^ ((1 << n) - 1);
+            int unmask = mask ^ (max_mask - 1);
             const auto& to_multiply = win_against_mask_prob[unmask];
             for (int submask = (mask - 1) & mask; submask;
                  (--submask) &= mask) {
