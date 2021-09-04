b2cfc697ceb757a007d3dc08269b4fb9dbe6ed3e
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2021-09-05 01:26:00 +0530

    Add solution for problem `F - Make Pair` Link: https://atcoder.jp/contests/abc217/tasks/abc217_f Tags: AtCoder, *500 Ref: https://atcoder.jp/contests/abc217/submissions/25619267

--- atcoder/abc217/F.cpp
+++ atcoder/abc217/F.cpp
@@ -137,15 +137,20 @@ using mint = ModInt32<nttmod>;
 constexpr int N = 300;
 
 vector<mint> fact(N + 1);
+vector<mint> ifact(N + 1);
+vector<mint> inv(N + 1);
 
 void precompute_facts() {
     fact[0] = 1;
     for (int i = 1; i <= N; ++i) fact[i] = fact[i - 1] * i;
+    ifact[N] = 1 / fact[N];
+    for (int i = N - 1; i >= 0; --i) ifact[i] = ifact[i + 1] * (i + 1);
+    for (int i = 1; i <= N; ++i) inv[i] = fact[i - 1] * ifact[i];
 }
 
 mint C(int n, int r) {
     if (r < 0 || r > n) return 0;
-    return fact[n] / fact[r] / fact[n - r];
+    return fact[n] * ifact[n - r] * ifact[r];
 };
 
 int main() {
