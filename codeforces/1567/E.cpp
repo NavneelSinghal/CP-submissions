30bf0370a0723185851da8e603e4a0eaf43295e4
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2021-09-06 00:29:00 +0530

    Add solution for problem `E - Non-Decreasing Dilemma` Link: https://codeforces.com/contest/1567/problem/E Tags: data structures, divide and conquer, math, *2200 Ref: https://codeforces.com/contest/1567/submission/127991591

---
+++ codeforces/1567/E.cpp
@@ -0,0 +1,244 @@
+#ifndef LOCAL
+    #pragma GCC optimize("O3,unroll-loops")
+    #pragma GCC target("avx,avx2,sse,sse2,sse3,sse4,popcnt,bmi,bmi2,lzcnt")
+#endif
+
+#include "bits/stdc++.h"
+
+#ifdef DEBUG
+    #include "includes/debug/debug.hpp"
+#else
+    #define debug(...) 0
+#endif
+
+namespace IO {
+    constexpr bool UNSAFE = false;
+    constexpr int GLOB_BUF_SIZE = 1 << 15;
+#ifndef DEBUG
+    #define CHANGE_DEFAULT_STREAMS
+    static struct FastInput {
+        FastInput() {
+            if constexpr (UNSAFE) {
+                chars_read = fread(buf, 1, BUF_SIZE, in);
+                buf_pos = 0;
+                buf[0] = (chars_read == 0 ? -1 : buf[0]);
+            }
+        }
+        static constexpr int BUF_SIZE = GLOB_BUF_SIZE;
+        char buf[BUF_SIZE];
+        size_t chars_read = 0;
+        size_t buf_pos = 0;
+        FILE* in = stdin;
+        char cur = 0;
+        inline char get_char() {
+            if constexpr (!UNSAFE) {
+                if (buf_pos >= chars_read) {
+                    chars_read = fread(buf, 1, BUF_SIZE, in);
+                    buf_pos = 0;
+                    buf[0] = (chars_read == 0 ? -1 : buf[0]);
+                }
+            }
+            return cur = buf[buf_pos++];
+        }
+        template <typename T>
+        inline FastInput* tie(T) {
+            return this;
+        }
+        inline void sync_with_stdio(bool) {}
+        inline explicit operator bool() { return cur != -1; }
+        inline static bool is_blank(char c) { return c <= ' '; }
+        inline bool skip_blanks() {
+            while (is_blank(cur) && cur != -1) get_char();
+            return cur != -1;
+        }
+        inline FastInput& operator>>(char& c) {
+            skip_blanks();
+            c = cur;
+            get_char();
+            return *this;
+        }
+        inline FastInput& operator>>(std::string& s) {
+            if (skip_blanks()) {
+                s.clear();
+                do {
+                    s += cur;
+                } while (!is_blank(get_char()));
+            }
+            return *this;
+        }
+        template <typename T>
+        inline FastInput& read_integer(T& n) {
+            // unsafe, doesn't check that characters are actually digits
+            n = 0;
+            if (skip_blanks()) {
+                int sign = +1;
+                if (cur == '-') {
+                    sign = -1;
+                    get_char();
+                }
+                do {
+                    n += n + (n << 3) + cur - '0';
+                } while (!is_blank(get_char()));
+                n *= sign;
+            }
+            return *this;
+        }
+        template <typename T>
+        inline typename std::enable_if<std::is_integral<T>::value,
+                                       FastInput&>::type
+        operator>>(T& n) {
+            return read_integer(n);
+        }
+    #if !defined(_WIN32) || defined(_WIN64)
+        inline FastInput& operator>>(__int128& n) { return read_integer(n); }
+    #endif
+        template <typename T>
+        inline typename std::enable_if<std::is_floating_point<T>::value,
+                                       FastInput&>::type
+        operator>>(T& n) {
+            // not sure if really fast, for compatibility only
+            n = 0;
+            if (skip_blanks()) {
+                std::string s;
+                (*this) >> s;
+                sscanf(s.c_str(), "%lf", &n);
+            }
+            return *this;
+        }
+    } fast_input;
+    #define cin IO::fast_input
+    static struct FastOutput {
+        static constexpr int BUF_SIZE = GLOB_BUF_SIZE;
+        char buf[BUF_SIZE];
+        size_t buf_pos = 0;
+        static constexpr int TMP_SIZE = GLOB_BUF_SIZE;
+        char tmp[TMP_SIZE];
+        FILE* out = stdout;
+        inline void put_char(char c) {
+            buf[buf_pos++] = c;
+            if (buf_pos == BUF_SIZE) {
+                fwrite(buf, 1, buf_pos, out);
+                buf_pos = 0;
+            }
+        }
+        ~FastOutput() { fwrite(buf, 1, buf_pos, out); }
+        inline FastOutput& operator<<(char c) {
+            put_char(c);
+            return *this;
+        }
+        inline FastOutput& operator<<(const char* s) {
+            while (*s) put_char(*s++);
+            return *this;
+        }
+        inline FastOutput& operator<<(const std::string& s) {
+            for (auto x : s) put_char(x);
+            return *this;
+        }
+        template <typename T>
+        inline char* integer_to_string(T n) {
+            // beware of TMP_SIZE
+            char* p = tmp + TMP_SIZE - 1;
+            if (n == 0)
+                *--p = '0';
+            else {
+                bool is_negative = false;
+                if (n < 0) {
+                    is_negative = true;
+                    n = -n;
+                }
+                while (n > 0) {
+                    *--p = (char)('0' + n % 10);
+                    n /= 10;
+                }
+                if (is_negative) *--p = '-';
+            }
+            return p;
+        }
+        template <typename T>
+        inline typename std::enable_if<std::is_integral<T>::value, char*>::type
+        stringify(T n) {
+            return integer_to_string(n);
+        }
+    #if !defined(_WIN32) || defined(_WIN64)
+        inline char* stringify(__int128 n) { return integer_to_string(n); }
+    #endif
+        template <typename T>
+        inline typename std::enable_if<std::is_floating_point<T>::value,
+                                       char*>::type
+        stringify(T n) {
+            sprintf(tmp, "%.17f", n);
+            return tmp;
+        }
+        template <typename T>
+        inline FastOutput& operator<<(const T& n) {
+            auto p = stringify(n);
+            for (; *p != 0; p++) put_char(*p);
+            return *this;
+        }
+    } fast_output;
+    #define cout IO::fast_output
+#endif
+}  // namespace IO
+
+using namespace std;
+
+using ll = int64_t;
+
+constexpr int N = (int)2e5;
+struct Node {
+    int first;
+    int last;
+    int suf;
+    int pre;
+    int len;
+    ll ans;
+    Node() : first(0), last(0), suf(0), pre(0), len(0), ans(0LL) {}
+    Node(int f, int l, int s, int p, int le, ll a)
+        : first(f), last(l), suf(s), pre(p), len(le), ans(a) {}
+    Node(int x) : first(x), last(x), suf(1), pre(1), len(1), ans(1) {}
+} t[N * 4];
+
+Node combine(const Node& a, const Node& b) {
+    if (a.len == 0) return b;
+    if (b.len == 0) return a;
+    Node x{a.first, b.last, b.suf, a.pre, a.len + b.len, a.ans + b.ans};
+    if (a.last <= b.first) {
+        if (a.len == a.suf) x.pre += b.pre;
+        if (b.len == b.suf) x.suf += a.suf;
+        x.ans += 1LL * a.suf * b.pre;
+    }
+    return x;
+}
+
+int n, q, lg, sz;
+int a[N];
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    cin >> n >> q;
+    for (int i = 0; i < n; ++i) cin >> a[i];
+    lg = 0;
+    while ((1 << lg) < n) ++lg;
+    sz = 1 << lg;
+    for (int i = 0; i < n; ++i) t[i + sz] = Node(a[i]);
+    for (int i = sz - 1; i >= 1; --i) t[i] = combine(t[i << 1], t[i << 1 | 1]);
+    while (q--) {
+        int type, l, r;
+        cin >> type >> l >> r;
+        l += sz - 1;
+        if (type == 1) {
+            t[l] = Node(r);
+            while (l >>= 1) t[l] = combine(t[l << 1], t[l << 1 | 1]);
+        } else {
+            r += sz;
+            Node sml{}, smr{};
+            while (l < r) {
+                if (l & 1) sml = combine(sml, t[l++]);
+                if (r & 1) smr = combine(t[--r], smr);
+                l >>= 1;
+                r >>= 1;
+            }
+            cout << combine(sml, smr).ans << '\n';
+        }
+    }
+}
