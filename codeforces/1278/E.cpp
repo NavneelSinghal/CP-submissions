1cc824a95bc71b00e148b8c8885040eb0af88a09
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2021-09-05 20:38:00 +0530

    Add solution for problem `E - Tests for problem D` Link: https://codeforces.com/contest/1278/problem/E Tags: constructive algorithms, dfs and similar, divide and conquer, trees, *2200 Ref: https://codeforces.com/contest/1278/submission/127950410

--- codeforces/1278/E.cpp
+++ codeforces/1278/E.cpp
@@ -11,175 +11,6 @@
     #define debug(...) 0
 #endif
 
-namespace IO {
-    constexpr bool UNSAFE = false;
-    constexpr int GLOB_BUF_SIZE = 1 << 15;
-#ifndef DEBUG
-    #define CHANGE_DEFAULT_STREAMS
-    static struct FastInput {
-        FastInput() {
-            if constexpr (UNSAFE) {
-                chars_read = fread(buf, 1, BUF_SIZE, in);
-                buf_pos = 0;
-                buf[0] = (chars_read == 0 ? -1 : buf[0]);
-            }
-        }
-        static constexpr int BUF_SIZE = GLOB_BUF_SIZE;
-        char buf[BUF_SIZE];
-        size_t chars_read = 0;
-        size_t buf_pos = 0;
-        FILE* in = stdin;
-        char cur = 0;
-        inline char get_char() {
-            if constexpr (!UNSAFE) {
-                if (buf_pos >= chars_read) {
-                    chars_read = fread(buf, 1, BUF_SIZE, in);
-                    buf_pos = 0;
-                    buf[0] = (chars_read == 0 ? -1 : buf[0]);
-                }
-            }
-            return cur = buf[buf_pos++];
-        }
-        template <typename T>
-        inline FastInput* tie(T) {
-            return this;
-        }
-        inline void sync_with_stdio(bool) {}
-        inline explicit operator bool() { return cur != -1; }
-        inline static bool is_blank(char c) { return c <= ' '; }
-        inline bool skip_blanks() {
-            while (is_blank(cur) && cur != -1) get_char();
-            return cur != -1;
-        }
-        inline FastInput& operator>>(char& c) {
-            skip_blanks();
-            c = cur;
-            get_char();
-            return *this;
-        }
-        inline FastInput& operator>>(std::string& s) {
-            if (skip_blanks()) {
-                s.clear();
-                do {
-                    s += cur;
-                } while (!is_blank(get_char()));
-            }
-            return *this;
-        }
-        template <typename T>
-        inline FastInput& read_integer(T& n) {
-            // unsafe, doesn't check that characters are actually digits
-            n = 0;
-            if (skip_blanks()) {
-                int sign = +1;
-                if (cur == '-') {
-                    sign = -1;
-                    get_char();
-                }
-                do {
-                    n += n + (n << 3) + cur - '0';
-                } while (!is_blank(get_char()));
-                n *= sign;
-            }
-            return *this;
-        }
-        template <typename T>
-        inline typename std::enable_if<std::is_integral<T>::value,
-                                       FastInput&>::type
-        operator>>(T& n) {
-            return read_integer(n);
-        }
-    #if !defined(_WIN32) || defined(_WIN64)
-        inline FastInput& operator>>(__int128& n) { return read_integer(n); }
-    #endif
-        template <typename T>
-        inline typename std::enable_if<std::is_floating_point<T>::value,
-                                       FastInput&>::type
-        operator>>(T& n) {
-            // not sure if really fast, for compatibility only
-            n = 0;
-            if (skip_blanks()) {
-                std::string s;
-                (*this) >> s;
-                sscanf(s.c_str(), "%lf", &n);
-            }
-            return *this;
-        }
-    } fast_input;
-    #define cin IO::fast_input
-    static struct FastOutput {
-        static constexpr int BUF_SIZE = GLOB_BUF_SIZE;
-        char buf[BUF_SIZE];
-        size_t buf_pos = 0;
-        static constexpr int TMP_SIZE = GLOB_BUF_SIZE;
-        char tmp[TMP_SIZE];
-        FILE* out = stdout;
-        inline void put_char(char c) {
-            buf[buf_pos++] = c;
-            if (buf_pos == BUF_SIZE) {
-                fwrite(buf, 1, buf_pos, out);
-                buf_pos = 0;
-            }
-        }
-        ~FastOutput() { fwrite(buf, 1, buf_pos, out); }
-        inline FastOutput& operator<<(char c) {
-            put_char(c);
-            return *this;
-        }
-        inline FastOutput& operator<<(const char* s) {
-            while (*s) put_char(*s++);
-            return *this;
-        }
-        inline FastOutput& operator<<(const std::string& s) {
-            for (auto x : s) put_char(x);
-            return *this;
-        }
-        template <typename T>
-        inline char* integer_to_string(T n) {
-            // beware of TMP_SIZE
-            char* p = tmp + TMP_SIZE - 1;
-            if (n == 0)
-                *--p = '0';
-            else {
-                bool is_negative = false;
-                if (n < 0) {
-                    is_negative = true;
-                    n = -n;
-                }
-                while (n > 0) {
-                    *--p = (char)('0' + n % 10);
-                    n /= 10;
-                }
-                if (is_negative) *--p = '-';
-            }
-            return p;
-        }
-        template <typename T>
-        inline typename std::enable_if<std::is_integral<T>::value, char*>::type
-        stringify(T n) {
-            return integer_to_string(n);
-        }
-    #if !defined(_WIN32) || defined(_WIN64)
-        inline char* stringify(__int128 n) { return integer_to_string(n); }
-    #endif
-        template <typename T>
-        inline typename std::enable_if<std::is_floating_point<T>::value,
-                                       char*>::type
-        stringify(T n) {
-            sprintf(tmp, "%.17f", n);
-            return tmp;
-        }
-        template <typename T>
-        inline FastOutput& operator<<(const T& n) {
-            auto p = stringify(n);
-            for (; *p != 0; p++) put_char(*p);
-            return *this;
-        }
-    } fast_output;
-    #define cout IO::fast_output
-#endif
-}  // namespace IO
-
 using namespace std;
 
 using ll = int64_t;
@@ -205,24 +36,20 @@ int main() {
         vector<pair<int, int>> events{pair{0, 0}};
         auto dfs = [&g, &events](const auto& self, int u, int p) -> void {
             vector<int> order;
-            for (auto v : g[u])
-                if (v != p) order.push_back(v);
+            for (auto v : g[u]) if (v != p) order.push_back(v);
             while (!order.empty()) {
                 events.emplace_back(order.back(), 0);
                 order.pop_back();
             }
             events.emplace_back(u, 1);
-            for (auto v : g[u])
-                if (v != p) self(self, v, u);
+            for (auto v : g[u]) if (v != p) self(self, v, u);
         };
         dfs(dfs, 0, -1);
         vector<int> l(n), r(n);
         for (int i = 0; i < 2 * n; ++i) {
             auto [index, type] = events[i];
-            if (type == 0)
-                l[index] = i + 1;
-            else
-                r[index] = i + 1;
+            if (type == 0) l[index] = i + 1;
+            else r[index] = i + 1;
         }
         for (int i = 0; i < n; ++i) {
             cout << l[i] << ' ' << r[i] << '\n';
