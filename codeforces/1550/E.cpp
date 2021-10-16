312fe346bed654828e653c2ef9bfaf2d43cb4447
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2021-10-16 22:21:00 +0530

    Add solution for problem `E - Stringforces` Link: https://codeforces.com/contest/1550/problem/E Tags: binary search, bitmasks, brute force, dp, strings, two pointers, *2500 Ref: https://codeforces.com/contest/1550/submission/132151164

--- codeforces/1550/E.cpp
+++ codeforces/1550/E.cpp
@@ -6,8 +6,8 @@
 using namespace std;
 using ll = int64_t;
 
-template <bool b>
-auto bin_search_split(auto l, auto r, const auto& predicate) {
+template <typename I, typename P, bool b>
+I bin_search_split(I l, I r, const P &predicate) {
     --l, ++r;
     while (r - l > 1) {
         auto mid = std::midpoint(l, r);
@@ -24,13 +24,15 @@ auto bin_search_split(auto l, auto r, const auto& predicate) {
 }
 
 // returns first i in [l, r], p(i) false, and if none found, returns r + 1
-auto find_first_false(auto l, auto r, const auto& p) {
-    return bin_search_split<true>(l, r, p);
+template <typename I, typename P>
+I find_first_false(I l, I r, const P &p) {
+    return bin_search_split<I, P, true>(l, r, p);
 }
 
 // returns last i in [l, r], p(i) true, and if none found, returns l - 1
-auto find_last_true(auto l, auto r, const auto& p) {
-    return bin_search_split<false>(l, r, p);
+template <typename I, typename P>
+I find_last_true(I l, I r, const P &p) {
+    return bin_search_split<I, P, false>(l, r, p);
 }
 
 int main() {
@@ -44,12 +46,12 @@ int main() {
         cin >> n >> k >> s;
         cout << find_last_true(1, n / k, [&](int m) {
             const int inf = 1e9;
-            vector leftmost(k, vector(n + 1, inf));
+            vector<vector<int>> leftmost(k, vector<int>(n + 1, inf));
             for (int bit = 0; bit < k; ++bit) {
                 int prv = n;
                 for (int i = n - 1; i >= 0; --i) {
                     leftmost[bit][i] = leftmost[bit][i + 1];
-                    if (s[i] != '?' and s[i] != 'a' + bit) prv = i;
+                    if (s[i] != '?' && s[i] != 'a' + bit) prv = i;
                     if (prv >= i + m) leftmost[bit][i] = i;
                 }
             }
