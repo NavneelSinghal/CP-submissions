2d002f0b04fb03fb8fd23d2fc4a5c54703aa0b1a
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2021-10-16 22:23:00 +0530

    Add solution for problem `E - Stringforces` Link: https://codeforces.com/contest/1550/problem/E Tags: binary search, bitmasks, brute force, dp, strings, two pointers, *2500 Ref: https://codeforces.com/contest/1550/submission/132151379

--- codeforces/1550/E.cpp
+++ codeforces/1550/E.cpp
@@ -1,5 +1,3 @@
-#define PROBLEM "https://judge.yosupo.jp/problem/general_weighted_matching"
-
 #pragma GCC optimize("O3,unroll-loops")
 #pragma GCC target("avx,avx2,sse,sse2,sse3,sse4,popcnt")
 
@@ -8,82 +6,31 @@
 using namespace std;
 using ll = int64_t;
 
-template <class T, bool unsafe = false>
-constexpr uint32_t ctz(T a) {
-    if constexpr (!unsafe) {
-        if (!a) return sizeof(T) * 8;
-    }
-    if constexpr (sizeof(T) <= sizeof(uint32_t)) {
-        return (uint32_t)(__builtin_ctz((uint32_t)a));
-    } else if constexpr (sizeof(T) <= sizeof(uint64_t)) {
-        return (uint32_t)(__builtin_ctzll((uint64_t)a));
-    } else {
-        static_assert(sizeof(T) == sizeof(uint64_t) * 2);
-        uint32_t l = ctz((uint64_t)a);
-        return l != sizeof(uint64_t) * 8
-                   ? l
-                   : l + ctz((uint64_t)(a >> sizeof(uint64_t) * 8));
-    }
-}
-
-template <class T, bool unsafe = false>
-constexpr uint32_t clz(T a) {
-    if constexpr (!unsafe) {
-        if (!a) return sizeof(T) * 8;
-    }
-    if constexpr (sizeof(T) <= sizeof(uint32_t)) {
-        return (uint32_t)(__builtin_clz((uint32_t)a));
-    } else if constexpr (sizeof(T) <= sizeof(uint64_t)) {
-        return (uint32_t)(__builtin_clzll((uint64_t)a));
-    } else {
-        static_assert(sizeof(T) == sizeof(uint64_t) * 2);
-        uint32_t l = clz((uint64_t)(a >> sizeof(uint64_t) * 8));
-        return l != sizeof(uint64_t) * 8 ? l : l + clz((uint64_t)a);
-    }
-}
-
-// -1 for 0
-template <class T, bool unsafe = false>
-constexpr uint32_t lg(T a) {
-    if constexpr (sizeof(T) <= sizeof(uint32_t)) {
-        return sizeof(uint32_t) * 8 - 1 - clz<T, unsafe>(a);
-    } else if constexpr (sizeof(T) <= sizeof(uint64_t)) {
-        return sizeof(uint64_t) * 8 - 1 - clz<T, unsafe>(a);
-    } else {
-        return sizeof(uint64_t) * 16 - 1 - clz<T, unsafe>(a);
-    }
-}
-
-// split [l, r]
-template <class Integer, class Predicate, bool type>
-// __attribute__((target("bmi")))
-Integer find_bin_split(Integer l, Integer r, const Predicate &pred) {
-    if (l > r) {
-        if constexpr (type)
-            return r;
+template <bool b>
+auto bin_search_split(auto l, auto r, const auto& predicate) {
+    --l, ++r;
+    while (r - l > 1) {
+        auto mid = std::midpoint(l, r);
+        if (predicate(mid))
+            l = mid;
         else
-            return r + 1;
+            r = mid;
     }
-    ++r;
-    Integer w = Integer(1) << lg<Integer, true>(r - l);
-    --l;
-    if (pred(l + w)) l = r - w;
-    for (w >>= 1; w >= Integer(1); w >>= 1)
-        if (pred(l + w)) l += w;
-    if constexpr (type)
+    if constexpr (b) {
+        return r;
+    } else {
         return l;
-    else
-        return l + 1;
+    }
 }
 
-template <class Integer, class Predicate>
-Integer find_first_false(Integer l, Integer r, const Predicate &pred) {
-    return find_bin_split<Integer, Predicate, false>(l, r, pred);
+// returns first i in [l, r], p(i) false, and if none found, returns r + 1
+auto find_first_false(auto l, auto r, const auto& p) {
+    return bin_search_split<true>(l, r, p);
 }
 
-template <class Integer, class Predicate>
-Integer find_last_true(Integer l, Integer r, const Predicate &pred) {
-    return find_bin_split<Integer, Predicate, true>(l, r, pred);
+// returns last i in [l, r], p(i) true, and if none found, returns l - 1
+auto find_last_true(auto l, auto r, const auto& p) {
+    return bin_search_split<false>(l, r, p);
 }
 
 int main() {
@@ -97,12 +44,12 @@ int main() {
         cin >> n >> k >> s;
         cout << find_last_true(1, n / k, [&](int m) {
             const int inf = 1e9;
-            vector<vector<int>> leftmost(k, vector<int>(n + 1, inf));
+            vector leftmost(k, vector(n + 1, inf));
             for (int bit = 0; bit < k; ++bit) {
                 int prv = n;
                 for (int i = n - 1; i >= 0; --i) {
                     leftmost[bit][i] = leftmost[bit][i + 1];
-                    if (s[i] != '?' && s[i] != 'a' + bit) prv = i;
+                    if (s[i] != '?' and s[i] != 'a' + bit) prv = i;
                     if (prv >= i + m) leftmost[bit][i] = i;
                 }
             }
