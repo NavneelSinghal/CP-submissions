d94bea49ed25da36d8ab689c3e0c2c82ac05c745
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2021-11-15 00:18:00 +0530

    Add solution for problem `E - Connected Components?` Link: https://codeforces.com/contest/920/problem/E Tags: data structures, dfs and similar, dsu, graphs, *2100 Ref: https://codeforces.com/contest/920/submission/135508935

--- codeforces/920/E.cpp
+++ codeforces/920/E.cpp
@@ -336,19 +336,6 @@ struct graph_edge_pointers {
     }
 };
 
-template <class T>
-struct simple_queue {
-    std::vector<T> payload;
-    int pos = 0;
-    void reserve(int n) { payload.reserve(n); }
-    int size() const { return int(payload.size()) - pos; }
-    bool empty() const { return pos == int(payload.size()); }
-    void push(const T& t) { payload.push_back(t); }
-    T& front() { return payload[pos]; }
-    void clear() { payload.clear(), pos = 0; }
-    void pop() { pos++; }
-};
-
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
 
@@ -387,8 +374,7 @@ int main() {
     vector<int> unvisited(n);
     iota(begin(unvisited), end(unvisited), 0);
 
-    simple_queue<int> q;
-    q.reserve(n);
+    queue<int> q;
     for (int i = 0; i < n; ++i) {
         if (component[i] != -1) continue;
         cur_component++;
