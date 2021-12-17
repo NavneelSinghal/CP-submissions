51b53140032ad103c5d46b91d6c65467ba634716
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2021-12-17 15:07:41 +0530

    clean

--- codeforces/1182/A.py
+++
@@ -1,5 +0,0 @@
-n = int(input())
-if(n%2==1):
-    print(0)
-else:
-    print(2**(n//2))
--- codeforces/664/A.py
+++
@@ -1,5 +0,0 @@
-a = [int(x) for x in input().split()]
-if(a[0]!=a[1]):
-    print(1)
-else:
-    print(a[0])
