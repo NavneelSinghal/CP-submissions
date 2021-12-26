43fc1a5c636ca481495873e8aa62a5a821f1f21a
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2021-12-27 00:48:00 +0530

    Add solution for problem `C - Greedy Shopping` Link: https://codeforces.com/contest/1439/problem/C Tags: binary search, data structures, divide and conquer, greedy, implementation, *2600 Ref: https://codeforces.com/contest/1439/submission/140689334

--- codeforces/1439/C.cpp
+++ codeforces/1439/C.cpp
@@ -340,15 +340,16 @@ struct lazy_segtree {
     void build(const std::vector<Base>& v) {
         _n = int(v.size());
         log = 0;
-        while ((2 * _n - 1) >> log > 1) ++log;
-        d = std::vector<Node>(2 * _n, id_node);
-        if constexpr (is_lazy) lz = std::vector<Update>(_n, id_update);
-        for (int i = 0; i < _n; i++) d[_n + i] = make_node(v[i], i);
-        for (int i = _n - 1; i >= 1; i--) update(i);
+        while ((1 << log) < _n) ++log;
+        size = 1 << log;
+        d = std::vector<Node>(2 * size, id_node);
+        if constexpr (is_lazy) lz = std::vector<Update>(size, id_update);
+        for (int i = 0; i < _n; i++) d[size + i] = make_node(v[i], i);
+        for (int i = size - 1; i >= 1; i--) update(i);
     }
     
     void set(int p, Node x) {
-        p += _n;
+        p += size;
         if constexpr (is_lazy)
             for (int i = log; i >= 1; i--) push(p >> i);
         d[p] = x;
@@ -356,7 +357,7 @@ struct lazy_segtree {
     }
 
     Node get(int p) {
-        p += _n;
+        p += size;
         if constexpr (is_lazy)
             for (int i = log; i >= 1; i--) push(p >> i);
         return d[p];
@@ -364,31 +365,26 @@ struct lazy_segtree {
 
     Node query(int l, int r) {
         if (l == r) return id_node;
-        l += _n, r += _n;
+        l += size, r += size;
         if constexpr (is_lazy) {
             int l_ctz = __builtin_ctz(l);
             int r_ctz = __builtin_ctz(r);
             for (int i = log; i > l_ctz; --i) push(l >> i);
             for (int i = log; i > r_ctz; --i) push((r - 1) >> i);
         }
-        Node sm = id_node;
-        --l;
-        const int lg = __lg(r ^ l);
-        for (int h = 0; h < lg; ++h) {
-            const int L = (l >> h) + 1;
-            if (L & 1) sm = combine(sm, d[L]);
+        Node sml = id_node, smr = id_node;
+        while (l < r) {
+            if (l & 1) sml = combine(sml, d[l++]);
+            if (r & 1) smr = combine(d[--r], smr);
+            l >>= 1, r >>= 1;
         }
-        for (int h = lg - 1; h >= 0; --h) {
-            const int R = r >> h;
-            if (R % 2) sm = combine(sm, d[R - 1]);
-        }
-        return sm;
+        return combine(sml, smr);
     }
     
-    Node all_query() const { return query(0, _n); }
+    Node all_query() const { return d[1]; }
     
     void update(int p, Update f) {
-        p += _n;
+        p += size;
         if constexpr (is_lazy)
             for (int i = log; i >= 1; i--) push(p >> i);
         d[p] = apply_update(f, d[p]);
@@ -397,7 +393,7 @@ struct lazy_segtree {
     
     void update(int l, int r, Update f) {
         if (l == r) return;
-        l += _n, r += _n;
+        l += size, r += size;
         const int l_ctz = __builtin_ctz(l);
         const int r_ctz = __builtin_ctz(r);
         if constexpr (is_lazy) {
@@ -406,15 +402,10 @@ struct lazy_segtree {
         }
         {
             const int l2 = l, r2 = r;
-            --l;
-            const int lg = __lg(r ^ l);
-            for (int h = 0; h < lg; ++h) {
-                const int L = (l >> h) + 1;
-                if (L & 1) all_apply(L, f);
-            }
-            for (int h = lg - 1; h >= 0; --h) {
-                const int R = r >> h;
-                if (R & 1) all_apply(R - 1, f);
+            while (l < r) {
+                if (l & 1) all_apply(l++, f);
+                if (r & 1) all_apply(--r, f);
+                l >>= 1, r >>= 1;
             }
             l = l2, r = r2;
         }
@@ -426,65 +417,28 @@ struct lazy_segtree {
     int max_right(int l, G g) {
         // assert(0 <= l && l <= _n);
         // assert(g(id_node));
-
         if (l == _n) return _n;
-
-        l += _n;
-        int r = 2 * _n;
-        
-        const int l_ctz = __builtin_ctz(l);
-        const int r_ctz = __builtin_ctz(r);
-        
-        if constexpr (is_lazy) {
-            for (int i = log; i > l_ctz; --i) push(l >> i);
-            for (int i = log; i > r_ctz; --i) push((r - 1) >> i);
-        }
-        
+        l += size;
+        if constexpr (is_lazy)
+            for (int i = log; i >= 1; i--) push(l >> i);
         Node sm = id_node;
-
-        --l;
-        int i = -1;
-        int lg = __lg(r ^ l);
-        for (int h = 0; h < lg; ++h) {
-            int L = (l >> h) + 1;
-            if (L & 1) {
-                // ans.push_back(L);
-                int root = L;
-                auto c = combine(sm, d[root]);
-                if (!g(c)) {
-                    i = root;
-                    break;
-                }
-                sm = c;
-            }
-        }
-        
-        if (i == -1)
-            for (int h = lg - 1; h >= 0; --h) {
-                int R = r >> h;
-                if (R & 1) {
-                    // ans.push_back(R - 1);
-                    int root = R - 1;
-                    auto c = combine(sm, d[root]);
-                    if (!g(c)) {
-                        i = root;
-                        break;
+        do {
+            while (l % 2 == 0) l >>= 1;
+            if (!g(combine(sm, d[l]))) {
+                while (l < size) {
+                    if constexpr (is_lazy) push(l);
+                    l = (2 * l);
+                    if (g(combine(sm, d[l]))) {
+                        sm = combine(sm, d[l]);
+                        l++;
                     }
-                    sm = c;
                 }
+                return l - size;
             }
-        
-        if (i == -1) return _n;
-        while (i < _n) {
-            push(i);
-            i = 2 * i;
-            auto c = combine(sm, d[i]);
-            if (g(c)) {
-                sm = c;
-                ++i;
-            }
-        }
-        return i - _n;
+            sm = combine(sm, d[l]);
+            l++;
+        } while ((l & -l) != l);
+        return _n;
     }
 
     template <class G>
@@ -492,63 +446,31 @@ struct lazy_segtree {
         // assert(0 <= r && r <= _n);
         // assert(g(id_node));
         if (r == 0) return 0;
-        r += _n;
-        int l = _n;
-       
-        const int l_ctz = __builtin_ctz(l);
-        const int r_ctz = __builtin_ctz(r);
-        
-        if constexpr (is_lazy) {
-            for (int i = log; i > l_ctz; --i) push(l >> i);
-            for (int i = log; i > r_ctz; --i) push((r - 1) >> i);
-        }
-        
-        --l;
+        r += size;
+        if constexpr (is_lazy)
+            for (int i = log; i >= 1; i--) push((r - 1) >> i);
         Node sm = id_node;
-        int i = -1;
-        int lg = __lg(r - l) + 1;
-        for (int h = 0; h < lg; ++h) {
-            int R = r >> h;
-            if (R & 1) {
-                int root = R - 1;
-                auto c = combine(d[root], sm);
-                if (!g(c)) {
-                    i = root;
-                    break;
-                }
-                sm = c;
-            }
-        }
-
-        if (i == -1)
-            for (int h = lg - 1; h >= 0; --h) {
-                int L = (l >> h) + 1;
-                if (L & 1) {
-                    int root = L;
-                    auto c = combine(d[root], sm);
-                    if (!g(c)) {
-                        i = root;
-                        break;
+        do {
+            r--;
+            while (r > 1 && (r % 2)) r >>= 1;
+            if (!g(combine(d[r], sm))) {
+                while (r < size) {
+                    if constexpr (is_lazy) push(r);
+                    r = (2 * r + 1);
+                    if (g(combine(d[r], sm))) {
+                        sm = combine(d[r], sm);
+                        r--;
                     }
-                    sm = c;
                 }
+                return r + 1 - size;
             }
-
-        if (i == -1) return 0;
-        while (i < _n) {
-            push(i);
-            i = 2 * i + 1;
-            auto c = combine(d[i], sm);
-            if (g(c)) {
-                sm = c;
-                --i;
-            }
-        }
-        return i + 1 - _n;
+            sm = combine(d[r], sm);
+        } while ((r & -r) != r);
+        return 0;
     }
 
    private:
-    int _n, log;
+    int _n, size, log;
     std::vector<Node> d;
     std::vector<Update> lz;
     MakeNode make_node;
@@ -563,7 +485,7 @@ struct lazy_segtree {
     void all_apply(int k, Update f) {
         d[k] = apply_update(f, d[k]);
         if constexpr (is_lazy)
-            if (k < _n) lz[k] = compose_updates(f, lz[k]);
+            if (k < size) lz[k] = compose_updates(f, lz[k]);
     }
     void push(int k) {
         if constexpr (is_check_lazy) {
