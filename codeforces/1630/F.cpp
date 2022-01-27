084efd70a1ae702b1a64772bbb36903c7a81bcec
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2022-01-28 00:05:00 +0530

    Add solution for problem `F - Making It Bipartite` Link: https://codeforces.com/contest/1630/problem/F Tags: flows, graph matchings, graphs, number theory, *3400 Ref: https://codeforces.com/contest/1630/submission/144254842

--- codeforces/1630/F.cpp
+++ codeforces/1630/F.cpp
@@ -18,184 +18,154 @@ using ld = long double;
 
 using namespace std;
 
-struct Random : std::mt19937 {
-    using std::mt19937::mt19937;
-    using std::mt19937::operator();
-    static int64_t gen_seed() {
-        return std::chrono::steady_clock::now().time_since_epoch().count();
+template <class T>
+struct simple_queue {
+    std::vector<T> payload;
+    int pos = 0;
+    void reserve(int n) { payload.reserve(n); }
+    int size() const { return int(payload.size()) - pos; }
+    bool empty() const { return pos == int(payload.size()); }
+    void push(const T& t) { payload.push_back(t); }
+    T& front() { return payload[pos]; }
+    void clear() {
+        payload.clear();
+        pos = 0;
     }
-    Random() : std::mt19937(gen_seed()) {}
-    template <class Int>
-    auto operator()(Int a, Int b)
-        -> std::enable_if_t<std::is_integral_v<Int>, Int> {
-        return std::uniform_int_distribution<Int>(a, b)(*this);
-    }
-    template <class Int>
-    auto operator()(Int a) -> std::enable_if_t<std::is_integral_v<Int>, Int> {
-        return std::uniform_int_distribution<Int>(0, a - 1)(*this);
-    }
-    template <class Real>
-    auto operator()(Real a, Real b)
-        -> std::enable_if_t<std::is_floating_point_v<Real>, Real> {
-        return std::uniform_real_distribution<Real>(a, b)(*this);
-    }
+    void pop() { pos++; }
 };
 
-template <bool ToShuffle = false>
-struct bipartite_matching {
-    int n_left, n_right;
-    std::vector<std::vector<int>> g;
-    std::vector<int> vis;
-    std::vector<int> match_from_left, match_from_right;
-    int iteration;
+template <class Cap>
+struct mf_graph {
+   public:
+    mf_graph() : _n(0) {}
+    mf_graph(int n) : _n(n), g(n) {}
 
-    bipartite_matching(int _n_left, int _n_right)
-        : n_left(_n_left),
-          n_right(_n_right),
-          g(_n_left),
-          vis(_n_left, 0),
-          match_from_left(_n_left, -1),
-          match_from_right(_n_right, -1),
-          iteration(0) {}
-
-    // u on left, v on right
-    void add(int u, int v) { g[u].push_back(v); }
-
-    bool dfs(int u) {
-        vis[u] = iteration;
-        for (auto v : g[u]) {
-            if (match_from_right[v] == -1) {
-                match_from_right[v] = u;
-                match_from_left[u] = v;
-                return true;
-            }
-        }
-        for (auto v : g[u]) {
-            if (vis[match_from_right[v]] != iteration &&
-                dfs(match_from_right[v])) {
-                match_from_right[v] = u;
-                match_from_left[u] = v;
-                return true;
-            }
-        }
-        return false;
+    int add_edge(int from, int to, Cap cap) {
+        int m = int(pos.size());
+        pos.push_back({from, int(g[from].size())});
+        g[from].push_back(_edge{to, int(g[to].size()), cap});
+        g[to].push_back(_edge{from, int(g[from].size()) - 1, 0});
+        return m;
     }
 
-    int get_max_matching() {
-        if constexpr (ToShuffle) {
-            Random rng;
-            for (int i = 0; i < n_left; ++i)
-                std::random_shuffle(std::begin(g[i]), std::end(g[i]), rng);
-        }
-        int new_matchings = 0;
-        int matchings = 0;
-        do {
-            iteration++;
-            new_matchings = 0;
-            for (int u = 0; u < n_left; ++u)
-                if (match_from_left[u] == -1 && dfs(u)) new_matchings++;
-            matchings += new_matchings;
-        } while (new_matchings > 0);
-        return matchings;
+    struct edge {
+        int from, to;
+        Cap cap, flow;
+    };
+
+    edge get_edge(int i) {
+        int m = int(pos.size());
+        auto& [f, s] = pos[i];
+        auto _e = g[f][s];
+        auto _re = g[_e.to][_e.rev];
+        return edge{f, _e.to, _e.cap + _re.cap, _re.cap};
     }
+
+    std::vector<edge> edges() {
+        int m = int(pos.size());
+        std::vector<edge> result;
+        for (int i = 0; i < m; i++) {
+            result.push_back(get_edge(i));
+        }
+        return result;
+    }
+
+    void change_edge(int i, Cap new_cap, Cap new_flow) {
+        int m = int(pos.size());
+        auto& _e = g[pos[i].first][pos[i].second];
+        auto& _re = g[_e.to][_e.rev];
+        _e.cap = new_cap - new_flow;
+        _re.cap = new_flow;
+    }
+
+    Cap flow(int s, int t) {
+        return flow(s, t, std::numeric_limits<Cap>::max());
+    }
+    Cap flow(int s, int t, Cap flow_limit) {
+        std::vector<int> level(_n), iter(_n);
+        simple_queue<int> que;
+
+        auto bfs = [&]() {
+            std::fill(level.begin(), level.end(), -1);
+            level[s] = 0;
+            que.clear();
+            que.push(s);
+            while (!que.empty()) {
+                int v = que.front();
+                que.pop();
+                for (auto& e : g[v]) {
+                    if (e.cap == 0 || level[e.to] >= 0) continue;
+                    level[e.to] = level[v] + 1;
+                    if (e.to == t) return;
+                    que.push(e.to);
+                }
+            }
+        };
+        auto dfs = [&](const auto& self, int v, Cap up) {
+            if (v == s) return up;
+            Cap res = 0;
+            int level_v = level[v];
+            for (int& i = iter[v]; i < int(g[v].size()); i++) {
+                _edge& e = g[v][i];
+                if (level_v <= level[e.to] || g[e.to][e.rev].cap == 0) continue;
+                Cap d =
+                    self(self, e.to, std::min(up - res, g[e.to][e.rev].cap));
+                if (d <= 0) continue;
+                g[v][i].cap += d;
+                g[e.to][e.rev].cap -= d;
+                res += d;
+                if (res == up) break;
+            }
+            return res;
+        };
+
+        Cap flow = 0;
+        while (flow < flow_limit) {
+            bfs();
+            if (level[t] == -1) break;
+            std::fill(iter.begin(), iter.end(), 0);
+            while (flow < flow_limit) {
+                Cap f = dfs(dfs, t, flow_limit - flow);
+                if (!f) break;
+                flow += f;
+            }
+        }
+        return flow;
+    }
+
+    std::vector<bool> min_cut(int s) {
+        std::vector<bool> visited(_n);
+        simple_queue<int> que;
+        que.push(s);
+        while (!que.empty()) {
+            int p = que.front();
+            que.pop();
+            visited[p] = true;
+            for (auto e : g[p])
+                if (e.cap && !visited[e.to]) {
+                    visited[e.to] = true;
+                    que.push(e.to);
+                }
+        }
+        return visited;
+    }
+
+   private:
+    int _n;
+    struct _edge {
+        int to, rev;
+        Cap cap;
+    };
+    std::vector<std::pair<int, int>> pos;
+    std::vector<std::vector<_edge>> g;
 };
-
-//// faster if the previous one fails
-// template <bool ToShuffle = false>
-// struct bipartite_matching {
-//     int n_left, n_right, flow = 0;
-//     std::vector<std::vector<int>> g;
-//     std::vector<int> match_from_left, match_from_right;
-//
-//     bipartite_matching(int _n_left, int _n_right)
-//         : n_left(_n_left),
-//           n_right(_n_right),
-//           g(_n_left),
-//           match_from_left(_n_left, -1),
-//           match_from_right(_n_right, -1),
-//           dist(_n_left) {}
-//
-//     void add(int u, int v) { g[u].push_back(v); }
-//
-//     std::vector<int> dist;
-//
-//     void bfs() {
-//         std::queue<int> q;
-//         for (int u = 0; u < n_left; ++u) {
-//             if (!~match_from_left[u])
-//                 q.push(u), dist[u] = 0;
-//             else
-//                 dist[u] = -1;
-//         }
-//         while (!q.empty()) {
-//             int u = q.front();
-//             q.pop();
-//             for (auto v : g[u])
-//                 if (~match_from_right[v] && !~dist[match_from_right[v]]) {
-//                     dist[match_from_right[v]] = dist[u] + 1;
-//                     q.push(match_from_right[v]);
-//                 }
-//         }
-//     }
-//
-//     bool dfs(int u) {
-//         for (auto v : g[u])
-//             if (!~match_from_right[v]) {
-//                 match_from_left[u] = v, match_from_right[v] = u;
-//                 return true;
-//             }
-//         for (auto v : g[u])
-//             if (dist[match_from_right[v]] == dist[u] + 1 &&
-//                 dfs(match_from_right[v])) {
-//                 match_from_left[u] = v, match_from_right[v] = u;
-//                 return true;
-//             }
-//         return false;
-//     }
-//
-//     int get_max_matching() {
-//         if constexpr (ToShuffle) {
-//             Random rng;
-//             for (int i = 0; i < n_left; ++i)
-//                 std::random_shuffle(std::begin(g[i]), std::end(g[i]), rng);
-//         }
-//         while (true) {
-//             bfs();
-//             int augment = 0;
-//             for (int u = 0; u < n_left; ++u)
-//                 if (!~match_from_left[u]) augment += dfs(u);
-//             if (!augment) break;
-//             flow += augment;
-//         }
-//         return flow;
-//     }
-//
-//     std::pair<std::vector<int>, std::vector<int>> minimum_vertex_cover() {
-//         std::vector<int> L, R;
-//         for (int u = 0; u < n_left; ++u) {
-//             if (!~dist[u])
-//                 L.push_back(u);
-//             else if (~match_from_left[u])
-//                 R.push_back(match_from_left[u]);
-//         }
-//         return {L, R};
-//     }
-//
-//     std::vector<std::pair<int, int>> get_edges() {
-//         std::vector<std::pair<int, int>> ans;
-//         for (int u = 0; u < n_left; ++u)
-//             if (match_from_left[u] != -1)
-//                 ans.emplace_back(u, match_from_left[u]);
-//         return ans;
-//     }
-// };
 
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
     // cout << setprecision(20) << fixed;
 
     constexpr int N = 50005;
-    vector<int> id(N, -1);
+    vector<int> id(N);
     vector<vector<int>> d(N);
     for (int i = 1; i < N; ++i)
         for (int j = 2 * i; j < N; j += i) d[j].push_back(i);
@@ -209,20 +179,22 @@ int main() {
         cin >> n;
         vector<int> a(n);
         for (auto& x : a) cin >> x;
-        for (int i = 0; i < n; ++i) id[a[i]] = i;
-        bipartite_matching<> b(2 * n, 2 * n);
-        for (int i = 0; i < n; ++i) {
-            b.add(i, n + i);
-            const int ai = a[i];
+        for (int i = 0; i < n; ++i) id[a[i]] = i + 1;
+        mf_graph<int> g(4 * n + 2);
+        for (int i = 1; i <= n; ++i) {
+            g.add_edge(0, i, 1);
+            g.add_edge(n + i, 2 * n + i, 1);
+            g.add_edge(3 * n + i, 4 * n + 1, 1);
+            const int ai = a[i - 1];
             for (auto m : d[ai]) {
                 int j = id[m];
-                if (j == -1) continue;
-                b.add(i, j);
-                b.add(n + i, n + j);
+                if (j == 0) continue;
+                g.add_edge(i, n + j, 1);
+                g.add_edge(2 * n + i, 3 * n + j, 1);
             }
         }
-        cout << b.get_max_matching() - n << '\n';
-        for (int i = 0; i < n; ++i) id[a[i]] = -1;
+        cout << g.flow(0, 4 * n + 1) << '\n';
+        for (int i = 0; i < n; ++i) id[a[i]] = 0;
     }
 }
 
