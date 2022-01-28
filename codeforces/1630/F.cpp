219f125bc4bdf65f085289653687ba6030845bae
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2022-01-28 11:49:00 +0530

    Add solution for problem `F - Making It Bipartite` Link: https://codeforces.com/contest/1630/problem/F Tags: flows, graph matchings, graphs, number theory, *3400 Ref: https://codeforces.com/contest/1630/submission/144287946

--- codeforces/1630/F.cpp
+++ codeforces/1630/F.cpp
@@ -41,14 +41,11 @@ struct Random : std::mt19937 {
     }
 };
 
-// consider shuffling adjacency lists if this gives TLE
 template <bool ToShuffle = false>
 struct bipartite_matching {
-    int n_left, n_right;
-    std::vector<std::basic_string<int>> g;
+    int n_left, n_right, flow = 0;
+    std::vector<std::vector<int>> g;
     std::vector<int> match_from_left, match_from_right;
-    std::vector<int> vis;
-    int iteration;
 
     bipartite_matching(int _n_left, int _n_right)
         : n_left(_n_left),
@@ -56,29 +53,43 @@ struct bipartite_matching {
           g(_n_left),
           match_from_left(_n_left, -1),
           match_from_right(_n_right, -1),
-          vis(_n_left, 0),
-          iteration(0) {}
+          dist(_n_left) {}
 
-    // u on left, v on right
     void add(int u, int v) { g[u].push_back(v); }
 
+    std::vector<int> dist;
+
+    void bfs() {
+        std::queue<int> q;
+        for (int u = 0; u < n_left; ++u) {
+            if (!~match_from_left[u])
+                q.push(u), dist[u] = 0;
+            else
+                dist[u] = -1;
+        }
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+            for (auto v : g[u])
+                if (~match_from_right[v] && !~dist[match_from_right[v]]) {
+                    dist[match_from_right[v]] = dist[u] + 1;
+                    q.push(match_from_right[v]);
+                }
+        }
+    }
+
     bool dfs(int u) {
-        vis[u] = iteration;
-        for (auto v : g[u]) {
-            if (match_from_right[v] == -1) {
-                match_from_right[v] = u;
-                match_from_left[u] = v;
+        for (auto v : g[u])
+            if (!~match_from_right[v]) {
+                match_from_left[u] = v, match_from_right[v] = u;
                 return true;
             }
-        }
-        for (auto v : g[u]) {
-            if (vis[match_from_right[v]] != iteration &&
+        for (auto v : g[u])
+            if (dist[match_from_right[v]] == dist[u] + 1 &&
                 dfs(match_from_right[v])) {
-                match_from_right[v] = u;
-                match_from_left[u] = v;
+                match_from_left[u] = v, match_from_right[v] = u;
                 return true;
             }
-        }
         return false;
     }
 
@@ -88,27 +99,44 @@ struct bipartite_matching {
             for (int i = 0; i < n_left; ++i)
                 std::random_shuffle(std::begin(g[i]), std::end(g[i]), rng);
         }
-        int new_matchings = 0;
-        int matchings = 0;
-        do {
-            iteration++;
-            new_matchings = 0;
+        while (true) {
+            bfs();
+            int augment = 0;
             for (int u = 0; u < n_left; ++u)
-                if (match_from_left[u] == -1 && dfs(u)) new_matchings++;
-            matchings += new_matchings;
-        } while (new_matchings > 0);
-        return matchings;
+                if (!~match_from_left[u]) augment += dfs(u);
+            if (!augment) break;
+            flow += augment;
+        }
+        return flow;
+    }
+
+    std::pair<std::vector<int>, std::vector<int>> minimum_vertex_cover() {
+        std::vector<int> L, R;
+        for (int u = 0; u < n_left; ++u) {
+            if (!~dist[u])
+                L.push_back(u);
+            else if (~match_from_left[u])
+                R.push_back(match_from_left[u]);
+        }
+        return {L, R};
+    }
+
+    std::vector<std::pair<int, int>> get_edges() {
+        std::vector<std::pair<int, int>> ans;
+        for (int u = 0; u < n_left; ++u)
+            if (match_from_left[u] != -1)
+                ans.emplace_back(u, match_from_left[u]);
+        return ans;
     }
 };
-
-constexpr int N = 50005;
-std::array<std::basic_string<int>, N> d;
 
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
     // cout << setprecision(20) << fixed;
 
+    constexpr int N = 50005;
     vector<int> id(N, -1);
+    vector<vector<int>> d(N);
     for (int i = 1; i < N; ++i)
         for (int j = 2 * i; j < N; j += i) d[j].push_back(i);
 
