d617cff63c8b944b43af556d2c49b1a63676252f
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2022-01-28 12:23:00 +0530

    Add solution for problem `F - Making It Bipartite` Link: https://codeforces.com/contest/1630/problem/F Tags: flows, graph matchings, graphs, number theory, *3400 Ref: https://codeforces.com/contest/1630/submission/144291001

--- codeforces/1630/F.cpp
+++ codeforces/1630/F.cpp
@@ -327,6 +327,31 @@ IO io;
 #define cin io
 #define cout io
 
+template <class T>
+struct simple_queue {
+    std::vector<T> payload;
+    int pos = 0;
+    void reserve(int n) { payload.reserve(n); }
+    int size() const { return int(payload.size()) - pos; }
+    bool empty() const { return pos == int(payload.size()); }
+    void push(const T& t) { payload.push_back(t); }
+    T& front() { return payload[pos]; }
+    void clear() { payload.clear(), pos = 0; }
+    void pop() { pos++; }
+};
+
+template <class T>
+struct simple_stack {
+    std::vector<T> payload;
+    void reserve(int n) { payload.reserve(n); }
+    int size() const { return int(payload.size()); }
+    bool empty() const { return payload.empty(); }
+    void push(const T& t) { payload.push_back(t); }
+    T& top() { return payload.back(); }
+    void clear() { payload.clear(); }
+    void pop() { payload.pop_back(); }
+};
+
 struct Random : std::mt19937 {
     using std::mt19937::mt19937;
     using std::mt19937::operator();
@@ -352,112 +377,60 @@ struct Random : std::mt19937 {
 
 template <bool ToShuffle = false>
 struct bipartite_matching {
-   public:
+    int n_left, n_right;
+    std::vector<std::basic_string<int>> g;
+    std::vector<int> match_from_left, match_from_right;
+    std::vector<int> vis;
+    int iteration;
+
     bipartite_matching(int _n_left, int _n_right)
         : n_left(_n_left),
           n_right(_n_right),
           g(_n_left),
           match_from_left(_n_left, -1),
           match_from_right(_n_right, -1),
-          dist(_n_left) {}
+          vis(_n_left, 0),
+          iteration(0) {}
 
+    // u on left, v on right
     void add(int u, int v) { g[u].push_back(v); }
 
-    int get_max_matching() {
-        if (!computed) compute_max_matching();
-        return flow;
-    }
-
-    std::pair<std::vector<int>, std::vector<int>> minimum_vertex_cover() {
-        if (!computed) compute_max_matching();
-        std::vector<int> L, R;
-        for (int u = 0; u < n_left; ++u) {
-            if (!~dist[u])
-                L.push_back(u);
-            else if (~match_from_left[u])
-                R.push_back(match_from_left[u]);
-        }
-        return {L, R};
-    }
-
-    std::vector<std::pair<int, int>> get_edges() {
-        if (!computed) compute_max_matching();
-        std::vector<std::pair<int, int>> ans;
-        for (int u = 0; u < n_left; ++u)
-            if (match_from_left[u] != -1)
-                ans.emplace_back(u, match_from_left[u]);
-        return ans;
-    }
-
-   private:
-    template <class T>
-    struct simple_queue {
-        std::vector<T> payload;
-        int pos = 0;
-        void reserve(int n) { payload.reserve(n); }
-        int size() const { return int(payload.size()) - pos; }
-        bool empty() const { return pos == int(payload.size()); }
-        void push(const T& t) { payload.push_back(t); }
-        T& front() { return payload[pos]; }
-        void clear() { payload.clear(), pos = 0; }
-        void pop() { pos++; }
-    };
-
-    int n_left, n_right, flow = 0;
-    bool computed = false;
-    std::vector<std::basic_string<int>> g;
-    std::vector<int> match_from_left, match_from_right;
-    std::vector<int> dist;
-
-    void bfs() {
-        std::queue<int> q;
-        for (int u = 0; u < n_left; ++u) {
-            if (!~match_from_left[u])
-                q.push(u), dist[u] = 0;
-            else
-                dist[u] = -1;
-        }
-        while (!q.empty()) {
-            int u = q.front();
-            q.pop();
-            for (auto v : g[u])
-                if (~match_from_right[v] && !~dist[match_from_right[v]]) {
-                    dist[match_from_right[v]] = dist[u] + 1;
-                    q.push(match_from_right[v]);
-                }
-        }
-    }
-
     bool dfs(int u) {
-        for (auto v : g[u])
-            if (!~match_from_right[v]) {
-                match_from_left[u] = v, match_from_right[v] = u;
+        vis[u] = iteration;
+        for (auto v : g[u]) {
+            if (match_from_right[v] == -1) {
+                match_from_right[v] = u;
+                match_from_left[u] = v;
                 return true;
             }
-        for (auto v : g[u])
-            if (dist[match_from_right[v]] == dist[u] + 1 &&
+        }
+        for (auto v : g[u]) {
+            if (vis[match_from_right[v]] != iteration &&
                 dfs(match_from_right[v])) {
-                match_from_left[u] = v, match_from_right[v] = u;
+                match_from_right[v] = u;
+                match_from_left[u] = v;
                 return true;
             }
+        }
         return false;
     }
 
-    void compute_max_matching() {
+    int get_max_matching() {
         if constexpr (ToShuffle) {
             Random rng;
             for (int i = 0; i < n_left; ++i)
                 std::random_shuffle(std::begin(g[i]), std::end(g[i]), rng);
         }
-        while (true) {
-            bfs();
-            int augment = 0;
+        int new_matchings = 0;
+        int matchings = 0;
+        do {
+            iteration++;
+            new_matchings = 0;
             for (int u = 0; u < n_left; ++u)
-                if (!~match_from_left[u]) augment += dfs(u);
-            if (!augment) break;
-            flow += augment;
-        }
-        computed = true;
+                if (match_from_left[u] == -1 && dfs(u)) new_matchings++;
+            matchings += new_matchings;
+        } while (new_matchings > 0);
+        return matchings;
     }
 };
 
