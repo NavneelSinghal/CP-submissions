3b7d64ae045a6a94afe86f8860a19f33c0d0882f
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2022-07-09 13:27:00 +0530

    Add solution for problem `F - Points` Link: https://codeforces.com/contest/1701/problem/F Tags: combinatorics, data structures, implementation, math, matrices, *2500 Ref: https://codeforces.com/contest/1701/submission/163355893

--- codeforces/1701/F.cpp
+++ codeforces/1701/F.cpp
@@ -1,16 +1,7 @@
-#ifndef LOCAL
-    #pragma GCC optimize("O3,unroll-loops")
-    #pragma GCC target("avx2,bmi,bmi2,popcnt,lzcnt")
-// #pragma GCC target("avx2,sse4.2,bmi,bmi2,popcnt,lzcnt")
-#endif
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx2,bmi,bmi2,popcnt,lzcnt")
 
 #include "bits/stdc++.h"
-
-#ifdef DEBUG
-    #include "includes/debug/debug.hpp"
-#else
-    #define debug(...) 0
-#endif
 
 using ll = int64_t;
 using ull = uint64_t;
@@ -330,247 +321,236 @@ IO io;
 #define cin io
 #define cout io
 
-// clang-format off
-template <
-          class Node,
-          class Update,
-          class CombineNodes,
-          class ApplyUpdate,
-          class ComposeUpdates = std::nullptr_t,
-          class CheckLazy = std::nullptr_t>
-struct lazy_segtree {
-    static constexpr bool is_lazy =
-        !std::is_same<ComposeUpdates, std::nullptr_t>::value;
-    static constexpr bool is_check_lazy = !std::is_same<CheckLazy, std::nullptr_t>::value;
+template <int n>
+struct st_wrapper {
+    template <class Node, class Update, class CombineNodes, class ApplyUpdate,
+              class ComposeUpdates = std::nullptr_t,
+              class CheckLazy = std::nullptr_t>
+    struct lazy_segtree {
+        static constexpr bool is_lazy =
+            !std::is_same<ComposeUpdates, std::nullptr_t>::value;
+        static constexpr bool is_check_lazy =
+            !std::is_same<CheckLazy, std::nullptr_t>::value;
 
-   public:
-    template <typename... T>
-    explicit lazy_segtree(int n,
-                          const Node& _id_node,
-                          const CombineNodes& _combine,
-                          const Update& _id_update,
-                          const ApplyUpdate& _apply_update,
-                          const ComposeUpdates& _compose_updates = nullptr,
-                          const CheckLazy& _check_lazy = nullptr)
-        : _n(n),
-          combine(_combine),
-          id_node(_id_node),
-          apply_update(_apply_update),
-          id_update(_id_update),
-          compose_updates(_compose_updates),
-          check_lazy(_check_lazy) {
-        log = 0;
-        while ((1 << log) < _n) ++log;
-        size = 1 << log;
-        d = std::vector<Node>(2 * size, id_node);
-        if constexpr (is_lazy) lz = std::vector<Update>(size, id_update);
-    }
-    
-    void set(int p, Node x) {
-        p += size;
-        if constexpr (is_lazy)
-            for (int i = log; i >= 1; i--) push(p >> i);
-        d[p] = x;
-        for (int i = 1; i <= log; ++i) update(p >> i);
-    }
+        static constexpr int log = [] {
+            int x = 0;
+            while ((1 << x) < n) ++x;
+            return x;
+        }();
+        static constexpr int size = 1 << log;
 
-    Node get(int p) {
-        p += size;
-        if constexpr (is_lazy)
-            for (int i = log; i >= 1; i--) push(p >> i);
-        return d[p];
-    }
+       public:
+        template <typename... T>
+        explicit lazy_segtree(const Node& _id_node,
+                              const CombineNodes& _combine,
+                              const Update& _id_update,
+                              const ApplyUpdate& _apply_update,
+                              const ComposeUpdates& _compose_updates = nullptr,
+                              const CheckLazy& _check_lazy = nullptr)
+            : combine{_combine},
+              id_node{_id_node},
+              apply_update{_apply_update},
+              id_update{_id_update},
+              compose_updates{_compose_updates},
+              check_lazy{_check_lazy},
+              d{},
+              lz{} {}
 
-    Node query(int l, int r) {
-        if (l == r) return id_node;
-        l += size, r += size;
-        if constexpr (is_lazy) {
-            int l_ctz = __builtin_ctz(l);
-            int r_ctz = __builtin_ctz(r);
-            for (int i = log; i > l_ctz; --i) push(l >> i);
-            for (int i = log; i > r_ctz; --i) push((r - 1) >> i);
+        void set(int p, Node x) {
+            p += size;
+            if constexpr (is_lazy)
+                for (int i = log; i >= 1; i--) push(p >> i);
+            d[p] = x;
+            for (int i = 1; i <= log; ++i) update(p >> i);
         }
-        Node sml = id_node, smr = id_node;
-        while (l < r) {
-            if (l & 1) sml = combine(sml, d[l++]);
-            if (r & 1) smr = combine(d[--r], smr);
-            l >>= 1, r >>= 1;
+
+        Node get(int p) {
+            p += size;
+            if constexpr (is_lazy)
+                for (int i = log; i >= 1; i--) push(p >> i);
+            return d[p];
         }
-        return combine(sml, smr);
-    }
-    
-    Node all_query() const { return d[1]; }
-    
-    void update(int p, Update f) {
-        p += size;
-        if constexpr (is_lazy)
-            for (int i = log; i >= 1; i--) push(p >> i);
-        d[p] = apply_update(f, d[p]);
-        for (int i = 1; i <= log; ++i) update(p >> i);
-    }
-    
-    void update(int l, int r, Update f) {
-        if (l == r) return;
-        l += size, r += size;
-        const int l_ctz = __builtin_ctz(l);
-        const int r_ctz = __builtin_ctz(r);
-        if constexpr (is_lazy) {
-            for (int i = log; i > l_ctz; --i) push(l >> i);
-            for (int i = log; i > r_ctz; --i) push((r - 1) >> i);
-        }
-        {
-            const int l2 = l, r2 = r;
+
+        Node query(int l, int r) {
+            if (l == r) return id_node;
+            l += size, r += size;
+            if constexpr (is_lazy) {
+                int l_ctz = __builtin_ctz(l);
+                int r_ctz = __builtin_ctz(r);
+                for (int i = log; i > l_ctz; --i) push(l >> i);
+                for (int i = log; i > r_ctz; --i) push((r - 1) >> i);
+            }
+            Node sml = id_node, smr = id_node;
             while (l < r) {
-                if (l & 1) all_apply(l++, f);
-                if (r & 1) all_apply(--r, f);
+                if (l & 1) sml = combine(sml, d[l++]);
+                if (r & 1) smr = combine(d[--r], smr);
                 l >>= 1, r >>= 1;
             }
-            l = l2, r = r2;
+            return combine(sml, smr);
         }
-        for (int i = l_ctz + 1; i <= log; ++i) update(l >> i);
-        for (int i = r_ctz + 1; i <= log; ++i) update((r - 1) >> i);
-    }
 
-    template <class G>
-    int max_right(int l, G g) {
-        // assert(0 <= l && l <= _n);
-        // assert(g(id_node));
-        if (l == _n) return _n;
-        l += size;
-        if constexpr (is_lazy)
-            for (int i = log; i >= 1; i--) push(l >> i);
-        Node sm = id_node;
-        do {
-            while (l % 2 == 0) l >>= 1;
-            if (!g(combine(sm, d[l]))) {
-                while (l < size) {
-                    if constexpr (is_lazy) push(l);
-                    l = (2 * l);
-                    if (g(combine(sm, d[l]))) {
-                        sm = combine(sm, d[l]);
-                        l++;
-                    }
-                }
-                return l - size;
-            }
-            sm = combine(sm, d[l]);
-            l++;
-        } while ((l & -l) != l);
-        return _n;
-    }
+        Node all_query() const { return d[1]; }
 
-    template <class G>
-    int min_left(int r, G g) {
-        // assert(0 <= r && r <= _n);
-        // assert(g(id_node));
-        if (r == 0) return 0;
-        r += size;
-        if constexpr (is_lazy)
-            for (int i = log; i >= 1; i--) push((r - 1) >> i);
-        Node sm = id_node;
-        do {
-            r--;
-            while (r > 1 && (r % 2)) r >>= 1;
-            if (!g(combine(d[r], sm))) {
-                while (r < size) {
-                    if constexpr (is_lazy) push(r);
-                    r = (2 * r + 1);
-                    if (g(combine(d[r], sm))) {
-                        sm = combine(d[r], sm);
-                        r--;
-                    }
-                }
-                return r + 1 - size;
-            }
-            sm = combine(d[r], sm);
-        } while ((r & -r) != r);
-        return 0;
-    }
-
-   private:
-    int _n, size, log;
-    std::vector<Node> d;
-    std::vector<Update> lz;
-    CombineNodes combine;
-    Node id_node;
-    ApplyUpdate apply_update;
-    Update id_update;
-    ComposeUpdates compose_updates;
-    CheckLazy check_lazy;
-
-    void update(int k) { d[k] = combine(d[2 * k], d[2 * k + 1]); }
-    void all_apply(int k, Update f) {
-        d[k] = apply_update(f, d[k]);
-        if constexpr (is_lazy)
-            if (k < size) lz[k] = compose_updates(f, lz[k]);
-    }
-    void push(int k) {
-        if constexpr (is_check_lazy) {
-            if (!check_lazy(lz[k])) return;
+        void update(int p, Update f) {
+            p += size;
+            if constexpr (is_lazy)
+                for (int i = log; i >= 1; i--) push(p >> i);
+            d[p] = apply_update(f, d[p]);
+            for (int i = 1; i <= log; ++i) update(p >> i);
         }
-        all_apply(2 * k, lz[k]);
-        all_apply(2 * k + 1, lz[k]);
-        lz[k] = id_update;
-    }
+
+        void update(int l, int r, Update f) {
+            if (l == r) return;
+            l += size, r += size;
+            const int l_ctz = __builtin_ctz(l);
+            const int r_ctz = __builtin_ctz(r);
+            if constexpr (is_lazy) {
+                for (int i = log; i > l_ctz; --i) push(l >> i);
+                for (int i = log; i > r_ctz; --i) push((r - 1) >> i);
+            }
+            {
+                const int l2 = l, r2 = r;
+                while (l < r) {
+                    if (l & 1) all_apply(l++, f);
+                    if (r & 1) all_apply(--r, f);
+                    l >>= 1, r >>= 1;
+                }
+                l = l2, r = r2;
+            }
+            for (int i = l_ctz + 1; i <= log; ++i) update(l >> i);
+            for (int i = r_ctz + 1; i <= log; ++i) update((r - 1) >> i);
+        }
+
+        template <class G>
+        int max_right(int l, G g) {
+            if (l == n) return n;
+            l += size;
+            if constexpr (is_lazy)
+                for (int i = log; i >= 1; i--) push(l >> i);
+            Node sm = id_node;
+            do {
+                while (l % 2 == 0) l >>= 1;
+                if (!g(combine(sm, d[l]))) {
+                    while (l < size) {
+                        if constexpr (is_lazy) push(l);
+                        l = (2 * l);
+                        if (g(combine(sm, d[l]))) {
+                            sm = combine(sm, d[l]);
+                            l++;
+                        }
+                    }
+                    return l - size;
+                }
+                sm = combine(sm, d[l]);
+                l++;
+            } while ((l & -l) != l);
+            return n;
+        }
+
+        template <class G>
+        int min_left(int r, G g) {
+            if (r == 0) return 0;
+            r += size;
+            if constexpr (is_lazy)
+                for (int i = log; i >= 1; i--) push((r - 1) >> i);
+            Node sm = id_node;
+            do {
+                r--;
+                while (r > 1 && (r % 2)) r >>= 1;
+                if (!g(combine(d[r], sm))) {
+                    while (r < size) {
+                        if constexpr (is_lazy) push(r);
+                        r = (2 * r + 1);
+                        if (g(combine(d[r], sm))) {
+                            sm = combine(d[r], sm);
+                            r--;
+                        }
+                    }
+                    return r + 1 - size;
+                }
+                sm = combine(d[r], sm);
+            } while ((r & -r) != r);
+            return 0;
+        }
+
+       private:
+        CombineNodes combine;
+        Node id_node;
+        ApplyUpdate apply_update;
+        Update id_update;
+        ComposeUpdates compose_updates;
+        CheckLazy check_lazy;
+        Node d[2 * size];
+        Update lz[size];
+
+        void update(int k) { d[k] = combine(d[2 * k], d[2 * k + 1]); }
+        void all_apply(int k, Update f) {
+            d[k] = apply_update(f, d[k]);
+            if constexpr (is_lazy)
+                if (k < size) lz[k] = compose_updates(f, lz[k]);
+        }
+        void push(int k) {
+            if constexpr (is_check_lazy)
+                if (!check_lazy(lz[k])) return;
+            all_apply(2 * k, lz[k]);
+            all_apply(2 * k + 1, lz[k]);
+            lz[k] = id_update;
+        }
+    };
 };
-// clang-format on
+
+using Base = char;
+struct Node {
+    int c{}, x{};
+    ll ans{};
+};
+constexpr Node id_node{};
+constexpr auto combine = [](const Node& l, const Node& r) -> Node {
+    return Node{l.c + r.c, l.x + r.x, l.ans + r.ans};
+};
+using Update = int;
+constexpr Update id_update = 0;
+constexpr auto apply_update = [](const Update& u, const Node& n) -> Node {
+    return Node{n.c + u, n.x, n.ans + ll(n.x) * u};
+};
+constexpr auto compose_updates = [](const Update& u,
+                                    const Update& v) -> Update {
+    return u + v;
+};
+constexpr int N = 200'000;
+
+st_wrapper<N>::lazy_segtree st{id_node, combine, id_update, apply_update,
+                               compose_updates};
+char x[N];
+
+int rd() {
+    unsigned _;
+    cin >> _;
+    return _;
+}
 
 int main() {
-    cin.tie(nullptr)->sync_with_stdio(false);
-    // cout << setprecision(20) << fixed;
-    int _tests = 1;
-    // cin >> _tests;
-    for (int _test = 1; _test <= _tests; ++_test) {
-        // cout << "Case #" << _test << ": ";
-        int q, d;
-        cin >> q >> d;
-        if (d == 1) {
-            while (q--) cout << "0\n";
-        } else {
-            using Base = char;
-            struct Node {
-                int c{}, x{};
-                ll ans{};
-            };
-            constexpr Node id_node{};
-            constexpr auto combine = [](const Node& l, const Node& r) -> Node {
-                return Node{l.c + r.c, l.x + r.x, l.ans + r.ans};
-            };
-            using Update = int;
-            constexpr Update id_update = 0;
-            // update for c, set for x
-            constexpr auto apply_update = [](const Update& u,
-                                             const Node& n) -> Node {
-                return {n.c + u, n.x, n.ans + ll(n.x) * u};
-            };
-            constexpr auto compose_updates = [](const Update& u,
-                                                const Update& v) -> Update {
-                return u + v;
-            };
-            constexpr int n = 200'000;
-            vector<Base> x(n);
-            lazy_segtree st(n, id_node, combine, id_update, apply_update,
-                            compose_updates);
-            ull ans = 0;
-            while (q--) {
-                unsigned I;
-                cin >> I;
-                int i = I - 1;
-                const int l = max(0, i - d), r = i;
-                const auto nd = st.query(l, r), nd2 = st.get(i);
-                if (x[i]) {
-                    x[i] = false;
-                    ans += -nd.ans + 2 * nd.x - ll(nd2.c - 1) * (nd2.c - 2) / 2;
-                    st.update(l, r, -1);
-                    st.set(i, Node{nd2.c - 1, 0, 0});
-                } else {
-                    x[i] = true;
-                    ans += nd.ans - nd.x + nd2.c * ll(nd2.c - 1) / 2;
-                    st.update(l, r, 1);
-                    st.set(i, Node{nd2.c + 1, 1, nd2.c + 1});
-                }
-                cout << ans << '\n';
+    const int q = rd(), d = rd();
+    if (d == 1) {
+        for ([[maybe_unused]] auto _ : ranges::iota_view(0, q)) cout << "0\n";
+    } else {
+        ll ans = 0;
+        for ([[maybe_unused]] auto _ : ranges::iota_view(0, q)) {
+            const int i = rd() - 1;
+            const int l = max(0, i - d), r = i;
+            const auto nd = st.query(l, r), nd2 = st.get(i);
+            if (x[i]) {
+                x[i] = false;
+                ans += -nd.ans + 2 * nd.x - ll(nd2.c - 1) * (nd2.c - 2) / 2;
+                st.update(l, r, -1);
+                st.set(i, Node{nd2.c - 1, 0, 0});
+            } else {
+                x[i] = true;
+                ans += nd.ans - nd.x + nd2.c * ll(nd2.c - 1) / 2;
+                st.update(l, r, 1);
+                st.set(i, Node{nd2.c + 1, 1, nd2.c + 1});
             }
+            cout << ull(ans) << '\n';
         }
     }
 }
