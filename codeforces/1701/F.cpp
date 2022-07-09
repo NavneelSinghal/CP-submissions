262ad2264921c961f4a84d2d04b009c36a5226e6
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2022-07-09 12:51:00 +0530

    Add solution for problem `F - Points` Link: https://codeforces.com/contest/1701/problem/F Tags: combinatorics, data structures, implementation, math, matrices, *2500 Ref: https://codeforces.com/contest/1701/submission/163352616

--- codeforces/1701/F.cpp
+++ codeforces/1701/F.cpp
@@ -1,7 +1,6 @@
 #ifndef LOCAL
     #pragma GCC optimize("O3,unroll-loops")
     #pragma GCC target("avx2,bmi,bmi2,popcnt,lzcnt")
-
 // #pragma GCC target("avx2,sse4.2,bmi,bmi2,popcnt,lzcnt")
 #endif
 
