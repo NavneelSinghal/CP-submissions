cf490abaad7bafb3e9db5811cb142dbbe1230192
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2022-08-12 10:06:00 +0530

    Add solution for problem `B - Integers Have Friends` Link: https://codeforces.com/contest/1548/problem/B Tags: binary search, data structures, divide and conquer, math, number theory, two pointers, *1800 Ref: https://codeforces.com/contest/1548/submission/167932500

--- codeforces/1548/B.cpp
+++ codeforces/1548/B.cpp
@@ -6,19 +6,19 @@
 
 #define ll long long
 #define N 200000
-#define S 100
+#define LG 20
 
 ll read() {
     ll f = 0;
     ll fu = 1;
-    char c = (char)getchar();
+    char c = getchar();
     while (c < '0' || c > '9') {
         if (c == '-') fu = -1;
-        c = (char)getchar();
+        c = getchar();
     }
     while (c >= '0' && c <= '9') {
         f = (f << 3) + (f << 1) + (c & 15);
-        c = (char)getchar();
+        c = getchar();
     }
     f *= fu;
     return f;
@@ -27,7 +27,7 @@ ll read() {
 void print(ll x) {
     if (x < 0) putchar('-'), x = -x;
     if (x < 10)
-        putchar((char)(x + 48));
+        putchar(x + 48);
     else
         print(x / 10), putchar(x % 10 + 48);
 }
@@ -58,43 +58,60 @@ ll gcd(ll a, ll b) {
     return a << c;
 }
 
-ll a[N], distinct[S];
-int index[S];
+ll a[LG][N];
+int lg[N + 1];
+
+void build(int n) {
+    for (int h = 1; (1 << h) <= n; ++h)
+        for (int i = 0; i + (1 << h) <= n; ++i)
+            a[h][i] = gcd(a[h - 1][i], a[h - 1][i + (1 << (h - 1))]);
+}
+
+ll query(int l, int r) {
+    if (l > r) return 0;
+    if (l == r) return a[0][l];
+    int h = lg[r - l + 1];
+    return gcd(a[h][l], a[h][r - (1 << h) + 1]);
+}
 
 int main() {
+    lg[1] = 0;
+    for (int i = 2; i < N; ++i) lg[i] = lg[i >> 1] + 1;
     int t;
     scanf("%d", &t);
     while (t--) {
         int n;
         scanf("%d", &n);
-        for (int i = 0; i < n; ++i) a[i] = read();
+        for (int i = 0; i < n; ++i) a[0][i] = read();
         if (n == 1) {
             print(1);
             putchar('\n');
             continue;
         }
         --n;
-        for (int i = 0; i < n; ++i) a[i] = abs(a[i] - a[i + 1]);
-        int ans = 0, ptr = 0;
-        for (int i = 0; i < n; ++i) {
-            distinct[ptr] = a[i];
-            index[ptr] = i;
-            for (int j = ptr - 1; j >= 0; --j)
-                distinct[j] = gcd(distinct[j], distinct[j + 1]);
-            int newptr = 0;
-            for (int j = 0; j <= ptr; ++j) {
-                if (j == 0 || distinct[j] != distinct[j - 1]) {
-                    distinct[newptr] = distinct[j];
-                    index[newptr] = index[j];
-                    ++newptr;
+        for (int i = 0; i < n; ++i) a[0][i] = abs(a[0][i] - a[0][i + 1]);
+        build(n);
+        int ans = 0;
+        int l = -1, r = -1;
+        while (++l < n) {
+            while (l <= r) {
+                ll g = query(l, r);
+                if (g != 1) {
+                    break;
+                } else {
+                    ++l;
                 }
             }
-            ptr = newptr;
-            if (distinct[0] != 1) {
-                if (ans < i + 1) ans = i + 1;
-            } else if (newptr > 1) {
-                if (ans < i + 1 - index[1]) ans = i + 1 - index[1];
+            if (r - l + 1 > ans) ans = r - l + 1;
+            while (r + 1 < n) {
+                ll g = query(l, r + 1);
+                if (g == 1) {
+                    break;
+                } else {
+                    ++r;
+                }
             }
+            if (r - l + 1 > ans) ans = r - l + 1;
         }
         print(ans + 1);
         putchar('\n');
