f795e1aab38eea41c9b1d9370fe0242cab92fb44
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2022-08-12 19:52:00 +0530

    Add solution for problem `F - Lost Array` Link: https://codeforces.com/contest/1713/problem/F Tags: bitmasks, combinatorics, constructive algorithms, dp, math, *2900 Ref: https://codeforces.com/contest/1713/submission/167982443

--- codeforces/1713/F.cpp
+++ codeforces/1713/F.cpp
@@ -22,92 +22,11 @@ void print_char(char x) {
     *(obufptr++) = x;
 }
 
-// 1 2 3 4 5 6 7 8 9 10
 void print_non_negative(int x) {
-#define PRINT_MID(X, P) print_char((x / P) % 10 + 48) 
-#define PRINT_TOP(X, P) print_char(x / P + 48) 
-#define PRINT_BOTTOM(X) print_char(x % 10 + 48)
-    if (x < 1000000) {
-        if (x < 1000) {
-            if (x < 10) {
-                print_char(x + 48);
-            } else if (x < 100) {
-                PRINT_TOP(X, 10);
-                PRINT_BOTTOM(X);
-            } else {
-                PRINT_TOP(X, 100);
-                PRINT_MID(X, 10);
-                PRINT_BOTTOM(X);
-            }
-        } else {
-            if (x < 10000) {
-                PRINT_TOP(X, 1000);
-                PRINT_MID(X, 100);
-                PRINT_MID(X, 10);
-                PRINT_BOTTOM(X);
-            } else if (x < 100000) {
-                PRINT_TOP(X, 10000);
-                PRINT_MID(X, 1000);
-                PRINT_MID(X, 100);
-                PRINT_MID(X, 10);
-                PRINT_BOTTOM(X);
-            } else {
-                PRINT_TOP(X, 100000);
-                PRINT_MID(X, 10000);
-                PRINT_MID(X, 1000);
-                PRINT_MID(X, 100);
-                PRINT_MID(X, 10);
-                PRINT_BOTTOM(X);
-            }
-        }
-    } else {
-        if (x < 100000000) {
-            if (x < 10000000) {
-                PRINT_TOP(X, 1000000);
-                PRINT_MID(X, 100000);
-                PRINT_MID(X, 10000);
-                PRINT_MID(X, 1000);
-                PRINT_MID(X, 100);
-                PRINT_MID(X, 10);
-                PRINT_BOTTOM(X);
-            } else {
-                PRINT_TOP(X, 10000000);
-                PRINT_MID(X, 1000000);
-                PRINT_MID(X, 100000);
-                PRINT_MID(X, 10000);
-                PRINT_MID(X, 1000);
-                PRINT_MID(X, 100);
-                PRINT_MID(X, 10);
-                PRINT_BOTTOM(X);
-            }
-        } else {
-            if (x < 1000000000) {
-                PRINT_TOP(X, 100000000);
-                PRINT_MID(X, 10000000);
-                PRINT_MID(X, 1000000);
-                PRINT_MID(X, 100000);
-                PRINT_MID(X, 10000);
-                PRINT_MID(X, 1000);
-                PRINT_MID(X, 100);
-                PRINT_MID(X, 10);
-                PRINT_BOTTOM(X);
-            } else {
-                PRINT_TOP(X, 1000000000);
-                PRINT_MID(X, 100000000);
-                PRINT_MID(X, 10000000);
-                PRINT_MID(X, 1000000);
-                PRINT_MID(X, 100000);
-                PRINT_MID(X, 10000);
-                PRINT_MID(X, 1000);
-                PRINT_MID(X, 100);
-                PRINT_MID(X, 10);
-                PRINT_BOTTOM(X);
-            }
-        }
-    }
-#undef PRINT_MID
-#undef PRINT_TOP
-#undef PRINT_BOTTOM
+    if (x < 10)
+        print_char(x + 48);
+    else
+        print_non_negative(x / 10), print_char(x % 10 + 48);
 }
 
 void print(int x) {
