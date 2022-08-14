5c1eae05afb191aaed05d524894d457cc11db560
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2022-08-14 14:43:00 +0530

    Add solution for problem `E2 - LCM Sum (hard version)` Link: https://codeforces.com/contest/1712/problem/E2 Tags: brute force, data structures, math, number theory, two pointers, *2500 Ref: https://codeforces.com/contest/1712/submission/168247314

--- codeforces/1712/E2.cpp
+++ codeforces/1712/E2.cpp
@@ -32,23 +32,182 @@ void print_str(const char* const c, size_t len) {
     obufptr += len;
 }
 
+#define PRINT_MID(X, P) print_char(((X) / (P)) % 10 + 48)
+#define PRINT_TOP(X, P) print_char((X) / (P) + 48)
+#define PRINT_BOTTOM(X) print_char((X) % 10 + 48)
+
+#define MID_10(X) PRINT_MID(X, 10)
+#define MID_100(X)     \
+    PRINT_MID(X, 100); \
+    MID_10(X);
+#define MID_1000(X)     \
+    PRINT_MID(X, 1000); \
+    MID_100(X);
+#define MID_10000(X)     \
+    PRINT_MID(X, 10000); \
+    MID_1000(X);
+#define MID_100000(X)     \
+    PRINT_MID(X, 100000); \
+    MID_10000(X);
+#define MID_1000000(X)     \
+    PRINT_MID(X, 1000000); \
+    MID_100000(X);
+#define MID_10000000(X)     \
+    PRINT_MID(X, 10000000); \
+    MID_1000000(X);
+#define MID_100000000(X)     \
+    PRINT_MID(X, 100000000); \
+    MID_10000000(X);
+#define MID_1000000000(X)     \
+    PRINT_MID(X, 1000000000); \
+    MID_100000000(X);
+#define MID_10000000000(X)     \
+    PRINT_MID(X, 10000000000); \
+    MID_1000000000(X);
+#define MID_100000000000(X)     \
+    PRINT_MID(X, 100000000000); \
+    MID_10000000000(X);
+#define MID_1000000000000(X)     \
+    PRINT_MID(X, 1000000000000); \
+    MID_100000000000(X);
+#define MID_10000000000000(X)     \
+    PRINT_MID(X, 10000000000000); \
+    MID_1000000000000(X);
+#define MID_100000000000000(X)     \
+    PRINT_MID(X, 100000000000000); \
+    MID_10000000000000(X);
+#define MID_1000000000000000(X)     \
+    PRINT_MID(X, 1000000000000000); \
+    MID_100000000000000(X);
+#define MID_10000000000000000(X)     \
+    PRINT_MID(X, 10000000000000000); \
+    MID_1000000000000000(X);
+#define MID_100000000000000000(X)     \
+    PRINT_MID(X, 100000000000000000); \
+    MID_10000000000000000(X);
+#define MID_1000000000000000000(X)     \
+    PRINT_MID(X, 1000000000000000000); \
+    MID_100000000000000000(X);
+#define MID_10000000000000000000(X)     \
+    PRINT_MID(X, 10000000000000000000); \
+    MID_1000000000000000000(X);
+
+#define PRINT_ALL(X, P)      \
+    PRINT_TOP(X, 10ULL * P); \
+    MID_##P(X);              \
+    PRINT_BOTTOM(X);
+
 // 1 2 3 4 5 6 7 8 9 10
 void print_non_negative(uint32_t x) {
-    if (x < 10)
-        print_char(x + 48);
-    else {
-        print_non_negative(x / 10);
-        print_char(x % 10 + 48);
+    if (x < 1000000) {
+        if (x < 1000) {
+            if (x < 10) {
+                print_char(x + 48);
+            } else if (x < 100) {
+                PRINT_TOP(x, 10);
+                PRINT_BOTTOM(x);
+            } else {
+                PRINT_ALL(x, 10);
+            }
+        } else {
+            if (x < 10000) {
+                PRINT_ALL(x, 100);
+            } else if (x < 100000) {
+                PRINT_ALL(x, 1000);
+            } else {
+                PRINT_ALL(x, 10000);
+            }
+        }
+    } else {
+        if (x < 100000000) {
+            if (x < 10000000) {
+                PRINT_ALL(x, 100000);
+            } else {
+                PRINT_ALL(x, 1000000);
+            }
+        } else {
+            if (x < 1000000000) {
+                PRINT_ALL(x, 10000000);
+            } else {
+                PRINT_ALL(x, 100000000);
+            }
+        }
     }
 }
 
 void print_non_negative_64(uint64_t x) {
-    if (x < 10) print_char(x + 48);
-    else {
-        print_non_negative_64(x / 10);
-        print_char(x % 10 + 48);
+    if (x < 10000000000) {
+        if (x < 1000000) {
+            if (x < 1000) {
+                if (x < 10) {
+                    print_char(x + 48);
+                } else if (x < 100) {
+                    PRINT_TOP(x, 10);
+                    PRINT_BOTTOM(x);
+                } else {
+                    PRINT_ALL(x, 10);
+                }
+            } else {
+                if (x < 10000) {
+                    PRINT_ALL(x, 100);
+                } else if (x < 100000) {
+                    PRINT_ALL(x, 1000);
+                } else {
+                    PRINT_ALL(x, 10000);
+                }
+            }
+        } else {
+            if (x < 100000000) {
+                if (x < 10000000) {
+                    PRINT_ALL(x, 100000);
+                } else {
+                    PRINT_ALL(x, 1000000);
+                }
+            } else {
+                if (x < 1000000000) {
+                    PRINT_ALL(x, 10000000);
+                } else {
+                    PRINT_ALL(x, 100000000);
+                }
+            }
+        }
+    } else {
+        if (x < 10000000000000000) {
+            if (x < 10000000000000) {
+                if (x < 100000000000) {
+                    PRINT_ALL(x, 1000000000);
+                } else if (x < 1000000000000) {
+                    PRINT_ALL(x, 10000000000);
+                } else {
+                    PRINT_ALL(x, 100000000000);
+                }
+            } else {
+                if (x < 100000000000000) {
+                    PRINT_ALL(x, 1000000000000);
+                } else if (x < 1000000000000000) {
+                    PRINT_ALL(x, 10000000000000);
+                } else {
+                    PRINT_ALL(x, 100000000000000);
+                }
+            }
+        } else {
+            if (x < 1000000000000000000) {
+                if (x < 100000000000000000) {
+                    PRINT_ALL(x, 1000000000000000);
+                } else {
+                    PRINT_ALL(x, 10000000000000000);
+                }
+            } else {
+                PRINT_ALL(x, 100000000000000000);
+            }
+        }
     }
 }
+
+#undef PRINT_MID
+#undef PRINT_TOP
+#undef PRINT_BOTTOM
+#undef PRINT_ALL
 
 #define GEN_PRINT(NAME, TYPE, SPECIFIER_STR)  \
     int print_generated_##NAME(TYPE x) {      \
