e01cb795f4db92b455ee369d7a0aa79c8f010238
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2022-08-19 10:00:00 +0530

    Add solution for problem `D - GCD Counting` Link: https://codeforces.com/contest/1101/problem/D Tags: data structures, dfs and similar, dp, number theory, trees, *2000 Ref: https://codeforces.com/contest/1101/submission/168917716

--- codeforces/1101/D.cpp
+++ codeforces/1101/D.cpp
@@ -356,92 +356,95 @@ fast_sieve_func_spf<200000> sv{};
 
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    for (auto& x : a) cin >> x;
-    int add = 0;
-    for (auto x : a)
-        if (x > 1) add = 1;
-    if (not add) {
-        cout << add << '\n';
-        return 0;
-    }
-    const int N = 2e5;
-    vector<bool> nonempty(N + 1);
-    struct Pair {
-        int first, second;
-    };
-    vector<basic_string<Pair>> edges(N + 1);
-    for (int i = 1; i < n; ++i) {
-        int u, v;
-        cin >> u >> v;
-        --u, --v;
-        int g = gcd(a[u], a[v]);
-        while (g > 1) {
-            int p = sv.spf[g];
-            nonempty[p] = true;
-            edges[p].push_back({u, v});
-            while (g % p == 0) g /= p;
-        }
-    }
-    int ans = 0;
+    // cout << setprecision(20) << fixed;
+    int _tests = 1;
+    // cin >> _tests;
+    for (int _test = 1; _test <= _tests; ++_test) {
+        // cout << "Case #" << _test << ": ";
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (auto& x : a) cin >> x;
+        int add = 0;
+        for (auto x : a)
+            if (x > 1) add = 1;
 
-    vector<basic_string<int>> g(n);
-    vector<bool> used(n), visited_1(n), visited_2(n);
-    vector<int> all;
-    all.reserve(n);
-    queue<pair<int, int>> q;
+        const int N = 2e5;
+        vector<bool> nonempty(N + 1);
+        struct Pair {
+            int first, second;
+        };
+        vector<basic_string<Pair>> edges(N + 1);
+        for (int i = 1; i < n; ++i) {
+            int u, v;
+            cin >> u >> v;
+            --u, --v;
+            int g = gcd(a[u], a[v]);
+            while (g > 1) {
+                int p = sv.spf[g];
+                nonempty[p] = true;
+                edges[p].push_back({u, v});
+                while (g % p == 0) g /= p;
+            }
+        }
+        int ans = 0;
 
-    for (int i = 2; i <= N; ++i) {
-        if (!nonempty[i]) continue;
-        auto& edges_p = edges[i];
-        for (auto [u, v] : edges_p) {
-            g[u].push_back(v);
-            g[v].push_back(u);
-            if (!used[u]) used[u] = true, all.push_back(u);
-            if (!used[v]) used[v] = true, all.push_back(v);
-        }
-        if (all.empty()) continue;
-        for (auto root : all) {
-            if (visited_2[root]) continue;
-            pair<int, int> best{0, root};
-            q.emplace(0, root);
-            while (not q.empty()) {
-                auto [du, u] = q.front();
-                q.pop();
-                for (auto v : g[u]) {
-                    if (visited_1[v]) continue;
-                    visited_1[v] = true;
-                    pair x{du + 1, v};
-                    best = max(best, x);
-                    q.push(x);
-                }
+        vector<basic_string<int>> g(n);
+        vector<bool> used(n), visited_1(n), visited_2(n);
+        vector<int> all;
+        all.reserve(n);
+        queue<pair<int, int>> q;
+
+        for (int i = 2; i <= N; ++i) {
+            if (!nonempty[i]) continue;
+            auto& edges_p = edges[i];
+            for (auto [u, v] : edges_p) {
+                g[u].push_back(v);
+                g[v].push_back(u);
+                if (!used[u]) used[u] = true, all.push_back(u);
+                if (!used[v]) used[v] = true, all.push_back(v);
             }
-            root = best.second;
-            best = {0, root};
-            q.emplace(0, root);
-            visited_2[root] = true;
-            while (not q.empty()) {
-                auto [du, u] = q.front();
-                q.pop();
-                for (auto v : g[u]) {
-                    if (visited_2[v]) continue;
-                    visited_2[v] = true;
-                    pair x{du + 1, v};
-                    best = max(best, x);
-                    q.push(x);
+            if (all.empty()) continue;
+            for (auto root : all) {
+                if (visited_2[root]) continue;
+                pair<int, int> best{0, root};
+                q.emplace(0, root);
+                while (not q.empty()) {
+                    auto [du, u] = q.front();
+                    q.pop();
+                    for (auto v : g[u]) {
+                        if (visited_1[v]) continue;
+                        visited_1[v] = true;
+                        pair x{du + 1, v};
+                        best = max(best, x);
+                        q.push(x);
+                    }
                 }
+                root = best.second;
+                best = {0, root};
+                q.emplace(0, root);
+                visited_2[root] = true;
+                while (not q.empty()) {
+                    auto [du, u] = q.front();
+                    q.pop();
+                    for (auto v : g[u]) {
+                        if (visited_2[v]) continue;
+                        visited_2[v] = true;
+                        pair x{du + 1, v};
+                        best = max(best, x);
+                        q.push(x);
+                    }
+                }
+                ans = max(ans, best.first);
             }
-            ans = max(ans, best.first);
+            for (auto u : all) {
+                used[u] = false;
+                visited_1[u] = false;
+                visited_2[u] = false;
+                g[u].clear();
+            }
+            all.clear();
         }
-        for (auto u : all) {
-            used[u] = false;
-            visited_1[u] = false;
-            visited_2[u] = false;
-            g[u].clear();
-        }
-        all.clear();
+        cout << ans + add << '\n';
     }
-    cout << ans + add << '\n';
 }
